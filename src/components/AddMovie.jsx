4a4f7c65c75e0b1fda87aa452e6d02e05ec7e85a
Rodolfo <rodolforrcont@gmail.com>
2021-05-31 15:37:31 -0300

    AddMovie - Select and Options

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -44,8 +44,16 @@ class AddMovie extends Component {
           Avaliação
           <input type="text" name="rating" data-testid="rating-input" value={ this.state.rating } onChange={ this.handleChange } />
         </label>
+        <label data-testid="genre-input-label">
+          Gênero
+          <select name="genre" data-testid="genre-input" onChange={ this.handleChange}>
+            <option value="action" data-testid="genre-option">Ação</option>
+            <option value="comedy" data-testid="genre-option">Comédia</option>
+            <option value="Thriller" data-testid="genre-option">Suspense</option>
+          </select>
+        </label>
       </form>
-    )
+    );
   }
 }
 
