e4690e9f6c2a7684d41ec50b1a71f812168038ca
Víctor Faria <vitilevi@hotmail.com>
2021-05-31 15:21:46 -0300

    Requisito 1

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -7,18 +7,30 @@ import AddMovie from './AddMovie';
 class MovieLibrary extends Component {
   constructor(props) {
     super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange() {
+    console.log(this);
   }
 
   render() {
+    const { movies } = this.props;
     return (
       <div>
         <h2> My awesome movie library </h2>
         <SearchBar />
-        <MovieList movies={this.props.movies} />
+        <MovieList movies={ movies } />
         <AddMovie />
       </div>
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.object,
+  ).isRequired,
+};
 
 export default MovieLibrary;
