4038d35aefc8f5a1e6bd35016e64ee3681883d7d
Rodolfo <rodolforrcont@gmail.com>
2021-05-31 19:33:11 -0300

    Ajustando Movies Data

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -2,17 +2,26 @@ import React, { Component } from 'react';
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
+
 class MovieLibrary extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: props.movies,
+    };
   }
+
   render() {
+    const { movies } = this.state;
     return (
       <div>
         <h2>My awesome movie library</h2>
         <SearchBar />
-        <MovieList movies={ this.props.movies } />
-        <AddMovie onClick={ (teste) => console.log(teste) }/>
+        <MovieList movies={ movies } />
+        <AddMovie onClick={ (teste) => console.log(teste) } />
       </div>
     );
   }
