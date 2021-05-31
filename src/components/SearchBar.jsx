18110723ed48ce5e33291ebceb8ea7b3ed2a7f8f
gabrielfndz <gabriel.omercador@hotmail.com>
2021-05-31 15:16:26 -0300

    adicionando items ao form, ainda com erro pois faltam as propriedades

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,7 +3,25 @@ import React, { Component } from 'react';
 export default class SearchBar extends Component {
   render() {
     return (
-      <div>SearchBar</div>
+      <form data-testid="search-bar-form">
+        <label data-testid="text-input-label">
+          Inclui o texto  
+        <input type="text" value={ searchText } onChange={ onSearchTextChange } data-testid="text-input" />
+        </label>
+        <label data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input type="checkbox" checked={ bookmarkedOnly } onChange={ onBookmarkedChange } data-testid="checkbox-input" />
+        </label>
+        <label data-testid="select-input-label">
+          Filtrar por gênero
+          <select value={ selectedGenre } onChange={ onSelectedGenreChange } data-testid="select-input">
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
+        </label>
+      </form>
     );
   }
 }
