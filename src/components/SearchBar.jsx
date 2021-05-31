de479bbfde95a5befda8887cd9ad59490caef096
Jessica Arruda <sicarruda@gmail.com>
2021-05-31 19:33:56 -0300

    Correçao do lint para requisitos 1 e 2, concluçao do requisito 3

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,35 +1,42 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 class SerarchBar extends React.Component {
   render() {
-    // const {
-    //   searchText,
-    //   onSearchTextChange,
-    //   bookmarkedOnly,
-    //   onBookmarkedChange,
-    //   selectedGenre,
-    //   onSelectedGenreChange,
-    // } = this.props;
+    const {
+      searchText,
+      onSearchTextChange,
+      // bookmarkedOnly,
+      // onBookmarkedChange,
+      // selectedGenre,
+      // onSelectedGenreChange,
+    } = this.props;
 
     return (
       <div>
         <form data-testid="search-bar-form">
-          <label>Inclui o texto</label>
-          <input type="text" />
+          <label htmlFor="text-input" data-testid="text-input-label">
+            Inclui o texto:
+            <input
+              onChange={ onSearchTextChange }
+              data-testid="text-input"
+              type="text"
+              value={ searchText }
+            />
+          </label>
         </form>
       </div>
     );
   }
 }
 
-// SerarchBar.defaultProps = {
-//   searchText: PropTypes.string,
-//   onSearchTextChange: PropTypes.string,
+SerarchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.string.isRequired,
 //   bookmarkedOnly: PropTypes.string,
 //   onBookmarkedChange: PropTypes.string,
 //   selectedGenre: PropTypes.string,
 //   onSelectedGenreChange: PropTypes.string,
-// };
+};
 
 export default SerarchBar;
