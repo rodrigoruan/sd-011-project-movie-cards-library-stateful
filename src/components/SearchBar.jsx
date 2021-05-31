f98f15542d136e036c4e0de3cd84072b62451796
Marco Galindo <marco1408@gmail.com>
2021-05-31 15:01:37 -0300

    adicionando inputs ao searchbar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,20 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <form action="" data-testid="search-bar-form">
+        <input type="text" name="" id="" data-testid="text-input-label" />
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
