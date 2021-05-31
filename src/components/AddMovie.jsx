e8e3bf993e15a987bf7ef8facd92fbe2ba346b95
Oryange <oryangestrifezze@gmail.com>
2021-05-31 18:49:29 -0300

    Adiciona requisito 13

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,35 +2,20 @@ import React from 'react';
 
 class AddMovie extends React.Component {
   render() {
-    const { handler, title, subtitle, imagePath, storyline } = this.props;
+    const { handler, title, subtitle, imagePath, storyline, rating, genre } = this.props;
     return (
       <form data-testid="add-movie-form">
         <label data-testid="title-input-label" htmlFor="input">
           Título
-          <input
-            data-testid="title-input"
-            value={ title }
-            name="title"
-            onChange={ handler }
-          />
+          <input data-testid="title-input" value={ title } name="title" onChange={ handler } />
         </label>
         <label data-testid="subtitle-input-label" htmlFor="input">
           Subtítulo
-          <input
-            data-testid="subtitle-input"
-            value={ subtitle }
-            name="subtitle"
-            onChange={ handler }
-          />
+          <input data-testid="subtitle-input" value={ subtitle } name="subtitle" onChange={ handler } />
         </label>
         <label data-testid="image-input-label" htmlFor="input">
           Imagem
-          <input
-            data-testid="image-input"
-            value={ imagePath }
-            name="imagePath"
-            onChange={ handler }
-          />
+          <input data-testid="image-input" value={ imagePath } name="imagePath" onChange={ handler } />
         </label>
         <label data-testid="storyline-input-label" htmlFor="area">
           Sinopse
@@ -41,7 +26,25 @@ class AddMovie extends React.Component {
             onChange={ handler }
           />
         </label>
+        <label data-testid="rating-input-label" htmlFor="number">
+          Avaliação
+          <input
+            data-testid="rating-input"
+            type="number"
+            value={ rating }
+            name="rating"
+            onChange={ handler }
+          />
+        </label>
+        <label data-testid="genre-input-label" htmlFor="select-1">
+          Gênero
+          <select value={ genre } onChange={ handler } data-testid="genre-input">
+            <option data-testid="genre-option" value="action">Ação</option>
+            <option data-testid="genre-option" value="comedy">Comédia</option>
+            <option data-testid="genre-option" value="thriller">Suspense</option>
+          </select>
 
+        </label>
       </form>
     );
   }
