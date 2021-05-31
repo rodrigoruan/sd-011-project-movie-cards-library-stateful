ef51eff03463558c06d411b467d7de5afa74af54
Hamander <1hamander@gmail.com>
2021-05-31 16:27:11 -0300

    Check Box add

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,30 +1,28 @@
-import React, { Component } from 'react'
-
-
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
   render() {
-    const { 
-      searchText, 
-      onSearchTextChange, 
-      bookmarkedOnly, 
-      onBookmarkedChange, 
-      selectedGenre, 
-      onSelectedGenreChange
-    } = this.props
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
 
     return (
       <form data-testid="search-bar-form">
         <label data-testid="text-input-label">
-        Inclui o texto:
-          <input data-testid="text-input" type="text" value={searchText} onChange={onSearchTextChange}></input>
+          Inclui o texto:
+          <input data-testid="text-input" type="text" value={ searchText } onChange={ onSearchTextChange } />
         </label>
-        <label>
-        Mostrar somente favoritos
-          <input type='checkbox'></input>
+        <label data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input data-testid="checkbox-input" type="checkbox" checked="bookmarkedOnly" onChange={ onBookmarkedChange } />
         </label>
       </form>
-    )
+    );
   }
 }
-
