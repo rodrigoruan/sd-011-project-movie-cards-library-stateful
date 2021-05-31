43aebee21f41086f16371b67418a68dc03a6bf93
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-05-31 19:00:35 -0300

    Requisitos 1, 2 e 3 - componente SearchBar, form e input text

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,33 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends Component {
+  render() {
+    const { searchText, onSearchTextChange } = this.props;
+
+    return (
+      <div>
+        <form data-testid="search-bar-form">
+          <fieldset>
+            <label htmlFor="text-input" data-testid="text-input-label">
+              Inclui o texto:
+              <input
+                type="text"
+                data-testid="text-input"
+                value={ searchText }
+                onChange={ onSearchTextChange }
+              />
+            </label>
+          </fieldset>
+        </form>
+      </div>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+};
+
+export default SearchBar;
