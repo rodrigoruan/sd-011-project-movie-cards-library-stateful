df5b40dfb838ffb60f6403a0309abfa10527816a
Antonio Malato <malatoneto@gmail.com>
2021-05-31 19:19:14 -0300

    Requisito 2

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 
 class SearchBar extends React.Component {
   render() {
-  const { searchText, onSearchTextChange,  bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
 
-    return <h1></h1>;
+    return (
+      <form data-testid="search-bar-form" />
+    );
   }
 }
 
