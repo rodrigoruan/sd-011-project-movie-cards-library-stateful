00bb0d8ef2f24b36aa76dcb5b6a6c2e675487e3c
Jessica Arruda <sicarruda@gmail.com>
2021-05-31 17:08:34 -0300

    Requisitos 1 e 2

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,37 @@
-// implement AddMovie component here
+// https://github.com/tryber/sd-011-project-movie-cards-library-stateful codigo base retirado daqui.
+import React, { Component } from 'react';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import PropTypes from 'prop-types'
+// import AddMovie from './AddMovie';
+
+class MovieLibrary extends Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        <h2> My awesome movie library </h2>
+        <SearchBar
+          searchText=""
+          onSearchTextChange={ () => {} }
+          bookmarkedOnly={ false }
+          onBookmarkedChange={ () => {} }
+          selectedGenre=""
+          onSelectedGenreChange={ () => {} }
+        />
+        <MovieList movies={ movies } />
+        {/* <AddMovie /> */}
+      </div>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.objectOf(PropTypes.string).isRequired,
+};
+
+export default MovieLibrary;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,34 @@
-// implement AddMovie component here
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class SerarchBar extends React.Component {
+  render() {
+    // const {
+    //   searchText,
+    //   onSearchTextChange,
+    //   bookmarkedOnly,
+    //   onBookmarkedChange,
+    //   selectedGenre,
+    //   onSelectedGenreChange,
+    // } = this.props;
+
+    return (
+      <div>
+        <form data-testid="search-bar-form">
+          <input type="text" />
+        </form>
+      </div>
+    );
+  }
+}
+
+// SerarchBar.defaultProps = {
+//   searchText: PropTypes.string,
+//   onSearchTextChange: PropTypes.string,
+//   bookmarkedOnly: PropTypes.string,
+//   onBookmarkedChange: PropTypes.string,
+//   selectedGenre: PropTypes.string,
+//   onSelectedGenreChange: PropTypes.string,
+// };
+
+export default SerarchBar;
