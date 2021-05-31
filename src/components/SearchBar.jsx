e24f80b6950384a32d737260983ff097886968aa
Alexandre <naitwa.alexandre@gmail.com>
2021-05-31 19:55:09 -0300

    04

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,13 +5,22 @@ export default class SearchBar extends Component {
     const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
     return (
       <form data-testid="search-bar-form">
-        <label data-testid="text-input-label" htmlFor="inputText">
+        <label data-testid="text-input-label" htmlFor="text">
           Inclui o texto:
           <input
             data-testid="text-input"
             value={ searchText }
             type="text"
             onChange={ onSearchTextChange }
+          />
+        </label>
+        <label htmlFor="checkbox" data-testid="checkbox-input-label">
+          Mostrar somente favoritos:
+          <input
+            data-testid="checkbox-input"
+            type="checkbox"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
           />
         </label>
       </form>
