36f4b181620e6a4e490a343b59cfa18c8addd498
israeltonholo <israeltonholo@hotmail.com>
2021-05-31 19:04:01 -0300

    corrige lint

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -16,46 +16,44 @@ export default class SearchBar extends Component {
       onSelectedGenreChange,
     } = this.props;
     return (
-      <div>
-        <section>
-          <form data-testid="search-bar-form">
-            <label data-testid="text-input-label" htmlFor="search-text">
-              Inclui o texto:
-              <input
-                id="search-text"
-                type="text"
-                value={ searchText }
-                onChange={ onSearchTextChange }
-                data-testid="text-input"
-              />
-            </label>
-            <label htmlFor="show-favorites" data-testid="checkbox-input-label">
-              Mostrar somente favoritos
-              <input
-                type="checkbox"
-                id="show-favorites"
-                checked={ bookmarkedOnly }
-                onChange={ onBookmarkedChange }
-                data-testid="checkbox-input"
-              />
-            </label>
-            <label data-testid="select-input-label" htmlFor="movie-genres">
-              Filtrar por gênero
-              <select
-                id="movie-genres"
-                value={ selectedGenre }
-                onChange={ onSelectedGenreChange }
-                data-testid="select-input"
-              >
-                <option value="" data-testid="select-option">Todos</option>
-                <option value="action" data-testid="select-option">Ação</option>
-                <option value="comedy" data-testid="select-option">Comédia</option>
-                <option value="thriller" data-testid="select-option">Suspense</option>
-              </select>
-            </label>
-          </form>
-        </section>
-      </div>
+      <section>
+        <form data-testid="search-bar-form">
+          <label data-testid="text-input-label" htmlFor="search-text">
+            Inclui o texto:
+            <input
+              id="search-text"
+              type="text"
+              value={ searchText }
+              onChange={ onSearchTextChange }
+              data-testid="text-input"
+            />
+          </label>
+          <label htmlFor="show-favorites" data-testid="checkbox-input-label">
+            Mostrar somente favoritos
+            <input
+              type="checkbox"
+              id="show-favorites"
+              checked={ bookmarkedOnly }
+              onChange={ onBookmarkedChange }
+              data-testid="checkbox-input"
+            />
+          </label>
+          <label data-testid="select-input-label" htmlFor="movie-genres">
+            Filtrar por gênero
+            <select
+              id="movie-genres"
+              value={ selectedGenre }
+              onChange={ onSelectedGenreChange }
+              data-testid="select-input"
+            >
+              <option value="" data-testid="select-option">Todos</option>
+              <option value="action" data-testid="select-option">Ação</option>
+              <option value="comedy" data-testid="select-option">Comédia</option>
+              <option value="thriller" data-testid="select-option">Suspense</option>
+            </select>
+          </label>
+        </form>
+      </section>
     );
   }
 }
