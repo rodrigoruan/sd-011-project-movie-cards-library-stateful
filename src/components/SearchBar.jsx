2730cc96ec5e04986671422cde3d56b71894bce9
israeltonholo <israeltonholo@hotmail.com>
2021-05-31 19:01:17 -0300

    adiciona requisito 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -19,25 +19,39 @@ export default class SearchBar extends Component {
       <div>
         <section>
           <form data-testid="search-bar-form">
-            <label data-testid="text-input-label" htmlFor="searchText">
+            <label data-testid="text-input-label" htmlFor="search-text">
               Inclui o texto:
               <input
-                id="searchText"
+                id="search-text"
                 type="text"
                 value={ searchText }
                 onChange={ onSearchTextChange }
                 data-testid="text-input"
               />
             </label>
-            <label htmlFor="showFavorites" data-testid="checkbox-input-label">
+            <label htmlFor="show-favorites" data-testid="checkbox-input-label">
               Mostrar somente favoritos
               <input
                 type="checkbox"
-                id="showFavorites"
+                id="show-favorites"
                 checked={ bookmarkedOnly }
                 onChange={ onBookmarkedChange }
                 data-testid="checkbox-input"
               />
+            </label>
+            <label data-testid="select-input-label" htmlFor="movie-genres">
+              Filtrar por gênero
+              <select
+                id="movie-genres"
+                value={ selectedGenre }
+                onChange={ onSelectedGenreChange }
+                data-testid="select-input"
+              >
+                <option value="" data-testid="select-option">Todos</option>
+                <option value="action" data-testid="select-option">Ação</option>
+                <option value="comedy" data-testid="select-option">Comédia</option>
+                <option value="thriller" data-testid="select-option">Suspense</option>
+              </select>
             </label>
           </form>
         </section>
@@ -51,4 +65,6 @@ SearchBar.propTypes = {
   onSearchTextChange: PropTypes.string.isRequired,
   bookmarkedOnly: PropTypes.bool.isRequired,
   onBookmarkedChange: PropTypes.bool.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.string.isRequired,
 };
