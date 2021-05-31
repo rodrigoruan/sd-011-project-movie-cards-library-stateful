118ceb870f5c8709b53e1a920e5b69b22a7955c2
Julio Cesar <julio.cesarcj1402@gmail.com>
2021-05-31 16:23:53 -0300

    3 primeiros requisitos

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './components/Header';
 import SearchBar from './components/SearchBar';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <SearchBar
-        searchText=""
-        onSearchTextChange=""
-        bookmarkedOnly=""
-        onBookmarkedChange=""
-        selectedGenre=""
-        onSelectedGenreChange=""
-      />
-      <Header />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <SearchBar
+          data-testid="search-bar-form"
+          searchText=""
+          onSearchTextChange=""
+          bookmarkedOnly=""
+          onBookmarkedChange=""
+          selectedGenre=""
+          onSelectedGenreChange=""
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,27 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <form>
+        <div>
+          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+          <label htmlFor="input">
+            Inclui o texto:
+          </label>
+          <input type="text" data-testid="text-input" id="input" />
+        </div>
+        <div>
+          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+          <label htmlFor="checkbox">
+            Mostrar somente favoritos
+          </label>
+          <input onChange="onBookmarkedChange" checked="" type="checkbox" id="checkbox" data-testid="checkbox-input" />
+        </div>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
