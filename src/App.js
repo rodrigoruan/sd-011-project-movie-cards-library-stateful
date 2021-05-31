d20f98fd1cc5e9f3e977e35bd5f593016ae0ea93
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-05-31 14:50:36 -0300

    Done requirement 1, 2 and 3. Missing adding the SearchBar  props functions.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar
+        searchText=""
+        onSearchTextChange="Adicionar a logica callBack"
+        bookmarkedOnly={ false } // Adicionar a logica
+        onBookmarkedChange="Adicionar a logica callBack"
+        selectedGenre="Adicionar a logica"
+        onSelectedGenreChange="Adicionar a logica callBack"
+      />
     </div>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,30 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends React.Component {
+  render() {
+    const { searchText, onSearchTextChange } = this.props;
+
+    return (
+      <form data-testid="search-bar-form">
+        <label data-testid="text-input-label" htmlFor="input">
+          Inclui o texto:
+          <input
+            onChange={ onSearchTextChange }
+            value={ searchText }
+            name="searchBar"
+            id="input"
+            placeholder="Search..."
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+};
+
+export default SearchBar;
