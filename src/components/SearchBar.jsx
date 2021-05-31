8c213446bc2036eb31dc1e8e9a0767ab1dcc789c
Daniel Leon <daniel.radialista@gmail.com>
2021-05-31 14:45:36 -0300

    iniciando projeto, fazendo requisitos 1 e 2

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
   render() {
+    //   const {
+    //       searchText,
+    //       onSearchTextChange,
+    //   } = this.props;
+
     return (
       <div>
-        Search Bar
+        <form data-testid="search-bar-form" />
       </div>
     );
   }
