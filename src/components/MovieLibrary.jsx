d9b7c87f6f15d834794f7ea62245dc53028e08e1
Tales Coelho <tales-coelho@live.com>
2021-05-31 18:20:31 -0300

    to perdido sem os testes

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,68 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+import AddMovie from './AddMovie';
+
+class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+    const { movies } = this.props;
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+  }
+
+  onSearchTextChange() {
+    const { movies, searchText } = this.state;
+    this.setState(() => ({
+      movies: movies.filter((el) => el.title.includes(searchText)
+        || el.subtitle.includes(searchText)
+        || el.storyline.includes(searchText)),
+    }));
+  }
+
+  onBookmarkedChange() {
+    const { movies, bookmarkedOnly } = this.state;
+    this.setState(() => ({
+      movies: movies.filter((el) => el.bookmarked === bookmarkedOnly),
+    }));
+  }
+
+  onSelectedGenreChange() {
+    const { movies, selectedGenre } = this.state;
+    this.setState(() => ({
+      movies: movies.filter((el) => el.genre === selectedGenre),
+    }));
+  }
+
+  render() {
+    const { movies, searchText, bookmarkedOnly, selectedGenre } = this.state;
+    return (
+      <div>
+        <SearchBar
+          searchText={ searchText }
+          bookmarkedOnly={ bookmarkedOnly }
+          selectedGenre={ selectedGenre }
+          onSearchTextChange={ onSearchTextChange }
+          onBookmarkedChange={ onBookmarkedChange }
+          onSelectedGenreChange={ onSelectedGenreChange }
+        />
+        <MovieList movies={ movies } />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.object.isRequired,
+  ).isRequired,
+};
+
+export default MovieLibrary;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-export class SearchBar extends Component {
+class SearchBar extends Component {
   render() {
-    const { searchText, onSearchTextChange,
-      bookmarkedOnly, onBookmarkedChange,
-      selectedGenre, onSelectedGenreChange } = this.props;
+    const {
+      searchText,
+      bookmarkedOnly,
+      selectedGenre,
+      onSearchTextChange,
+      onBookmarkedChange,
+      onSelectedGenreChange,
+    } = this.props;
     return (
       <form data-testid="search-bar-form">
         <label htmlFor="text-input-label" data-testid="text-input-label">
@@ -46,12 +51,12 @@ export class SearchBar extends Component {
   }
 }
 
-SearchBar.proTypes = {
+SearchBar.propTypes = {
   searchText: PropTypes.string.isRequired,
-  onSearchTextChange: PropTypes.func.isRequired,
   bookmarkedOnly: PropTypes.bool.isRequired,
-  onBookmarkedChange: PropTypes.func.isRequired,
   selectedGenre: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
   onSelectedGenreChange: PropTypes.func.isRequired,
 };
 
