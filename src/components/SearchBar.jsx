323e44547179a9e9a56485d9730e907384b06fa1
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-05-31 15:36:02 -0300

    exercicio 2 resolvido

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,7 +4,9 @@ class SearchBar extends Component {
   render() {
     const { searchText, onSearchTextChange, bookmarkedOnly, selectedGenre, onSelectedGenreChange } = this.props;
     return (
-      
+      <form data-testid="search-bar-form">
+
+      </form>
     );
   };
 };
