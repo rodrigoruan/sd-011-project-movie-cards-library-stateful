35725d50d3bc51553f56cc35e24b0d117c3a3c4c
heyynat <natalifplima@gmail.com>
2021-05-31 18:31:56 -0400

    Requisito 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            subtitle: '',
+            title: '',
+            imagePath: '',
+            storyline: '',
+            rating: 0,
+            genre: 'açao',
+        };
+    };
     render() {
         const { onClick } = this.props;
         return (
             <form data-testid="add-movie-form">
-
+                <label data-testid="title-input-label">
+                Título
+                <input
+                data-testid="title-input"
+                />
+                </label>
             </form>
         )
     }
