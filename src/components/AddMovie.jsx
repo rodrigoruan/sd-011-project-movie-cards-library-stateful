c083e195b48a84e5da52a56be106146fd33a5d94
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-05-31 14:44:39 -0300

    Adiciona requisito 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import TitleInput from './AddMoviesFomrsInputs/TitleInput';
 
 class AddMovie extends React.Component {
   constructor() {
     super();
+
+    this.chosenMovie = this.chosenMovie.bind(this);
 
     this.state = {
       subtitle: '',
@@ -14,11 +17,23 @@ class AddMovie extends React.Component {
     };
   }
 
+  chosenMovie({ target }) {
+    const { name } = target;
+    let { value } = target;
+
+    if (name === 'rating') value = Number(value);
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
     const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
 
     return (
       <form data-testid="add-movie-form">
+        <TitleInput chosenMovie={ this.chosenMovie } title={ title } />
       </form>
     );
   }
---
+++ src/components/AddMoviesFomrsInputs/TitleInput.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class TitleInput extends React.Component {
+  render() {
+    const { title, chosenMovie } = this.props;
+
+    return (
+      <label htmlFor="title-input" data-testid="title-input-label">
+        Título
+        <input
+          id="title-input"
+          name="title"
+          type="text"
+          value={ title }
+          data-testid="title-input"
+          onChange={ chosenMovie }
+        />
+      </label>
+    );
+  }
+}
+
+TitleInput.propTypes = {
+  title: PropTypes.string.isRequired,
+  chosenMovie: PropTypes.func.isRequired,
+};
+
+export default TitleInput;
