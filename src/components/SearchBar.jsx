ef02542984c17a5b1a0cfb1f0910fa1054fb094a
heyynat <natalifplima@gmail.com>
2021-05-31 17:13:41 -0400

    Requisito 1

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,12 @@
-// implement AddMovie component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+    render() {
+        const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+        return (
+            searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange
+        )
+    }
+}
+
+export default SearchBar;
