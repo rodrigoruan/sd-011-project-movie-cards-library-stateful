63b74c768bade72fbf3c93b3b474158b2feca612
Savio Moraes <smmsavio@hotmail.com>
2021-05-31 18:01:16 -0300

    testa evaluetor

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,28 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  constructor() {
+    super()
+
+  }
+
+  render() {
+    return;
+  }
+}
+
+// MovieCard.propTypes = {
+//   movie: PropTypes.shape({
+//     title: PropTypes.string,
+//     subtitle: PropTypes.string,
+//     storyline: PropTypes.string,
+//     rating: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+//     imagePath: PropTypes.string,
+//   }).isRequired,
+// };
+
+SearchBar.propTypes = {
+  
+}
+
+export default SearchBar;
