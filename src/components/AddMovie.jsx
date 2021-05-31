b78f36de73550c2bef7452114d3656cdf46593ab
Víctor Faria <vitilevi@hotmail.com>
2021-05-31 18:11:03 -0300

    Adicionado alterações

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -4,14 +4,60 @@ export default class AddMovie extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      subtitle: '',
+      title: '',
+      imagePatch: '',
+      storyLine: '',
+      rating: 0,
+      genre: 'action',
     };
+    this.initialState = { ...this.state };
+    this.modifyState = this.modifyState.bind(this);
+  }
+
+  modifyState({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
   }
 
   render() {
+    const {
+      subtitle,
+      title,
+      // imagePatch,
+      // storyLine,
+      // rating,
+      // genre
+    } = this.state;
     return (
       <div>
-        teste
+        <form data-testid="add-movie-form">
+          <label htmlFor="add-title" data-testid="title-input-label">
+            Título
+            <input
+              name="title"
+              type="text"
+              id="add-title"
+              value={ title }
+              data-testid="title-input"
+              onChange={ this.modifyState }
+            />
+          </label>
+
+          <label htmlFor="add-subtitle" data-testid="subtitle-input-label">
+            Subtítulo
+            <input
+              name="subtitle"
+              type="text"
+              id="add-subtitle"
+              value={ subtitle }
+              data-testid="subtitle-input"
+              onChange={ this.modifyState }
+            />
+          </label>
+        </form>
       </div>
     );
   }
