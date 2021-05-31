accc75b19db58a93edcb7686af4543fbfe74ed20
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-05-31 16:02:56 -0300

    adiciona requisito 08

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      title: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { title } = this.state;
     return (
-      <form data-testid="add-movie-form" />
+      <form data-testid="add-movie-form">
+        <label htmlFor="addMovieTitleInput" data-testid="title-input-label">
+          Título
+          <input
+            id="addMovieTitleInput"
+            name="title"
+            type="text"
+            value={ title }
+            onChange={ this.handleChange }
+            data-testid="title-input"
+          />
+        </label>
+      </form>
     );
   }
 }
