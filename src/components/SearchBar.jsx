2d71495493aab0cb25ae9c6e5e090185d00afed2
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-05-31 15:53:04 -0300

    Realizacao do desafio 2

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,7 +4,9 @@ class SearchBar extends React.Component {
   render() {
     const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
     return (
-      
+      <form>
+        test
+      </form>
     );
   }
 }
