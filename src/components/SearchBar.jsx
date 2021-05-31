c2803f89d64ff627c0e77c434e0b785bc335331b
josanjohnata <josanjohnata@gmail.com>
2021-05-31 16:05:20 -0300

    corrigindo o Lint

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,19 +3,26 @@ import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
-    const { searchText, onSearchTextChange} = this.props;
+    const { searchText, onSearchTextChange } = this.props;
     return (
       <form data-testid="search-bar-form">
         <label data-testid="text-input-label">
           Inclui o texto
-          <input type="text" value={ searchText }
+          <input 
+            type="text"
+            value={ searchText }
             onChange={ onSearchTextChange }
             data-testid="text-input"
           />
-        </label>
+        </label>;
       </form>
     )
   }
 }
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+};
 
 export default SearchBar;
