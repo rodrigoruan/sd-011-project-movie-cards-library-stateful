fc8c419613d89955f40235b875bff3571f9f9ad8
Gabriel <gabrielfernandessilva.ba@gmail.com>
2021-05-31 15:50:53 -0300

    6 e 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,24 @@
-// Iniciando Projeto!
+import React, { Component } from 'react'
+
+export default class AddMovie extends Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
+    return (
+      <form data-testid="add-movie-form">
+        
+      </form>
+    )
+  }
+}
