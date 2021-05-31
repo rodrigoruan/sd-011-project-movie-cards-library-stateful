7f35de503c90c22aa6bf8232fe3c7f1d625908cf
marcoswarmling <marcoswarmling15@gmail.com>
2021-05-31 18:56:24 -0300

    requisito 6

---
+++ src/components/AddMovie.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export default class AddMovie extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          subtitle: '',
+          title: '',
+          imagePath: '',
+          storyline: '',
+          rating: 0,
+          genre: 'action',
+        };
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
