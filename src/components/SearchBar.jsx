52173dcdea96f44711b81dee1bdc022dbf08bbd5
Alexandre <naitwa.alexandre@gmail.com>
2021-05-31 19:22:10 -0300

    03

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,20 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+  render() {
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+    return (
+      <form data-testid="search-bar-form">
+        <label data-testid="text-input-label" htmlFor="inputText">
+          Inclui o texto:
+          <input
+            data-testid="text-input"
+            value={ searchText }
+            type="text"
+            onChange={ onSearchTextChange }
+          />
+        </label>
+      </form>
+    );
+  }
+}
