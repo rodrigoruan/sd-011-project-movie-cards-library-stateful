8d00b0f77104138b9f88e76468b08f3daea9c107
Diogo Augusto <diogotrescastro@gmail.com>
2021-05-31 16:25:36 -0300

    Resoluçao do requisito 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,31 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+
+export default class AddMovie extends Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+
+    // this.handleChange = this.handleChange.bind(this);
+  }
+
+  // handleChange({target}) {
+  //   const { name, value } = target;
+  //   this.setState({
+  //     [name]: value,
+  //   });
+  // }
+
+  render() {
+    return (
+      <div> </div>
+    );
+  }
+}
