ebb6dbcfa310df5be0f478affaae53911aba0e4d
Rodolfo <rodolforrcont@gmail.com>
2021-05-31 16:37:50 -0300

    AddMovie Button - MovieLibrary

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -13,6 +13,7 @@ class AddMovie extends Component {
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.resetState = this.resetState.bind(this);
   }
 
   handleChange({ target }) {
@@ -22,50 +23,69 @@ class AddMovie extends Component {
     });
   }
 
-  handleSubmit(event) {
-    event.preventDefault();
-    this.setState = {
+  handleSubmit() {
+    const { onClick } = this.props;
+    onClick(this.state);
+    this.resetState();
+  }
+
+  resetState() {
+    this.setState = ({
       subtitle: '',
       title: '',
       imagePath: '',
       storyline: '',
       rating: 0,
       genre: 'action',
-    }
+    });
   }
 
   render() {
     return (
       <form data-testid="add-movie-form">
-        <label data-testid="title-input-label">
+        <label data-testid="title-input-label" htmlFor="title">
           Título
-          <input type="text" data-testid="title-input" name="title" value={ this.state.title } onChange={ this.handleChange } />
+          <input 
+            type="text"
+            data-testid="title-input"
+            name="title"
+            value={ this.state.title }
+            onChange={ this.handleChange }
+          />
         </label>
-        <label type="text" data-testid="subtitle-input-label">
+        <label type="text" data-testid="subtitle-input-label" htmlFor="subtitle">
           Subtítulo
-          <input type="text" data-testid="subtitle-input" name="subtitle" value={ this.state.subtitle } onChange={ this.handleChange } />
+          <input
+            type="text" 
+            data-testid="subtitle-input"
+            name="subtitle"
+            value={ this.state.subtitle }
+            onChange={ this.handleChange }
+          />
         </label>
-        <label data-testid="image-input-label">
+        <label data-testid="image-input-label" htmlFor="imagePath">
           Imagem
-          <input type="text" data-testid="image-input" name="imagePath" value={ this.state.imagePath } onChange={ this.handleChange } />
+          <input
+            type="text" 
+            data-testid="image-input" name="imagePath" value={ this.state.imagePath } onChange={ this.handleChange } />
         </label>
         <label data-testid="storyline-input-label">
           Sinopse
-          <textarea data-testid="storyline-input" name="stroyline" value={ this.state.storyline } />
+          <textarea data-testid="storyline-input" name="storyline" value={ this.state.storyline } onChange={ this.handleChange } />
         </label>
         <label data-testid="rating-input-label">
           Avaliação
-          <input type="text" name="rating" data-testid="rating-input" value={ this.state.rating } onChange={ this.handleChange } />
+          <input type="number" name="rating" data-testid="rating-input" value={ this.state.rating } onChange={ this.handleChange } />
         </label>
         <label data-testid="genre-input-label">
           Gênero
           <select name="genre" data-testid="genre-input" value={ this.state.genre }onChange={ this.handleChange}>
             <option value="action" data-testid="genre-option">Ação</option>
             <option value="comedy" data-testid="genre-option">Comédia</option>
-            <option value="Thriller" data-testid="genre-option">Suspense</option>
+            <option value="thriller" data-testid="genre-option">Suspense</option>
           </select>
         </label>
-        <button type="submit" data-testid="send-button" onClick={ this.handleSubmit }>Adicionar filme</button>
+        <button type="button" data-testid="send-button" onClick={ this.handleSubmit }>Adicionar filme</button>
       </form>
     );
   }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -44,7 +44,7 @@ class SearchBar extends Component {
           </select>
         </label>
       </form>
-    )
+    );
   }
 }
 
