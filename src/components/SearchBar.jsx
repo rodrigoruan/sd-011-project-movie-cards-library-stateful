631104cdc7687217ff3285354ca07872c3ac9f76
Tales Coelho <tales-coelho@live.com>
2021-05-31 15:17:53 -0300

    Criando um form para o searchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Label from './Label';
 
 export class SearchBar extends Component {
   render() {
@@ -7,10 +8,39 @@ export class SearchBar extends Component {
       bookmarkedOnly, onBookmarkedChange,
       selectedGenre, onSelectedGenreChange } = this.props;
     return (
-      <div>
-        
-      </div>
-    )
+      <form data-testid="search-bar-form">
+        <Label
+          id="text-input-label"
+          name="Inclui o texto:"
+          idLabel="text-input-label"
+          value={ searchText }
+          iOnChange={ onSearchTextChange }
+          idI="text-input"
+        />
+        <label htmlFor="checkbox-input-label" data-testid="checkbox-input-label">
+          "Filtrar por gênero"
+          <input
+            id="checkbox-input-label"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+            data-testid="checkbox-input"
+          />
+          <label htmlFor="checkbox-input-label" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            id="checkbox-input-label"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+            data-testid="checkbox-input"
+          >
+            <option selected value="">Todos</option>
+            <option value="action">Ação</option>
+            <option value="comedy">Comédia</option>
+            <option value="thriller">Suspense</option>
+          </select>
+        </label>
+      </form>
+    );
   }
 }
 
