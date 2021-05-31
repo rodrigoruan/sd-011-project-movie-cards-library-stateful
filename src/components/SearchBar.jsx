3cd8738537f9c0c0dd1520d57700d53790bca10f
israeltonholo <israeltonholo@hotmail.com>
2021-05-31 18:29:44 -0300

    adiciona requisito 3

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class SearchBar extends Component {
   constructor(props) {
@@ -17,13 +18,25 @@ export default class SearchBar extends Component {
     return (
       <div>
         <section>
-          <form data-testid="search-bar-form"></form>
+          <form data-testid="search-bar-form">
+            <label data-testid="text-input-label" htmlFor="searchText">
+              Inclui o texto:
+              <input
+                id="searchText"
+                type="text"
+                value={ searchText }
+                onChange={ onSearchTextChange }
+                data-testid="text-input"
+              />
+            </label>
+          </form>
         </section>
       </div>
-    )
+    );
   }
 }
 
 SearchBar.propTypes = {
-
-}
+  searchText: PropTypes.string,
+  onSearchTextChange: PropTypes.string,
+};
