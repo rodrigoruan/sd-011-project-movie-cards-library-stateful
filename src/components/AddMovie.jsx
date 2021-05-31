02f0f1b42d190d190dc46c33600bdc2c37c0e67b
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-05-31 16:47:31 -0300

    Requisitos 8 e 9 Realizados - LINT NOK

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -5,7 +5,8 @@ class AddMovie extends React.Component {
   constructor() {
     super();
 
-    this.handleChange = this.handleChange.bind(this);
+    this.handleChangeTitle = this.handleChangeTitle.bind(this);
+    this.handleChangesubTitle = this.handleChangesubTitle.bind(this);
 
     this.state = {
       subtitle: '',
@@ -17,24 +18,41 @@ class AddMovie extends React.Component {
     };
   }
 
-  handleChange(event) {
+  handleChangeTitle(event) {
     this.setState({
       title: event.target.value,
     });
   }
 
+  handleChangesubTitle(event) {
+    this.setState({
+      subtitle: event.target.value,
+    });
+  }
+
   render() {
-    const { onClick } = this.props;
     return (
       <div>
         <form action="" data-testid="add-movie-form">
           <label htmlFor="inputTitle" data-testid="title-input-label">
             Título
-            <input type="text" 
-            id="inputTitle"  
-            data-testid="title-input" 
-            value={this.state.title}
-            onChange={this.handleChange}/>
+            <input
+              type="text"
+              id="inputTitle"
+              data-testid="title-input"
+              value={ this.state.title }
+              onChange={ this.handleChangeTitle }
+            />
+          </label>
+          <label htmlFor="inputSubtitle" data-testid="subtitle-input-label">
+            Subtítulo
+            <input
+              type="text"
+              data-testid="subtitle-input"
+              id="inputSubtitle"
+              value={ this.state.subtitle }
+              onChange={ this.handleChangesubTitle }
+            />
           </label>
         </form>
       </div>
