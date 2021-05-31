7ea6274b1a4aab134c0e4ce4b5f0c6f0c73717d2
Daniel Leon <daniel.radialista@gmail.com>
2021-05-31 19:19:00 -0300

    requisitos 15 e 17, ok, trabalhando no requisito 16

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,17 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import AddMovie from './AddMovie';
+
+import SearchBar from './SearchBar';
+
+class MovieLibrary extends Component {
+  render() {
+    return (
+      <div>
+        <AddMovie onClick={ this.onClick } />
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
