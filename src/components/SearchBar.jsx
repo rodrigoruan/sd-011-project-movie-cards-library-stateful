4c785ffd3f0b9715e6ef2d61cbf978409c19ca15
Gisele Costa <jikinha@gmail.com>
2021-05-31 15:49:17 -0300

    requisito 4

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,18 +3,26 @@ import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
-    const { searchText, onSearchTextChange } = this.props;
-    // , bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange } = this.props;
+    // , selectedGenre string, onSelectedGenreChange callback
     return (
       <form data-testid="search-bar-form">
         { /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }
-        <label data-testid="text-input-label" htmlFor="text-input">
+        <label data-testid="text-input-label">
           Inclui o texto:
           <input
             value={ searchText }
             onChange={ onSearchTextChange }
             data-testid="text-input"
-            id="text-input"
+          />
+        </label>
+        <label data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input
+            type="checkbox"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+            data-testid="checkbox-input"
           />
         </label>
       </form>
@@ -25,11 +33,15 @@ class SearchBar extends React.Component {
 SearchBar.propTypes = {
   searchText: PropTypes.string,
   onSearchTextChange: PropTypes.func,
+  bookmarkedOnly: PropTypes.bool,
+  onBookmarkedChange: PropTypes.func,
 };
 
 SearchBar.defaultProps = {
   searchText: '',
   onSearchTextChange: 'undefined',
+  bookmarkedOnly: false,
+  onBookmarkedChange: 'undefined',
 };
 
 export default SearchBar;
