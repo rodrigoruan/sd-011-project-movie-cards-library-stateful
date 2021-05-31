260a686e83070a2f1902a3e233bc0d10fd4ddbba
Tales Coelho <tales-coelho@live.com>
2021-05-31 15:31:12 -0300

    Adicionando os inputs para o searchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import Label from './Label';
 
 export class SearchBar extends Component {
   render() {
@@ -9,29 +8,31 @@ export class SearchBar extends Component {
       selectedGenre, onSelectedGenreChange } = this.props;
     return (
       <form data-testid="search-bar-form">
-        <Label
-          id="text-input-label"
-          name="Inclui o texto:"
-          idLabel="text-input-label"
-          value={ searchText }
-          iOnChange={ onSearchTextChange }
-          idI="text-input"
-        />
+        <label htmlFor="text-input-label" data-testid="text-input-label">
+          Inclui o texto:
+          <input
+            id="text-input-label"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+            data-testid="text-input"
+          />
+        </label>
         <label htmlFor="checkbox-input-label" data-testid="checkbox-input-label">
-          "Filtrar por gênero"
+          Mostrar somente favoritos
           <input
             id="checkbox-input-label"
             checked={ bookmarkedOnly }
             onChange={ onBookmarkedChange }
             data-testid="checkbox-input"
           />
-          <label htmlFor="checkbox-input-label" data-testid="select-input-label">
+        </label>
+        <label htmlFor="select-input-label" data-testid="select-input-label">
           Filtrar por gênero
           <select
-            id="checkbox-input-label"
+            id="select-input-label"
             value={ selectedGenre }
             onChange={ onSelectedGenreChange }
-            data-testid="checkbox-input"
+            data-testid="select-input"
           >
             <option selected value="">Todos</option>
             <option value="action">Ação</option>
