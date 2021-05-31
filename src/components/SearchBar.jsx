f39476982ab179cd68209a6c43a912c1c3a1e7de
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-05-31 15:16:56 -0300

    Adiciona requisito 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,22 +2,16 @@ import React, { Component } from 'react';
 
 export class SearchBar extends Component {
   render() {
-    const {
-      searchText,
-      onSearchTextChange,
-      bookmarkedOnly,
-      onBookmarkedChange,
-      selectedGenre,
-      onSelectedGenreChange,
-    } = this.props;
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange,
+      selectedGenre, onSelectedGenreChange } = this.props;
     return (
       <form data-testid="search-bar-form">
         <label data-testid="text-input-label" htmlFor="Texto">
+          Inclui o texto
           <input
             data-testid="text-input"
             value={ searchText }
             onChange={ onSearchTextChange }
-            name="Texto"
             type="text"
           />
         </label>
@@ -27,9 +21,22 @@ export class SearchBar extends Component {
             data-testid="checkbox-input"
             onChange={ onBookmarkedChange }
             checked={ bookmarkedOnly }
-            name="Check"
             type="checkbox"
           />
+        </label>
+        <label data-testid="select-input-label" htmlFor="Select">
+          Filtrar por gênero
+          <select
+            data-testid="select-input"
+            options={ options }
+            onChange={ onSelectedGenreChange }
+            value={ selectedGenre }
+          >
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="Suspense">Suspense</option>
+          </select>
         </label>
       </form>
     );
