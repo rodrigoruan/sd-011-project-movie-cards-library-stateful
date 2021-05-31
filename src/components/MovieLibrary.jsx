b5cc9c0aca8ed53ee91e55d7862cd6f17e1d9d30
Gabriela Feijo <feijo.gabriela@gmail.com>
2021-05-31 15:37:28 -0300

    [Gabriela Feijó] - Movie Cards Library Stateful

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,12 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+
+export default class MovieLibrary extends Component {
+  render() {
+    return (
+      <div>
+        <h2> My awesome movie library </h2>
+      </div>
+    );
+  }
+}
