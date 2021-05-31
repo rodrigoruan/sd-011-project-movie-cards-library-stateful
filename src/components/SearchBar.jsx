09748168844a2cc3bf7cdadf67f3c92fe22ce935
Alexandre <naitwa.alexandre@gmail.com>
2021-05-31 20:41:33 -0300

    05

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -23,6 +23,15 @@ export default class SearchBar extends Component {
             onChange={ onBookmarkedChange }
           />
         </label>
+        <label data-testid="select-input-label" htmlFor="select">
+          Filtrar por gênero
+          <select value={ selectedGenre } onChange={ onSelectedGenreChange } data-testid="select-input">
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
+        </label>
       </form>
     );
   }
