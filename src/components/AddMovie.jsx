3d929452efbe6f9695ab4781f7811c3b3188729d
Ana Carolina R Santos <kroline0693@gmail.com>
2021-05-31 19:31:43 -0300

    Misericordia Lint!

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,74 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovies extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+    this.handleStates = this.handleStates.bind(this);
+  }
+
+  handleStates() {
+    //???
+  }
+
+  render() {
+    const { func: onClick } = this.props;
+    return (
+      <form data-testid="add-movie-form">
+        <label data-testid="subtitle-input-label" htmlFor="title"> Título
+          <input id="title" data-testid="title-input" onChange={ this.state.title } />
+        </label>
+        <label data-testid="title-input-label" htmlFor="subtitle"> Subtítulo
+          <input id="subtitle" data-testid="subtitle-input" onChange={ this.state.subtitle } />
+        </label>
+        <label data-testid="image-input-label" htmlFor="img">
+          Imagem
+          <input id="img" data-testid="image-input" onChange={ this.state.imagePath } />
+        </label>
+        <label data-testid="storyline-input-label" htmlFor="storyline">
+          Sinopse
+          <textarea
+            id="storyline"
+            data-testid="storyline-input" onChange={ this.state.storyline } />
+        </label>
+        <label data-testid="rating-input-label" htmlFor="rating">
+          Avaliação
+          <input
+            id="rating"
+            data-testid="rating-input"
+            onChange={ this.state.rating }
+          />
+        </label>
+        <label data-testid="genre-input-label" htmlFor="genre">
+          Gênero
+          <select
+            id="genre"
+            data-testid="genre-input"
+            onChange={ this.state.genre }
+          >
+            <option value="action">Ação</option>
+            <option value="comedy">Comédia</option>
+            <option value="thriller">Suspense</option>
+          </select>
+        </label>
+        <button
+          type="button"
+          data-testid="send-button"
+          onChange={ this.onClick }
+        >
+          Adicionar filme
+        </button>
+      </form>
+    );
+  }
+}
+
+export default AddMovies;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,67 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+
+
+  class SearchBar extends React.Component {
+    render() {
+      const {
+        search: {
+        searchText,
+        onSearchTextChange,
+        bookmarkedOnly,
+        onBookmarkedChange,
+        selectedGenre
+        }
+      } = this.props;
+
+      return (
+        <form data-testid="search-bar-form">
+          <label data-testid="text-input-label">
+            Inclui o texto
+            <input 
+            data-testid="text-input"
+            type= "text"
+            value={searchText}
+            onChange={onSearchTextChange}
+            />
+          </label>
+          <label data-testid="checkbox-input-label">
+            Mostrar favoritos
+            <input data-testid="checkbox-input"
+            checked={bookmarkedOnly}
+            onChange={onBookmarkedChange}
+            />
+          </label>
+          <label data-testid="select-input-label">
+            Filtrar por gênero
+            <select data-testid="select-input"
+            type='select'
+            value={selectedGenre}
+            onChange={onSelectedGenreChange}
+            >
+              <option data-testid="select-option" value= "">Todos</option>
+              <option data-testid="select-option" value= "action">Ação</option>
+              <option data-testid="select-option" value= "comedy">Comédia</option>
+              <option data-testid="select-option" value= "thriller">Suspense</option>
+            </select>
+          </label>
+        </form>
+
+      );
+    }
+  }
+
+  export default SearchBar;
+
+  SearchBar.proptypes = {
+    search: PropTypes.shape {
+      searchText: Proptypes.string,
+      onSearchTextChange = Proptypes.function,
+      bookmarkedOnly = Proptypes.function,
+      onBookmarkedChange = Proptypes.function,
+      selectedGenre = Proptypes.string,
+      onSelectedGenreChange = Proptypes.function,
+    }
+  }
