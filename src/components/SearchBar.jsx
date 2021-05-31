adb599d600be48cdf62c360eec7bc5318b8736f5
Cris Lustosa <cmlustosa@gmail.com>
2021-05-31 19:03:45 -0300

    Adiciona requisito 1

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,38 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends Component {
+  // constructor(props) {
+  //  super(props);
+  // }
+
+  render() {
+    const { searchText,
+      onSearchTextChange,
+      bookMarkedOnly,
+      onBookMarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
+    return (
+      <div>
+        {searchText}
+        {onSearchTextChange}
+        {bookMarkedOnly}
+        {onBookMarkedChange}
+        {selectedGenre}
+        {onSelectedGenreChange}
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
+SearchBar.propTypes = {
+  searchText: PropTypes.string,
+  onSearchTextChange: PropTypes.func,
+  bookMarkedOnly: PropTypes.bool,
+  onBookMarkedChange: PropTypes.func,
+  selectedGenre: PropTypes.string,
+  onSelectedGenreChange: PropTypes.func,
+}.isRequired;
