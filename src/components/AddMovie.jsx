59065d352de41398c49b8d893f6770209ca929f5
Diego Ribeiro <dihrm2001@gmail.com>
2021-05-31 15:40:55 -0300

    Requisitos 8 e 9

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -12,11 +12,41 @@ class AddMovie extends Component {
       rating: 0,
       genre: 'action',
     };
+
+    this.handleUserInput = this.handleUserInput.bind(this);
+  }
+
+  handleUserInput({ target: { name, value } }) {
+    this.setState({
+      [name]: value,
+    });
   }
 
   render() {
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
     return (
-      <form data-testid="add-movie-form" />
+      <form data-testid="add-movie-form">
+        <label htmlFor="title" data-testid="title-input-label">
+          Título
+          <input
+            data-testid="title-input"
+            type="text"
+            name="title"
+            value={ title }
+            onChange={ this.handleUserInput }
+          />
+        </label>
+        <label htmlFor="subtitle" data-testid="subtitle-input-label">
+          Subtítulo
+          <input
+            data-testid="subtitle-input"
+            type="text"
+            name="subtitle"
+            value={ subtitle }
+            onChange={ this.handleUserInput }
+          />
+        </label>
+      </form>
     );
   }
 }
