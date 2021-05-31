4da357fd80727e6afb75615d231b9f4d5f3fc52f
Amandha W. Barok <amandha.barok@gmail.com>
2021-05-31 11:39:28 -0700

    Requirements number 1, 2 and 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar
+        searchText=""
+        onSearchTextChange={ callback }
+        bookmarkedOnly={ false }
+        onBookmarkedChange={ callback }
+        selectedGenre=""
+        onSelectedGenreChabge={ callback }
+      />
     </div>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,28 @@
-// implement AddMovie component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    const { searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange } = this.props;
+    return (
+      <form data-testid="search-bar-form" >
+        <label data-testid="text-input-label">
+        Inclui o texto:
+          <input 
+            type='text' 
+            name='text-input' 
+            value={searchText} 
+            onChange={onSearchTextChange}
+            data-testid="text-input"
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
