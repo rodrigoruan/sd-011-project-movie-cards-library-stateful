71bf202ba6a4db70153d67551e504ae938046eda
Felipe Buso <felipe_buso@hotmail.com>
2021-05-31 15:19:24 -0300

    Iniciando o projeto. #VQVgit statusgit status :rocket:

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,9 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class MovieLibrary extends Component {
+  render() {
+    return (<h1> Minha lista de filmes</h1>);
+  }
+}
+
+export default MovieLibrary;
