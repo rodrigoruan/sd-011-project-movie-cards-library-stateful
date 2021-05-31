b8a3b5e0a59fc0f8d4283c932c23396e3b178555
marcoswarmling <marcoswarmling15@gmail.com>
2021-05-31 19:18:10 -0300

    requisito 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,22 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class AddMovie extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          subtitle: '',
-          title: '',
-          imagePath: '',
-          storyline: '',
-          rating: 0,
-          genre: 'action',
-        };
-    }
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+inputTitle(){
+
+}
+
+  render() {
+    return (
+
+         <form data-testid="add-movie-form">
+    
+         </form>
+    );
+  }
 }
