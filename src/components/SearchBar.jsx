7a8fbb6fb89841e4cf4f5cfc9076f164f48e581e
Thyago Pessoa <flash_thyago@hotmail.com>
2021-05-31 16:55:40 -0300

    Desafio 1

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,17 @@
 // implement AddMovie component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    const {
+      searchText,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange
+    } = this.props;
+
+    return (<h1>Hi</h1>);
+  }
+}
+
+export default SearchBar;
