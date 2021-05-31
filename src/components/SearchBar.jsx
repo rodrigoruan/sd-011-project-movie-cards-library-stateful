e79fbdaceba8023438c5a0cd1334841eface182d
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-05-31 15:32:35 -0300

    exercicio 1 resolvido

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,12 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    const { searchText, onSearchTextChange, bookmarkedOnly, selectedGenre, onSelectedGenreChange } = this.props;
+    return (
+      
+    );
+  };
+};
+
+export default SearchBar;
