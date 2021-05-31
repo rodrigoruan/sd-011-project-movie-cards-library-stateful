71fb27b50ba1b36e93e9d1c794cffec53501171d
Arthur Hermann <arthurhermannc@gmail.com>
2021-05-31 15:48:41 -0300

    requisito 4 e 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -10,7 +10,34 @@ class SearchBar extends Component {
       <form data-testid="search-bar-form">
         <label data-testid="text-input-label" name="Inclui o texto:">
           Inclui o texto:
-          <input value={ searchText } onChange={ onSearchTextChange } />
+          <input
+            data-testid="text-input"
+            name="Inclui o texto:"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+          />
+        </label>
+        <label data-testid="checkbox-input-label" name="Mostrar somente favoritos">
+          <input
+            data-testid="checkbox-input"
+            type="checkbox"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+          />
+          Mostrar somente favoritos
+        </label>
+        <label data-testid="select-input-label" name="Filtrar por gênero">
+          Filtrar por gênero
+          <select
+            data-testid="select-input"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+          >
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
         </label>
       </form>
     );
