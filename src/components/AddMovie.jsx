16460ffaa6d5648726cf0b58e2c7a2956e46a639
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-05-31 17:45:07 -0300

    Realizacao do desafio 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,27 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      subtitle: "",
+      title: "",
+      imagePath: "",
+      storyPath: "",
+      rating: 0,
+      genre: "action",
+    };
+  }
+
+  render() {
+    return (
+      <form>
+        test
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
