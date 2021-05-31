a3937798e56b229780d1b70bcc344065b8281623
renan <renan_menezes07@hotmail.com>
2021-05-31 18:05:50 -0300

    Requisito 18.

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -14,6 +14,60 @@ class MovieLibrary extends Component {
       selectedGenre: '', // guarda o gênero do filme selecionado para poder fazer a filtragem;
       movies: '', // guarda a lista de filmes.
     };
+
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+    this.addMovie = this.addMovie.bind(this);
+    this.filterMovieList = this.filterMovieList.bind(this);
+  }
+
+  onSearchTextChange({ target: { value } }) {
+    this.setState({
+      searchText: value,
+    });
+  }
+
+  onBookmarkedChange({ target }) {
+    this.setState({
+      bookmarkedOnly: target.value,
+    });
+  }
+
+  onSelectedGenreChange({ target: { value } }) {
+    this.setState({
+      selectedGenre: value,
+    });
+  }
+
+  addMovie({ target }) {
+    const { value } = target;
+    const newMovie = {
+      title: value.title,
+      subtitle: value.subtitle,
+      storyline: value.storyline,
+      rating: value.rating,
+      imagePath: value.imagePath,
+      bookmarked: false,
+      genre: value.genre,
+    };
+
+    this.setState((state) => ({
+      movies: state.movies.push(newMovie),
+    }));
+  }
+
+  filterMovieList() {
+    const filteredMovies = () => {
+      const { searchText } = this.state;
+      const filterStates = ['title', 'subtitle', 'storyline'];
+      return data.filter((movie) => filterStates.some((state) => movie[state]
+        .toString().toLowerCase().indexOf(searchText.toLowerCase()) >= 0));
+    };
+
+    this.setState({
+      movies: filteredMovies(),
+    });
   }
 
   render() {
@@ -29,7 +83,7 @@ class MovieLibrary extends Component {
           selectedGenre={ selectedGenre }
           onSelectedGenreChange={ this.onSelectedGenreChange }
         />
-        <MovieList movies={this.props.movies} />
+        <MovieList movies={ movies } />
         <AddMovie />
       </div>
     );
