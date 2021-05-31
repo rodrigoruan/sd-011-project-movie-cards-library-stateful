3dbe0b360d09a9c28379deec8d91bf127cf0d7d4
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-05-31 17:27:23 -0300

    exericio 8 resolvido

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 class AddMovie extends Component {
   constructor() {
     super();
+    this.changeValues = this.changeValues.bind(this);
     this.state({
       subtitle: '',
       title: '',
@@ -12,10 +13,28 @@ class AddMovie extends Component {
       genre: action,
     });
   }
+
+  changeValues(event) {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    })
+  }
+
   render() {
     const { onClick } = this.props;
     return (
       <form data-testid="add-movie-form">
+        <label data-testid="title-input-label">
+          Título
+          <input
+            data-testid="title-input"
+            type="text"
+            value={ this.state.title }
+            onChange={ this.changeValues() }
+          >
+
+        </label>
       </form>
     );
   }
