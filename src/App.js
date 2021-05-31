a7f539dbf42b3d1069f0784d8cd7f85ab918b062
Lucas Maestrelli <lucasmaestrelli95@gmail.com>
2021-05-31 16:37:40 -0300

    req 1 ao 5

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
+import MovieList from './components/MovieList';
+import filmes from './data';
+import SearchBar from './components/SearchBar';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar />
+
+      <MovieList className="movie-list" movies={ filmes } />
     </div>
   );
 }
