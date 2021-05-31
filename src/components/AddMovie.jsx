346ef830d8f6b5fb38179cca7feac59095e2f3df
Gabriel <gabrielfernandessilva.ba@gmail.com>
2021-05-31 16:41:55 -0300

    Requesito 9 a 13

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class AddMovie extends Component {
   constructor() {
@@ -15,16 +15,34 @@ export default class AddMovie extends Component {
     this.makeInput = this.makeInput.bind(this);
   }
 
+  // Função para receber o valor dos inputs e salvar no state.
   handleChange({ target }) {
-    const { name } = target;
+    const { name, value } = target;
+    if (name === 'image') {
       this.setState({
-        [name]: target.value,
+        imagePath: value, 
+      })
+    }
+      this.setState({
+        [name]: value,
       });
   }
 
   // Criando os inputs dinamicamente, necessitando apenas a chamada da função com o this e os parametros necessarios. (diminuir a quantidade de linhas pro lint não reclamar)
   makeInput = (name, value, type, onChange) => (
     <input
+      name={ name }
+      value={ value }
+      type={ type }
+      data-testid={ `${name}-input` }
+      id={ `${name}-input` }
+      onChange={ onChange }
+    />
+  );
+  
+  // Criando os textareas dinamicamente, necessitando apenas a chamada da função com o this e os parametros necessarios. (diminuir a quantidade de linhas pro lint não reclamar)
+  makeTextArea = (name, value, type, onChange) => (
+    <textarea
       name={ name }
       value={ value }
       type={ type }
@@ -42,7 +60,38 @@ export default class AddMovie extends Component {
           Título
           {this.makeInput('title', title, 'text', this.handleChange)}
         </label>
+        <label htmlFor="subtitle-input" data-testid="subtitle-input-label">
+          Subtítulo
+          {this.makeInput('subtitle', subtitle, 'text', this.handleChange)}
+        </label>
+        <label htmlFor="image-input" data-testid="image-input-label">
+          Imagem
+          {this.makeInput('image', imagePath, 'text', this.handleChange)}
+        </label>
+        <label htmlFor="storyline-input" data-testid="storyline-input-label">
+          Sinopse
+          {this.makeTextArea('storyline', storyline, 'text', this.handleChange)}
+        </label>
+        <label htmlFor="rating-input" data-testid="rating-input-label">
+          Avaliação
+          {this.makeInput('rating', rating, 'number', this.handleChange)}
+        </label>
+        <label htmlFor="genre" data-testid="genre-input-label">
+          Gênero
+          <select
+            name="genre"
+            value={ genre }
+            type="number"
+            data-testid="genre-input"
+            id="genre"
+            onChange={ this.handleChange }
+          >
+            <option value="action" data-testid="genre-option">Ação</option>
+            <option value="comedy" data-testid="genre-option">Comédia</option>
+            <option value="thriller" data-testid="genre-option">Suspense</option>
+          </select>
+        </label>
       </form>
-    )
+    );
   }
 }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,51 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+
+class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+    const { movies } = this.props;
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+  }
+
+  render() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+    return (
+      <div>
+        <h2> My awesome movie library </h2>
+        <SearchBar
+          searchText={ searchText }
+          bookmarkedOnly={ bookmarkedOnly }
+          selectedGenre={ selectedGenre }
+          onSearchTextChange={ this.onSearchTextChange }
+          onBookmarkedChange={ this.onBookmarkedChange }
+          onSelectedGenreChange={ this.onSelectedGenreChange }
+        />
+        <MovieList movies={ genreFilter } />
+        <AddMovie onClick={ this.createMovie } />
+      </div>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    imagePath: PropTypes.string,
+    bookmarked: PropTypes.bool,
+    genre: PropTypes.string,
+  })).isRequired,
+};
+
+export default MovieLibrary;
