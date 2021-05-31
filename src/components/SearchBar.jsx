3ed434da8411eaf83d86e4b32440d94f28806688
Pedro Castro <pa.coelho.castro@gmail.com>
2021-05-31 19:10:14 -0300

    Req 1

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,34 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends Component {
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
+
+    return (
+      <div />
+    );
+  }
+}
+
+SearchBar.defaultProps = {
+  movie: {},
+};
+
+SearchBar.propTypes = {
+  movie: PropTypes.shape({
+    searchText: PropTypes.string,
+    onSearchTextChange: PropTypes.func,
+    bookmarkedChange: PropTypes.bool,
+    selectedGenre: PropTypes.string,
+    onSelectedGenreChange: PropTypes.func,
+  }),
+};
+
+export default SearchBar;
