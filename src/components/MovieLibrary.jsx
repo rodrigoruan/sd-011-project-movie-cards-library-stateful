671f990d28f04ec1c4cfcd4f7acf1bf4e33d5e1f
PH Assis <pieit@live.com>
2021-05-31 14:23:34 -0300

    Initial Commit

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,24 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import Form from 'react-bootstrap/Form';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+
+class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h2 className="mt-3 text-center"> My awesome movie library </h2>
+        <SearchBar />
+        <MovieList movies={this.props.movies} />
+        {/* <AddMovie /> */}
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,71 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import '../App.css';
+
+export default class SearchBar extends Component {
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
+    return (
+      <Form data-testid="search-bar-form" className="col">
+        <Form.Group>
+          {/* SEARCH INPUT TEXT */}
+          <Form.Label data-testid="text-input-label">Inclui o texto: </Form.Label>
+          <Form.Control
+            data-testid="text-input"
+            size="lg"
+            type="text"
+            value={searchText}
+            onChange={onSearchTextChange}
+          ></Form.Control>
+
+          {/* CHECKBOX FAVORITOS */}
+        </Form.Group>
+        <Form.Group as={Row}>
+          <Form.Check
+            checked={bookmarkedOnly}
+            onChange={onBookmarkedChange}
+            data-testid="checkbox-input"
+            className="ml-3"
+            label=""
+          />
+          <Form.Label data-testid="checkbox-input-label">Mostrar somente favoritos </Form.Label>
+        </Form.Group>
+
+        {/* INPUT SELECT OPTIONS */}
+        <Form.Group className="genderMovieInput">
+          <Form.Label data-testid="select-input-label">Filtrar por gênero: </Form.Label>
+          <Form.Control
+            data-testid="select-input"
+            value={selectedGenre}
+            onChange={onSelectedGenreChange}
+            checked={bookmarkedOnly}
+            onChange={onSelectedGenreChange}
+            as="select"
+          >
+            <option value="" data-testid="select-option">
+              Todos
+            </option>
+            <option value="action" data-testid="select-option">
+              Ação
+            </option>
+            <option value="comedy" data-testid="select-option">
+              Comédia
+            </option>
+            <option value="thriller" data-testid="select-option">
+              Suspense
+            </option>
+          </Form.Control>
+        </Form.Group>
+      </Form>
+    );
+  }
+}
