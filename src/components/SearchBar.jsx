866eb1cf23af3d02e705e49b8ab818522a008db6
Amandha W. Barok <amandha.barok@gmail.com>
2021-05-31 11:55:43 -0700

    Requirements 4 and 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -11,7 +11,7 @@ class SearchBar extends React.Component {
     return (
       <form data-testid="search-bar-form" >
         <label data-testid="text-input-label">
-        Inclui o texto:
+          Inclui o texto:
           <input 
             type='text' 
             name='text-input' 
@@ -19,6 +19,39 @@ class SearchBar extends React.Component {
             onChange={onSearchTextChange}
             data-testid="text-input"
           />
+        </label>
+        <label data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input 
+            type='checkbox'
+            name='checkbox-input'
+            checked={bookmarkedOnly}
+            onChange={onBookmarkedChange}
+            data-testid="checkbox-input"
+          />
+        </label>
+        <label data-testid="select-input-label">
+          Filtrar por gênero
+          <select data-testid="select-input"
+            onChange={onSelectedGenreChange}
+            value={selectedGenre}>
+            <option data-testid="select-option"
+              value=''>
+              Todos
+            </option>
+            <option data-testid="select-option" 
+              value='action'>
+              Ação
+            </option>
+            <option data-testid="select-option"
+              value='comedy'>
+              Comédia
+            </option>
+            <option data-testid="select-option"
+              value='thriller'>
+              Suspense
+            </option>
+          </select>
         </label>
       </form>
     );
