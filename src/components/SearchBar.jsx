a372622e1f0aee654f20794f01db8da42b6fe88e
Julio Filizzola <juliofilizzola@hotmail.com>
2021-05-31 14:45:54 -0300

    [Julio Filizzola] - editando APP.js e Começando a criar o componente SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,24 @@
-// implement AddMovie component here
+import React, { Component } from 'react'
+
+export default class SearchBar extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
