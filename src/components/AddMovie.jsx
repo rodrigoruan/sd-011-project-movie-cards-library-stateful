5cd2d12fca99639c3fe1b84546c9055b1ed51f0c
Gisele Costa <jikinha@gmail.com>
2021-05-31 16:36:48 -0300

    me de uma luz, lint

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -8,19 +8,44 @@ const initialState = {
   storyline: '',
   rating: 0,
   genre: 'action',
-}
+};
 class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = initialState;
+    this.onChangeHandler = this.onChangeHandler.bind(this);
+  }
+
+  onChangeHandler = ({ target }) => {
+    let { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
-    const { onClick } = this.props;
+    const { onClick, title } = this.props;
     return (
       <form data-testid="add-movie-form">
+        <label data-testid="title-input-label" htmlFor="">
+          Título
+          <input
+            type="text"
+            value={ initialState.title }
+            data-testid="title-input"
+            onChange={ this.onChangeHandler }
+          />
+        </label>
       </form>
     );
   }
 }
 
-MovieList.propTypes = {
+AddMovie.propTypes = {
   onClick: PropTypes.func,
+  title: PropTypes.string,
 };
 
 export default AddMovie;
