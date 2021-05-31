5fd97262597f4b984c7fb41b1e17f48cacb4c5e5
thiagofox <thiagofox00@gmail.com>
2021-05-31 20:29:41 -0300

    lint problems

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,40 +1,37 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-class AddMovie extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      subtitle: '',
-      title: '',
-      imagePath: '',
-      storyline: '',
-      rating: 0,
-      genre: 'action',
-    };
-  }
+// class AddMovie extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       subtitle: '',
+//       title: '',
+//       imagePath: '',
+//       storyline: '',
+//       rating: 0,
+//       genre: 'action',
+//     };
+//   }
 
-  render() {
-    const { onClick } = this.props;
-    return (
-      <div>
-        {onClick}
-        <form data-testid="add-movie-form">
-          <label htmlFor="title">
-            Título
-            <input 
-              data-testid="title-input" 
-              value={this.state.title} 
-              type="text" 
-              onChange={}
-            />
-          </label>
-        </form>
-      </div>
-      
-      
-      
-      
-    );
-  }
-}
-export default AddMovie;
+//   render() {
+//     const { onClick } = this.props;
+//     return (
+//       <div>
+//         {onClick}
+//         <form data-testid="add-movie-form">
+//           <label htmlFor="title">
+//             Título
+//             <input
+//               data-testid="title-input"
+//               value={this.state.title}
+//               type="text"
+//               onChange={}
+//             />
+//           </label>
+//         </form>
+//       </div>
+
+//     );
+//   }
+// }
+// export default AddMovie;
