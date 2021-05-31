2a921bc4b78e857d9e465c1055807c8c2821d934
Julio Cesar <julio.cesarcj1402@gmail.com>
2021-05-31 17:19:47 -0300

    alinhando

--- src/App.js
+++ src/App.js
@@ -4,18 +4,48 @@ import SearchBar from './components/SearchBar';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      text: '',
+      favorite: false,
+      genre: '',
+    };
+
+    this.searchText = this.searchText.bind(this);
+    this.searchFavorite = this.searchFavorite.bind(this);
+    this.searchGenre = this.searchGenre.bind(this);
+  }
+
+  searchText(event) {
+    console.log('oi');
+    this.setState.text = event.target.value;
+  }
+
+  searchFavorite(event) {
+    console.log('oi');
+    this.setState.favorite = event.target.checked;
+  }
+
+  searchGenre(event) {
+    console.log('oi');
+    this.setState.genre = event.target.value;
+  }
+
   render() {
+    const { text, favorite, genre } = this.state;
     return (
       <div className="App">
         <Header />
         <SearchBar
           data-testid="search-bar-form"
-          searchText=""
-          onSearchTextChange=""
-          bookmarkedOnly=""
-          onBookmarkedChange=""
-          selectedGenre=""
-          onSelectedGenreChange=""
+          searchText={ text }
+          onSearchTextChange={ this.searchText }
+          bookmarkedOnly={ favorite }
+          onBookmarkedChange={ this.searchFavorite }
+          selectedGenre={ genre }
+          onSelectedGenreChange={ this.searchGenre }
         />
       </div>
     );
