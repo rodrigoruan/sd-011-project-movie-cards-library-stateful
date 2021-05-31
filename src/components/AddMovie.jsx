f068280e4dc8584da8ca62201438c3dcb3bbca82
israeltonholo <israeltonholo@hotmail.com>
2021-05-31 19:13:41 -0300

    adiciona requisito 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,17 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class AddMovie extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { onClick } = this.props;
+    return (
+      <form>
+        
+      </form>
+    )
+  }
+}
