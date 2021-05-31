e1ebe7545228bba5992a24c08f87c3390aa2510d
thiagofox <thiagofox00@gmail.com>
2021-05-31 19:35:29 -0300

    problemas com o lint

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,26 +1,40 @@
-// import React, { Component } from 'react';
+import React, { Component } from 'react';
 
-// class AddMovie extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       subtitle: '',
-//       title: '',
-//       imagePath: '',
-//       storyline: '',
-//       rating: 0,
-//       genre: 'action',
-//     };
-//   }
+class AddMovie extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
 
-//   render() {
-//     const { onClick } = this.props;
-
-//     return (
-//       <div>
-//         {onClick}
-//       </div>
-//     );
-//   }
-// }
-// export default AddMovie;
+  render() {
+    const { onClick } = this.props;
+    return (
+      <div>
+        {onClick}
+        <form data-testid="add-movie-form">
+          <label htmlFor="title">
+            Título
+            <input 
+              data-testid="title-input" 
+              value={this.state.title} 
+              type="text" 
+              onChange={}
+            />
+          </label>
+        </form>
+      </div>
+      
+      
+      
+      
+    );
+  }
+}
+export default AddMovie;
