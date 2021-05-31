2abf7a4bdde982490390438611dc0e12973f1740
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-05-31 17:14:26 -0300

    task 1 structure

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,2 +1,12 @@
 // implement AddMovie component here
-// initial commit
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = initialState;
+    this.onChangeHandler = this.onChangeHandler.bind(this);
+  }
+}
+
+export default AddMovie;
