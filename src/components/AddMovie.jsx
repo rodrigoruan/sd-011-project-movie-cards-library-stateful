d2bec34bb96904a0bf28bf2f3312a52778defe53
Diego Ribeiro <dihrm2001@gmail.com>
2021-05-31 15:33:50 -0300

    Requisitos 6 e 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,24 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    return (
+      <form data-testid="add-movie-form" />
+    );
+  }
+}
+
+export default AddMovie;
