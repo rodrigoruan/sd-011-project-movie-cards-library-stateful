dadb9984bdf0f04f286e6f6fe50011dba8e29061
Gisele Costa <jikinha@gmail.com>
2021-05-31 15:52:04 -0300

    lint

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,26 +3,28 @@ import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
-    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange } = this.props;
+    const { searchText, onSearchTextChange, bookmarkedOnly,
+      onBookmarkedChange } = this.props;
     // , selectedGenre string, onSelectedGenreChange callback
     return (
       <form data-testid="search-bar-form">
-        { /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }
-        <label data-testid="text-input-label">
+        <label htmlFor="text-input" data-testid="text-input-label">
           Inclui o texto:
           <input
             value={ searchText }
             onChange={ onSearchTextChange }
             data-testid="text-input"
+            id="text-input"
           />
         </label>
-        <label data-testid="checkbox-input-label">
+        <label htmlFor="checkbox-input" data-testid="checkbox-input-label">
           Mostrar somente favoritos
           <input
             type="checkbox"
             checked={ bookmarkedOnly }
             onChange={ onBookmarkedChange }
             data-testid="checkbox-input"
+            id="checkbox-input"
           />
         </label>
       </form>
