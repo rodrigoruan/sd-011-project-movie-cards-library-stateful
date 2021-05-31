9e3f30b9d7a7103445b712a5d7c94f1051fb9f7a
Matheus Araújo <matheusaraujo.dev@gmail.com>
2021-05-31 20:31:54 -0300

    Adiciona requisitos 1 ao 5, faltando ajustes do ESLint

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -8,9 +8,22 @@ class SearchBar extends Component {
       onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
     return (
       <form data-testid="search-bar-form">
-        <label>
+        <label data-testid="text-input-label">
           "Inclui o texto:"
-          <input type="text" />
+          <input type="text" data-testid="text-input" value={ searchText } onChange={ onSearchTextChange }/>
+        </label>
+        <label data-testid="checkbox-input-label">
+          "Mostrar somente favoritos"
+          <input type="checkbox" data-testid="checkbox-input" checked={ bookmarkedOnly } onChange={ onBookmarkedChange }/>
+        </label>
+        <label data-testid="select-input-label">
+          "Filtrar por gênero"
+          <select data-testid="select-input" value={ selectedGenre } onChange={ onSelectedGenreChange }>
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
         </label>
       </form>
     );
@@ -18,12 +31,21 @@ class SearchBar extends Component {
 }
 
 SearchBar.propTypes = {
-  searchText: PropTypes.string,
-  onSearchTextChange: PropTypes.func,
-  bookmarkedOnly: PropTypes.bool,
-  onBookmarkedChange: PropTypes.func,
-  selectedGenre: PropTypes.string,
-  onSelectedGenreChange: PropTypes.func,
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
 };
+
+// SearchBar.defaultProps = {
+//   searchText: PropTypes.string.isRequired,
+//   onSearchTextChange: PropTypes.func.isRequired,
+//   bookmarkedOnly: PropTypes.bool.isRequired,
+//   onBookmarkedChange: PropTypes.func.isRequired,
+//   selectedGenre: PropTypes.string.isRequired,
+//   onSelectedGenreChange: PropTypes.func.isRequired,
+// }
 
 export default SearchBar;
