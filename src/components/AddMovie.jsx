63611abdad8f30501bfdafe3fa9b796d7f45c6c2
VictorS <czpask@gmail.com>
2021-05-31 20:36:41 -0300

    Projeto pre lint

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,87 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super()
+    this.handleChange = this.handleChange.bind(this)
+    this.resetState = this.resetState.bind(this)
+    this.functionClick = this.functionClick.bind(this)
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action'
+    }
+  }
+
+  functionClick() {
+    const { onClick } = this.props
+    onClick(this.state)
+    this.resetState()
+  }
+
+  resetState() {
+    this.setState({
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action'
+    })
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+
+    this.setState({
+      [name]: value
+    })
+  }
+  
+  render() {
+    return (
+      <form data-testid="add-movie-form">
+        <label data-testid="title-input-label">
+          Título
+          <input type="text" name="title" value={this.state.title} data-testid="title-input" onChange={this.handleChange} />
+        </label>
+        <label data-testid="subtitle-input-label">
+          Subtítulo
+          <input type="text" name="subtitle" value={this.state.subtitle} data-testid="subtitle-input" onChange={this.handleChange} />
+        </label>
+        <label data-testid="image-input-label">
+          Imagem
+          <input type="text" name="imagePath" value={this.state.imagePath} data-testid="image-input" onChange={this.handleChange} />
+        </label>
+        <label data-testid="storyline-input-label">
+          Sinopse
+          <textarea value={this.state.storyline} name="storyline" data-testid="storyline-input" onChange={this.handleChange} />
+        </label>
+        <label data-testid="rating-input-label">
+          Avaliação
+          <input type="number" value={this.state.rating} name="rating" data-testid="rating-input" onChange={this.handleChange} />
+        </label>
+        <label data-testid="genre-input-label">
+          Gênero
+          <select value={this.state.genre} name="genre" data-testid="genre-input" onChange={this.handleChange} >
+            <option value="action" data-testid="genre-option">Ação</option>
+            <option value="comedy" data-testid="genre-option">Comédia</option>
+            <option value="thriller" data-testid="genre-option">Suspense</option>
+          </select>
+        </label>
+        <button data-testid="send-button" type="button" onClick={this.functionClick}>Adicionar filme</button>
+      </form>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+}
+
+export default AddMovie
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,28 +1,86 @@
 import React from 'react';
-import movies from '../data';
 import AddMovie from './AddMovie';
 import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+import PropTypes from 'prop-types';
 
 class MovieLibrary extends React.Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
+    const { movies } = this.props
+    this.handleChangeLibrary = this.handleChangeLibrary.bind(this)
+    this.movieFilter = this.movieFilter.bind(this)
+    this.onClick = this.onClick.bind(this)
     this.state = {
       searchText: '',
       bookmarkedOnly: false,
       selectedGenre: '',
-      movies: movies
+      movies
     }
   }
-  
+
+  onClick(newMovie) {
+    this.setState({
+      movies: [...this.state.movies, newMovie]
+    })
+  }
+
+  handleChangeLibrary({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+
+    this.setState({
+      [name]: value
+    }, () => this.movieFilter(name))
+  }
+
+  movieFilter(typeFilter) {
+    if(this.state.bookmarkedOnly === true) {
+      let bookmarkedMovies = this.state.movies.filter((filme) => filme.bookmarked === true)
+      return this.setState({
+        movies: bookmarkedMovies
+      })
+    }
+
+    if(typeFilter === 'selectedGenre') {
+      let genreMovies = this.props.movies.filter((filme) => filme.genre === this.state.selectedGenre)
+
+      return this.setState({
+        movies: genreMovies
+      })
+    }
+
+    if(this.state.searchText !== '') {
+      let searchedMovie = this.state.movies.filter((filme) => filme.title.includes(this.state.searchText) 
+      || filme.subtitle.includes(this.state.searchText) 
+      || filme.storyline.includes(this.state.searchText))
+
+      return this.setState({
+        movies: searchedMovie
+      })
+    }
+
+    else {
+      console.log('else')
+      return this.setState({
+        movies: this.props.movies
+      })
+    }
+  }
+
   render() {
-    const { movies } = this.props
     return (
       <div>
-        <SearchBar />
-        <AddMovie />
+        <SearchBar searchText={this.state.searchText} onSearchTextChange={this.handleChangeLibrary} bookmarkedOnly={this.state.bookmarkedOnly} onBookmarkedChange={this.handleChangeLibrary} selectedGenre={this.state.selectedGenre} onSelectedGenreChange={this.handleChangeLibrary} />
+        <MovieList movies={this.state.movies} />
+        <AddMovie onClick={this.onClick}/>
       </div>
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieLibrary
