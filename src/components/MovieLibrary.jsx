114159afdfb34c56cbc921d1788428dafba0fc2d
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-05-31 18:37:48 -0300

    adiciona requisito 16

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,11 +1,67 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
 
 class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: props.movies,
+    };
+
+    this.handleSearchText = this.handleSearchText.bind(this);
+  }
+
+  handleSearchText({ target }) {
+    const { value } = target;
+
+    this.setState({
+      searchText: value,
+    });
+  }
+
+  handleBookmarkedOnly({ target }) {
+    const { checked } = target;
+
+    this.setState({
+      bookmarkedOnly: checked,
+    });
+  }
+
+  handleSelectedGenre({ target }) {
+    const { value } = target;
+
+    this.setState({
+      selectedGenre: value,
+    });
+  }
+
   render() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+
     return (
-      <div />
+      <>
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ this.handleSearchText }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.handleBookmarkedOnly }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.handleSelectedGenre }
+        />
+        <MovieList movies={ movies } />
+      </>
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieLibrary;
