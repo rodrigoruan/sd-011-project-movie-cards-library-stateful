7f5d43e1b83612a68e17cab1996eaf2c26a52cae
Savio Moraes <smmsavio@hotmail.com>
2021-05-31 19:31:44 -0300

    inicia resolucao do requisito 01

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,9 @@
-// implement AddMovie component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return 'sou o SearchBar';
+  }
+}
+
+export default SearchBar;
