f323a6ab1fc6127a268bec5f71b20a9632e971d2
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-05-31 18:20:49 -0300

    add search filter films

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,21 +1,51 @@
-import React, { Component } from 'react';
-
+import React from 'react';
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
-import AddMovie from './AddMovie';
+// import AddMovie from './AddMovie';
 
-class MovieLibrary extends Component {
+class MovieLibrary extends React.Component {
   constructor(props) {
     super(props);
+    const { movies } = props;
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+
+    this.handleTextChange = this.handleTextChange.bind(this);
+  }
+
+  handleTextChange(event) {
+    this.setState({ searchText: event.target.value });
+  }
+
+  filterMovies() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+
+    if (searchText !== '') {
+      return movies.filter((movie) => movie.title.includes(searchText)
+        || movie.subtitle.includes(searchText)
+        || movie.storyline.includes(searchText));
+    }
+
+    return movies;
   }
 
   render() {
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
     return (
       <div>
         <h2> My awesome movie library </h2>
-        <SearchBar />
-        <MovieList movies={ this.props.movies } />
-        <AddMovie />
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ this.handleTextChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          selectedGenre={ selectedGenre }
+        />
+        <MovieList movies={ this.filterMovies() } />
+        {/* <AddMovie /> */}
       </div>
     );
   }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,29 @@
-// implement AddMovie component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  renderTextInput() {
+    const { searchText, onSearchTextChange } = this.props;
+    return (
+      <label htmlFor="search" data-testid="text-input-label">
+        Inclui o texto:
+        <input
+          id="search"
+          type="text"
+          value={ searchText }
+          onChange={ onSearchTextChange }
+          data-testid="text-input"
+        />
+      </label>
+    );
+  }
+
+  render() {
+    return (
+      <form data-testid="search-bar-form">
+        {this.renderTextInput()}
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
