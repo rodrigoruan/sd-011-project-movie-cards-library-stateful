77105e87615631235254f6f307ccbcf7a764cf3d
heyynat <natalifplima@gmail.com>
2021-05-31 17:17:09 -0400

    Requisito 2

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,7 +4,10 @@ class SearchBar extends React.Component {
     render() {
         const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
         return (
-            searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange
+            <fieldset>
+                <form data-testid="search-bar-form">     
+                </form>
+            </fieldset>            
         )
     }
 }
