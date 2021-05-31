e4eff573fda88cd0a80767f7c01311433460a73b
Tales Coelho <tales-coelho@live.com>
2021-05-31 16:56:37 -0300

    testando o lint

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,7 +1,27 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Label from './Label';
 
 class AddMovie extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  addState({ target }) {
+    const { name, value } = target;
+    this.setState(() => ({
+      [name]: value,
+    }));
+  }
+
   render() {
     const { onclick } = this.props;
     return (
@@ -13,12 +33,26 @@ class AddMovie extends Component {
           value=""
           idI="title-input"
         />
+        <Label
+          name="Subtítulo"
+          id="subtitle-input-label"
+          idLabel="subtitle-input-label"
+          value=""
+          idI="subtitle-input"
+        />
+        <Label
+          name="Imagem"
+          id="image-input-label"
+          idLabel="image-input-label"
+          value=""
+          idI="subtitle-input"
+        />
       </form>
     );
   }
 }
 
-AddMovie.proTypes = {
+AddMovie.propTypes = {
   onclick: PropTypes.func.isRequired,
 };
 
