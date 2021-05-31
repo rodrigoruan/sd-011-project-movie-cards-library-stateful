549b9b5fc8e37e948fc9cabb9e3695acd9ed4ae0
Gisele Costa <jikinha@gmail.com>
2021-05-31 17:33:05 -0300

    just for fun

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,40 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import SearchBar from './SearchBar';
+import MovieList from  './MovieList';
+import AddMovie from './AddMovie';
+
+const initialState = {
+  searchText: '',
+  bookmarkedOnly: false,
+  selectedGenre: '',
+  movies: [],
+};
+
+class Movielibrary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = initialState;
+  }
+
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        <SearchBar
+          searchText={ this.state.searchText }
+          bookmarkedOnly={ this.state.bookmarkedOnly }
+          selectedGenre={ this.state.selectedGenre }
+        />
+        <MovieList />
+        <AddMovie />
+      </div>
+    )
+  }
+}
+
+Movielibrary.propTypes = {
+  movies: PropTypes.arrayOf,
+};
+
+export default Movielibrary;
