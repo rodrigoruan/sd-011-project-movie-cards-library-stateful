0a98f96ef8129a9b6f21e7bce54f7e9db6bced98
AlexSmith7192 <alexsmith7192@gmail.com>
2021-05-31 18:36:12 -0300

    1 - Cria um componente chamado <SearchBar />

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,11 @@
-// implement AddMovie component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <h2>Simbora</h2>
+    );
+  }
+}
+
+export default SearchBar;
