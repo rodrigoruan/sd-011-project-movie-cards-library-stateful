c8c16b75c5c6e4630878eb672e66b58e7972b854
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-05-31 14:41:36 -0300

    Adiciona requisito 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,27 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
+
+    return (
+      <form >
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
