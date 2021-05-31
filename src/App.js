8ef41be880d2ff81c19a36b5fe9f5354c1bce996
Lucas Maestrelli <lucasmaestrelli95@gmail.com>
2021-05-31 18:02:53 -0300

    render addmovie

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import './App.css';
 import MovieList from './components/MovieList';
 import filmes from './data';
 import SearchBar from './components/SearchBar';
+import AddMovie from './components/AddMovie';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <SearchBar />
-
       <MovieList className="movie-list" movies={ filmes } />
+      <AddMovie />
     </div>
   );
 }
