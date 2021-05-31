13a589d3f5cb6237195539c49686bd1fb8442726
Thyago Pessoa <flash_thyago@hotmail.com>
2021-05-31 18:35:44 -0300

    Desafio 6, 7, 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,56 @@
 // implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const { onClick } = this.props;
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  render() {
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.props;
+    return (
+      <form data-testid="add-movie-form">
+        <label data-testid="title-input-label" htmlFor="title-input">
+          <input
+            data-testid="title-input"
+            value={ title }
+            onChange={ this.handleChange }
+          />
+          Título
+        </label>
+      </form>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  subtitle: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  imagePath: PropTypes.string.isRequired,
+  storyline: PropTypes.string.isRequired,
+  rating: PropTypes.string.isRequired,
+  genre: PropTypes.string.isRequired,
+};
+
+export default AddMovie;
