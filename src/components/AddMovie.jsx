42236103464efc0566995731a73c2484eb7540a8
LeoFuna <leo.funa@gmail.com>
2021-05-31 14:35:36 -0300

    Iniciando projeto

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,36 @@
 // implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyLine: '',
+      rating: 0,
+      genre: 'action',
+    }
+  }
+
+
+  render() {
+    const { onClick } = this.props;
+
+    // updateTitle() {
+    //   this.setState = {
+    //     title: 
+    //   }
+    // }
+
+    return (
+      <form data-testid="add-movie-form">
+        <label data-testid="title-input-label">Título</label>
+        <input data-testid="title-input" type="text" value={ this.state.title } onChange="" />
+      </form>
+    )
+  }
+}
+
+export default AddMovie;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,25 @@
 // implement AddMovie component here
+import React from 'react'
+
+class SearchBar extends React.Component {
+  render() {
+    const { searchText, onSearchTextChange, bookmarkedOnly, selectedGenre, onSelectedGenreChange, onBookMarkedChange } = this.props
+    return (
+      <form data-testid="search-bar-form">
+        <label>Inclui o texto:</label>
+        <input data-testid="text-input" type="text" value={ searchText } onChange={ onSearchTextChange } />
+        <label data-testid="checkbox-input-label">Mostrar somente favoritos</label>
+        <input data-testid="checkbox-input" type="checkbox" checked={ bookmarkedOnly } onChange={ onBookMarkedChange } />
+        <label data-testid="select-input-label">Filtrar por gênero</label>
+        <select data-testid="select-input" value={ selectedGenre } onChange={ onSelectedGenreChange }>
+          <option data-testid="select-option" value="">Todos</option>
+          <option data-testid="select-option" value="action">Ação</option>
+          <option data-testid="select-option" value="comedy">Comédia</option>
+          <option data-testid="select-option" value="thriller">Suspense</option>
+        </select>
+      </form>
+    )
+  }
+}
+
+export default SearchBar
