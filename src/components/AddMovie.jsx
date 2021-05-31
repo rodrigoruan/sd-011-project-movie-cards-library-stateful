acc9c7401ebb4f2ccec5e46d73d5b42b6e2c5f2c
Julio Filizzola <juliofilizzola@hotmail.com>
2021-05-31 15:06:46 -0300

    [Julio Filizzola] - add constructor e this.state

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
 
 export default class AddMovie extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+  
   render() {
     return (
       <div>
