b6049c4bbe034b93845a2059fed374cff09205cb
marcoswarmling <marcoswarmling15@gmail.com>
2021-05-31 15:14:04 -0300

    vamo com tudo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
+import MovieLobrary from './MovieLibrary'
 
 function App() {
   return (
     <div className="App">
+<MovieLibrary />
       <Header />
     </div>
   );
