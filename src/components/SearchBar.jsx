9dafd46b6651d1a7734650a08d3f73c465f304b9
Daniel Leon <daniel.radialista@gmail.com>
2021-05-31 13:37:35 -0300

    iniciando o projeto, requisito 1 ok, #vqv :rocket

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,13 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <div>
+        Search Bar
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
