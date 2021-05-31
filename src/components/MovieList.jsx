8d759b2cb6cbd1a3fa79ae3a965943b36c621761
Diego Ribeiro <dihrm2001@gmail.com>
2021-05-31 15:11:10 -0300

    Requisitos 1 e 2

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,13 +2,46 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import MovieCard from './MovieCard';
+import SearchBar from './SearchBar';
 
 class MovieList extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+    };
+
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+  }
+
+  onSearchTextChange() {
+  }
+
+  onBookmarkedChange() {
+  }
+
+  onSelectedGenreChange() {
+  }
+
   render() {
     const { movies } = this.props;
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
 
     return (
       <div data-testid="movie-list" className="movie-list">
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ this.onSearchTextChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.onBookmarkedChange }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.onSelectedGenreChange }
+        />
         { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
       </div>
     );
