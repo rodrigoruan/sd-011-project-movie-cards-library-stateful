50b02dccc4cc7d6063d94f72e79bafdeb6abd3e3
guinhoanjos <75916978+guinhoanjos@users.noreply.github.com>
2021-05-31 19:28:42 -0300

    requisito 8

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
-import AddMovie from './components/AddMovie'
+// import AddMovie from './components/AddMovie'
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <AddMovie />
     </div>
   );
 }
--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -14,17 +14,20 @@ class AddMovie extends React.Component {
 		this.addMovieFunction = this.addMovieFunction.bind;
 	}
 
-	addMovieFunction(event) {
-		this.setState(()=> {
-			return {title: event.target.value};
-		});
-	}
+	// addMovieFunction(event) {
+	// 	const { id } = event.target;
+	// 	this.setState(()=> {
+	// 		return {[id]: event.target.value};
+	// 	});
+	// }
 
   render() {
 		return (
 			<form data-testid="add-movie-form">
-				<label  data-testid="title-input-label" for="title-input-label">Título</label>
-				<input type="text" data-testid="title-input" onChange={this.addMovieFunctio} name="title-input-label"></input>
+				<label data-testid="title-input-label" for="title">Título</label>
+				<input type="text" data-testid="title-input" onChange={ this.addMovieFunction } id="title"></input>
+				<label data-testid="subtitle-input-label" for="subtitle">Subtítulo</label>
+				<input type="text" data-testid="subtitle-input" id="subtitle" onChange={ this.addMovieFunction }></input>
 
 			</form>
 		)
