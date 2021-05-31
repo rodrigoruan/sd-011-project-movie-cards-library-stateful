030ab5809e41dab09563e0a3147fce17e209c1c3
Julio Filizzola <juliofilizzola@hotmail.com>
2021-05-31 15:14:21 -0300

    [Julio Filizzola] - refaturando

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -26,7 +26,13 @@ export default class AddMovie extends Component {
           <input type="text" name="subtitle" value={this.state.subtitle} />
         </label>
         <label data-testid="image-input-label">
-          <input type="file" name="Imagem" value={this.state.imagePath} data-testid="image-input" onChange={this.handleChangerTitle} data-testid="subtitle-input"/>
+          <input type="file" name="Imagem" value={ this.state.imagePath } data-testid="image-input" onChange={ this.handleChangerTitle } data-testid="subtitle-input" />
+        </label>
+        <label data-testid="title-input-label">
+          <textarea name='sinopse' value={ this.state.storyline } data-testid="storyline-input" />
+        </label>
+        <label data-testid="title-input-label">
+          <textarea name='sinopse' value={ this.state.storyline } data-testid="storyline-input" />
         </label>
       </form>
     )
