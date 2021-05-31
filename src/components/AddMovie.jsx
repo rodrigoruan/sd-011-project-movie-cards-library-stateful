e6604c88e93cb8f4a935bba228c7bef10dd18264
guinhoanjos <75916978+guinhoanjos@users.noreply.github.com>
2021-05-31 19:05:10 -0300

    requisito 8 feito

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -11,11 +11,20 @@ class AddMovie extends React.Component {
 			rating: 0,
 			genre: 'action',
 		}
+		this.addMovieFunction = this.addMovieFunction.bind;
 	}
+
+	addMovieFunction(event) {
+		this.setState(()=> {
+			return {title: event.target.value};
+		});
+	}
+
   render() {
-		const { callback } = this.props;
 		return (
 			<form data-testid="add-movie-form">
+				<label  data-testid="title-input-label" for="title-input-label">Título</label>
+				<input type="text" data-testid="title-input" onChange={this.addMovieFunctio} name="title-input-label"></input>
 
 			</form>
 		)
