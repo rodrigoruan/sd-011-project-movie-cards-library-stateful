90efaf6e1ecd8646d5bfd777433471da2a023269
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-05-31 15:19:20 -0300

    Parte do SearchBar completa

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -28,14 +28,13 @@ export class SearchBar extends Component {
           Filtrar por gênero
           <select
             data-testid="select-input"
-            options={ options }
             onChange={ onSelectedGenreChange }
             value={ selectedGenre }
           >
             <option data-testid="select-option" value="">Todos</option>
             <option data-testid="select-option" value="action">Ação</option>
             <option data-testid="select-option" value="comedy">Comédia</option>
-            <option data-testid="select-option" value="Suspense">Suspense</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
           </select>
         </label>
       </form>
