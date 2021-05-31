e19ee3a2b8588948cbc410f809e4c21fb6cb3243
Matheus Gaspar <msouzagaspar@gmail.com>
2021-05-31 16:25:13 -0300

    requisito 1: cria componente SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,9 @@
-// implement AddMovie component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return <p>oi</p>;
+  }
+}
+
+export default SearchBar;
