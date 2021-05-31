d4af2e7a04bc2aec412fc1ca57d71a45d9051691
Julio Cesar <julio.cesarcj1402@gmail.com>
2021-05-31 17:00:38 -0300

    alinhando os requisitos

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,23 +1,36 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 class SearchBar extends Component {
   render() {
+    // const {
+    //   searchText,
+    //   onSearchTextChange,
+    //   bookmarkedOnly,
+    //   onBookmarkedChange,
+    //   selectedGenre,
+    //   onSelectedGenreChange,
+    // } = this.props;
+
     return (
       <form>
         <div>
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="input">
+          <label name="search" htmlFor="input-filter">
             Inclui o texto:
+            <input type="text" data-testid="text-input" id="input-filter" />
           </label>
-          <input type="text" data-testid="text-input" id="input" />
         </div>
         <div>
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="checkbox">
+          <label name="favorite" htmlFor="checkbox">
             Mostrar somente favoritos
+            <input
+              onChange="onBookmarkedChange"
+              checked=""
+              type="checkbox"
+              id="checkbox"
+              data-testid="checkbox-input"
+            />
           </label>
-          <input onChange="onBookmarkedChange" checked="" type="checkbox" id="checkbox" data-testid="checkbox-input" />
         </div>
       </form>
     );
