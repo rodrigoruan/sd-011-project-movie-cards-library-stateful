43e5c5c8f17da0b69e2545810d1fb5d23dcef036
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-05-31 16:30:11 -0300

    exercicio 5 resolvido

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -9,18 +9,35 @@ class SearchBar extends Component {
         <label data-testid="text-input-label">
           Inclui o texto:
           <input
+            type="text"
             data-testid="text-input"
             value={ searchText }
             onChange={ onSearchTextChange }
           />
         </label>
+        <br />
         <label data-testid="checkbox-input-label">
           Mostrar somente favoritos
           <input
+            type="checkbox"
             data-testid="checkbox-input"
             checked={ bookmarkedOnly }
             onChange={ onBookmarkedChange }
           />
+        </label>
+        <br />
+        <label data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+            data-testid="select-input"
+          >
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
         </label>
       </form>
     );
