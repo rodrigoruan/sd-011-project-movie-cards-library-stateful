0117da62fc8a02249d283386f763ecc974e2dc02
marsleite <marsleite@gmail.com>
2021-05-31 15:28:16 -0300

    Iniciando projeto movie cards library-stateful

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
 import './App.css';
+import movies from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="movie-list">
+        <MovieList movies={ movies } />
+      </div>
     </div>
   );
 }
