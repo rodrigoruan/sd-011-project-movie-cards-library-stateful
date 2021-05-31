320e09da062509c6f829b3b6e0491b28f05b298d
Hugo Mouto <hmoutol@gmail.com>
2021-05-31 18:39:41 -0300

    feat: Critérios até o 10

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -4,14 +4,70 @@ export default class AddMovie extends Component {
   constructor() {
     super();
     this.state = {
-
+      title: '',
+      subtitle: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
     };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value });
   }
 
   render() {
     return (
       <div>
-        {/* test */}
+        <form data-testid="add-movie-form" action="">
+          <label data-testid="title-input-label" htmlFor="title-add">
+            Título
+            <input
+              data-testid="title-input"
+              id="title-add"
+              type="text"
+              name="title"
+              value={ this.state.title }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label data-testid="subtitle-input-label" htmlFor="subtitle-add">
+            Subtítulo
+            <input
+              data-testid="subtitle-input"
+              id="subtitle-add"
+              type="text"
+              name="subtitle"
+              value={ this.state.subtitle }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label data-testid="image-input-label" htmlFor="image-add">
+            Imagem
+            <input
+              data-testid="image-input"
+              id="image-add"
+              type="text"
+              name="imagePath"
+              value={ this.state.imagePath }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label data-testid="storyline-input-label" htmlFor="storyline-add">
+            storylinem
+            <textarea
+              data-testid="storyline-input"
+              id="storyline-add"
+              type="text"
+              name="storylinePath"
+              value={ this.state.imagePath }
+              onChange={ this.handleChange }
+            />
+          </label>
+        </form>
       </div>
     );
   }
