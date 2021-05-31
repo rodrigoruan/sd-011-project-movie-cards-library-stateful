1cf70e7f987fb78b4b3e10861253c0191977b548
rubia braga <rubiaemilly29@gmail.com>
2021-05-31 15:42:00 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,25 @@
 // implement AddMovie component here
+import React from 'react'
+
+class SearchBar extends React.Component {
+  render() {
+    const {{ search: searchText, 
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    }
+    } = this.props
+    return(
+      <form data-testid="search-bar-form">
+        <label data-testid="text-input-label"> Inclui o texto:
+          <input 
+          type='text'
+          onChange="onSearchTextChange"
+          ></input>
+        </label>
+      </form>
+    )
+  }
+}
