92e8157e9b4e290322532434611c109818d53d12
Bernardo Prado <bernardoprado22@yahoo.com.br>
2021-05-31 18:13:07 -0300

    Adiciona requisitos 6 e 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,14 @@
 // implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  render(){
+   const { onclick } = this.props;
+
+    return(
+     <form data-testid="add-movie-form"/>
+    );
+  }
+}
+
+export default AddMovie;
