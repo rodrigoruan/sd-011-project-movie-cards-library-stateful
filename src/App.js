c970c593adc9faccedabe5695fe61fcafc41c400
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-05-31 15:35:10 -0300

    Requisitos 1, 2 e 3 realizados - lint NOK

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
+import MovieList from './components/MovieList';
+import movies from './data';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieList movies={ movies } />
     </div>
   );
-};
+}
 
 export default App;
