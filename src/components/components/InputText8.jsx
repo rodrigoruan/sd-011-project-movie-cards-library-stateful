5f717b62ba825c91ba0714c8220d27534ee8610b
marlon307 <marlon-ramosb@hotmail.com>
2021-05-31 18:17:35 -0300

    6-7-8 update

---
+++ src/components/components/InputText8.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+// implement AddMovie component here
+class InputText8 extends Component {
+  render() {
+    const { srtTxt, srcOnCh } = this.props;
+    return (
+      <label htmlFor="id" data-testid="title-input-label">
+        Título
+        <input
+          id="id"
+          type="text"
+          value={ srtTxt }
+          onChange={ srcOnCh }
+          data-testid="title-input"
+        />
+      </label>
+    );
+  }
+}
+
+export default InputText8;
+
+InputText8.propTypes = {
+  srtTxt: PropTypes.string.isRequired,
+  srcOnCh: PropTypes.func.isRequired,
+};
