73e906213e51f5b4b162341fad011148f2645570
Alberto Candido <albertocandido98@gmail.com>
2021-05-31 15:47:49 -0300

    Finaliza requisitos 6 ao 9

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -14,13 +14,26 @@ export default class AddMovie extends Component {
       rating: 0,
       genre: 'action'
     }
+    this.handleState = this.handleState.bind(this)
+  }
+
+  handleState({target}) {
+    const {value, name} = target;
+    this.setState({
+      [name]: value,
+    })
   }
 
   render() {
     const {onClick} = this.props
     return (
       <form data-testid="add-movie-form">
-
+        <label htmlFor="title-input" data-testid="title-input-label">Título
+          <input type="text" value={this.state.title} data-testid="title-input" id="title-input" name="title" onChange={this.handleState}/>
+        </label>
+        <label data-testid="subtitle-input-label" htmlFor="subtitle-input">Subtítulo
+          <input type="text" value={this.state.subtitle} data-testid="subtitle-input" id="subtitle-input" name="subtitle" onChange={this.handleState}></input>
+        </label>
       </form>
     )
   }
