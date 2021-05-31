e507b05cd205be8bb241bb17823523fe8a3ba4d2
Marco Galindo <marco1408@gmail.com>
2021-05-31 15:12:05 -0300

    adicionados todos os inputs do searchbar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -11,7 +11,23 @@ class SearchBar extends Component {
   render() {
     return (
       <form action="" data-testid="search-bar-form">
-        <input type="text" name="" id="" data-testid="text-input-label" />
+        <label htmlFor="search-text" data-testid="text-input-label">
+          Inclui o texto:
+          <input type="text" name="search-text" data-testid="text-input" />
+        </label>
+        <label htmlFor="favoritos" data-testid="checkbox-input-label">
+          Mostrar somente favoritos:
+          <input type="checkbox" name="favoritos" data-testid="checkbox-input" />
+        </label>
+        <label htmlFor="gender">
+          Filtrar por gênero:
+          <select name="gender" data-testid="select-input">
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
+        </label>
       </form>
     );
   }
