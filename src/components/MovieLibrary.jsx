933298856718a40916b54818e365d1a7ef77070a
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-05-31 15:05:01 -0300

    Adiciona requisito 16

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
 
 class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: props.movies,
+    };
+  }
+
   render() {
     return (
       <div>
