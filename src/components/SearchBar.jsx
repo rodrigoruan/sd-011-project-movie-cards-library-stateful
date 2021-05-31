12517fd2ac9f5bbeab70bf238676534ac8f0d4a9
Oryange <oryangestrifezze@gmail.com>
2021-05-31 15:25:46 -0300

    Adiciona requisitos de 1 a 4

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,29 @@
 // implement AddMovie component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    const { searchText, onSearchTextChange,
+      bookmarkedOnly, onBookmarkedChange,
+      selectedGenre, onSelectedGenreChange } = this.props;
+    return (
+      <form data-testid="search-bar-form">
+        <label data-testid="text-input-label">
+          Inclui o texto:
+          <input
+            type="text"
+            onChange={ onSearchTextChange }
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
+// searchText, uma string
+// onSearchTextChange, uma callback
+// bookmarkedOnly, um boolean
+// onBookmarkedChange, uma callback
+// selectedGenre, uma string
+// onSelectedGenreChange, uma callback
