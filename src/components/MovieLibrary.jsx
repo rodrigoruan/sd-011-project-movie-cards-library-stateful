d88b4e0b4ce7c1ad0fdea2b4fc3efc834fdfddff
Hamander <1hamander@gmail.com>
2021-06-01 00:09:09 -0300

    Agora sim

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import movies from '../data';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 
 export default class MovieLibrary extends Component {
-  constructor() {
-    super();
-    this.state = {
-      searchText: '',
-      bookmarkedOnly: false,
-      selectedGenre: '',
-      movies: movies,
-    };
-  }
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     searchText: '',
+  //     bookmarkedOnly: false,
+  //     selectedGenre: '',
+  //     movies: movies,
+  //   };
+  // }
 
   render() {
     return (
