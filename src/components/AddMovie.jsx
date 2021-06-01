14d27c21d0ed040525a2b3d12e532f352d92c578
Antonio Malato <malatoneto@gmail.com>
2021-06-01 15:42:20 -0300

    Requisito 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -15,7 +15,14 @@ class AddMovie extends React.Component {
   }
 
   render() {
-    return <form data-testid="add-movie-form" />;
+    return (
+      <form data-testid="add-movie-form">
+        <label htmlFor="title-input-label" data-testid="title-input-label">
+          Título
+          <input type="text" data-testid="title-input" />
+        </label>
+      </form>
+    );
   }
 }
 
