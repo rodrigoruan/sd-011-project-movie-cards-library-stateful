2cac1957c1726b7b3c7d8342beffaa7108e1f492
Gustavo Mendes <gusm217@gmail.com>
2021-06-01 18:56:35 -0300

    Adiciona o 8 e 9

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -14,19 +14,40 @@ class AddMovie extends React.Component {
       rating: 0,
       genre: 'action',
     }
+
+    this.handleChange = this.handleChange.bind(this);
   }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    console.log(target.value)
+    this.setState({
+      [name]:  value
+    })
+  }
+
   render() {
     const { onClick } = this.props;
 
     return(
       <div>
-        <form data-testid="add-movie-form"></form>
+        <form data-testid="add-movie-form">
+          <label data-testid="title-input-label">
+            Título
+            <input data-testid="title-input" value={this.state.title} name="title" type="text" onChange={this.handleChange} />
+          </label>
+          <label data-testid="subtitle-input-label">
+            Subtítulo
+            <input data-testid="subtitle-input" name="subtitle" type="text" value={this.state.subtitle} onChange={this.handleChange
+            }/>
+          </label>
+        </form>
       </div>
     )
   }
 }
 
-AddMovie.PropTypes = {
+AddMovie.propTypes = {
   onClick: PropTypes.func,
 }
 
