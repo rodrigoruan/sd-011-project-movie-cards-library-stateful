bb7e70e54c9116d40b1134db0e8863bc26263fc2
Gustavo Lemes <gustavolemes997@gmail.com>
2021-06-01 14:37:32 -0300

    Requisito 9

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -3,11 +3,15 @@ import React, { Component } from 'react';
 
 export default class AddMovie extends Component {
   render() {
-    const { title } =this.props;
+    const { title, subtitle } =this.props;
     return (
       <form data-testid="add-movie-form" >
         <label data-testid="title-input-label" htmlFor="">Título
         <input data-testid="title-input" type="text" value={title} />
+        </label>
+
+        <label data-testid="subtitle-input-label" htmlFor="">Subtítulo
+        <input data-testid="subtitle-input" type="text" value={subtitle} />
         </label>
       </form>
     );
