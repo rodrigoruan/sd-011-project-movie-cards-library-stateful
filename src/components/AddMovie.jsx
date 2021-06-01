4e9b2be9c9887305575c17711dbea3dd18c30c27
josanjohnata <josanjohnata@gmail.com>
2021-05-31 21:23:22 -0300

    Adiciona requisito 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      title: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+  
   render() {
+    const { title } = this.state;
+
     return (
-      <form data-testid="add-movie-form" />
+      <form data-testid="add-movie-form">
+        <label data-testid="title-input-label">
+          Título
+          <input
+            type="text"
+            value={ title }
+            data-testid="title-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+      </form>
     );
   }
 }
