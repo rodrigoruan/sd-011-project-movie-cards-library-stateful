b1a688332a3c30ad83bb3738149b04da7926693a
guinhoanjos <75916978+guinhoanjos@users.noreply.github.com>
2021-06-01 14:21:55 -0300

    consertando lint

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,52 +1,51 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			subtitle: '',
-			title: '',
-			imagePath: '',
-			storyline: '',
-			rating: 0,
-			genre: 'action',
-		}
-		this.addMovieFunction = this.addMovieFunction.bind(this);
-	}
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+    this.addMovieFunction = this.addMovieFunction.bind(this);
+  }
 
-	addMovieFunction(event) {
-		const { id, value } = event.target;
-		this.setState(()=> {
-			return {[id]: value};
-		});
-	}
+  addMovieFunction(event) {
+    const { id, value } = event.target;
+    this.setState(() => {
+      return { [id]: value };
+    );
+  }
 
   render() {
-		return (
-			<form data-testid="add-movie-form">
-				<label data-testid="title-input-label" for="title">
-					Título
-			    <input 
-					type="text" 
-					data-testid="title-input" 
-					onChange={ this.addMovieFunction } 
-					id="title"
-					value={ this.state.title }
+    return (
+      <form data-testid="add-movie-form">
+        <label data-testid="title-input-label" for="title">
+          Título
+          <input
+            type="text"
+            data-testid="title-input"
+            onChange={ this.addMovieFunction }
+            id="title"
+            value={ this.state.title }
 					/>
-				</label>
-				<label data-testid="subtitle-input-label" for="subtitle">
-					Subtítulo
-					<input type="text" 
-					data-testid="subtitle-input" 
-					id="subtitle" 
-					onChange={ this.addMovieFunction }
-					value={ this.state.subtitle }
-					/>
-				</label>
-				
-			</form>
-		)
-	}
+        </label>
+        <label data-testid="subtitle-input-label" for="subtitle">
+          Subtítulo
+          <input type="text"
+            data-testid="subtitle-input"
+            id="subtitle"
+            onChange={ this.addMovieFunction }
+            value={ this.state.subtitle }
+          />
+        </label>
+      </form>
+    )
+  }
 }
 
 export default AddMovie;
