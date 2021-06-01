e004c35b813e82f335eb594287bcb7a0479d3a70
Savio Moraes <smmsavio@hotmail.com>
2021-06-01 12:51:37 -0300

    inicia resoluçao do requisito 08

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,16 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class AddMovie extends React.Component {
   // constructor() {
   //   super();
-  //   this.handleAddMovie = this.handleAddMovie.bind(this);
+  //   this.handleTitle = this.handleTitle.bind(this);
   // }
 
   render() {
+    const { handleTitle } = this.props;
     return (
-      <form data-testid="add-movie-form" />
+      <form data-testid="add-movie-form">
+        <label htmlFor="input-title" data-testid="title-input-label">
+          Título
+          <input
+            type="text"
+            name=""
+            id=""
+            data-testid="title-input"
+            onChange={ handleTitle }
+          />
+        </label>
+      </form>
     );
   }
 }
+
+AddMovie.propTypes = {
+  handleTitle: PropTypes.func.isRequired,
+};
 
 export default AddMovie;
