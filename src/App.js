3f40bf3a313e8aaa0353940ecd7d3b731219741d
Lucas Maestrelli <lucasmaestrelli95@gmail.com>
2021-06-01 15:23:09 -0300

    export t

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import MovieList from './components/MovieList';
 import filmes from './data';
 import SearchBar from './components/SearchBar';
 import AddMovie from './components/AddMovie';
+import MovieLibrary from './components/MovieLibrary';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <SearchBar />
+      <MovieLibrary />
       <MovieList className="movie-list" movies={ filmes } />
       <AddMovie />
     </div>
