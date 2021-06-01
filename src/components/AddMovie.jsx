55f792c8f0f1409dffae7bf79d41352c1821bf9b
Bruno Duarte <brunoarduarte@gmail.com>
2021-06-01 10:11:21 -0300

    Requisito 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,31 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import FisrtPart from './FisrtPart';
+import SecondPart from './SecondPart';
+
+class AddMovie extends Component {
+  constructor() {
+    super();
+    this.state = {
+      // subtitle: '',
+      // title: '',
+      // imagePath: '',
+      // storyline: '',
+      // rating: 0,
+      // genre: 'action',
+    };
+  }
+
+  render() {
+    return (
+      <form>
+        <FisrtPart />
+        <SecondPart />
+      </form>
+    );
+  }
+}
+
+AddMovie.propTypes = PropTypes.Component;
+
+export default AddMovie;
