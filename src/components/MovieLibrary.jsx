3e1685ad37ef940922dca6748309bf3370525d8e
Gustavo Lemes <gustavolemes997@gmail.com>
2021-06-01 16:19:43 -0300

    Requisito 17

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,10 +1,27 @@
 // implement AddMovie component here
 import React, { Component } from 'react';
+import SearchBar from './SearchBar';
+
+const initialState = {
+  searchText: '',
+  bookmarkedOnly: false,
+  selectedGenre: '',
+}
 
 export default class MovieLibrary extends Component {
+  constructor() {
+    super();
+    
+    this.state = initialState;
+  }
+
   render() {
+    const { movies } = this.props;
+
     return (
-      <div> </div>
+      <div>
+        <SearchBar /> 
+      </div>
     );
   }
 }
