e4f4977522e520d85c8a8505ae509197e09db574
Arthur Hermann <arthurhermannc@gmail.com>
2021-06-01 13:34:46 -0300

    ajustes lint AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -8,16 +8,44 @@ import Select from './Select';
 
 class AddMovie extends Component {
   render() {
-    const { func, title, subtitle, imagePath, storyline, rating, genre, onClick } = this.props;
+    const { func, title, subtitle, imagePath,
+      storyline, rating, genre, onClick } = this.props;
     return (
       <form data-testid="add-movie-form">
-        <Input handler={ func } text="Título" dataLabel="title-input-label" dataInput="title-input" value={ title } name="title" />
-        <Input handler={ func } text="Subtítulo" dataLabel="subtitle-input-label" dataInput="title-input" value={ subtitle } name="subtitle" />
-        <Input handler={ func } text="Imagem" dataLabel="image-input-label" dataInput="image-input" value={ imagePath } name="imagePath" />
+        <Input
+          handler={ func }
+          text="Título"
+          dataLabel="title-input-label"
+          dataInput="title-input"
+          value={ title }
+          name="title"
+        />
+        <Input
+          handler={ func }
+          text="Subtítulo"
+          dataLabel="subtitle-input-label"
+          dataInput="title-input"
+          value={ subtitle }
+          name="subtitle"
+        />
+        <Input
+          handler={ func }
+          text="Imagem"
+          dataLabel="image-input-label"
+          dataInput="image-input"
+          value={ imagePath }
+          name="imagePath"
+        />
         <TextArea value={ storyline } onChange={ func } />
         <Rating handler={ func } value={ rating } />
         <Select handler={ func } value={ genre } />
-        <button type="reset" data-testid="send-button" onClick={ onClick }>Adicionar filme</button>
+        <button
+          type="reset"
+          data-testid="send-button"
+          onClick={ onClick }
+        >
+          Adicionar filme
+        </button>
       </form>
     );
   }
