6e87c60f44bb0eeb81292c5c6afeb2be8967dc85
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-06-01 12:30:24 -0300

    Add Req 6

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
+import AddMovie from './components/AddMovie';
 
-function App() {
+function App(onClick) {
+  console.log(onClick);
   return (
     <div className="App">
       <Header />
+      <AddMovie />
     </div>
   );
 }
--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,31 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
+    return (
+      <form action="" data-testid="add-movie-form">
+        <div>{ title }</div>
+        <div>{ subtitle }</div>
+        <div>{ imagePath }</div>
+        <div>{ storyline }</div>
+        <div>{ rating }</div>
+        <div>{ genre }</div>
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
