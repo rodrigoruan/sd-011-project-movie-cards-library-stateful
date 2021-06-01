c7066f9a580c8c12e735100edd0ec9caa86c9ade
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-06-01 16:24:58 -0300

    Requisitos 6, 7 e 8 - componente AddMovie, form e input text

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,37 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleChangeTitle = this.handleChangeTitle.bind(this);
+  }
+
+  handleChangeTitle() {
+
+  }
+
+  render() {
+    return (
+      <form data-testid="add-movie-form">
+        <label htmlFor="title-input" data-testid="title-input-label">
+          Título
+          <input
+            type="text"
+            data-testid="title-input"
+            onChange={ this.handleChangeTitle }
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  state: PropTypes.shape({
+    title: PropTypes.string,
+  }).isRequired,
+};
+
+export default AddMovie;
