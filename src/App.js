87849ae5e6e8489f8b4d51852a40040330a5c0a3
Vinicius <viniciusfod@gmail.com>
2021-06-01 15:07:18 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
+import MovieCard from './components/MovieCard';
 
 function App() {
   return (
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,8 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+  render() {
+    const { ...props } = this.props;
+    return <input { ...props } />;
+  }
+}
