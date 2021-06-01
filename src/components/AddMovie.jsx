3e36a3a4964cb61b727532c86bb57e1e2fff2285
josanjohnata <josanjohnata@gmail.com>
2021-05-31 21:51:34 -0300

    Adiciona ID

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -30,6 +30,8 @@ class AddMovie extends React.Component {
             value={ title }
             data-testid="title-input"
             onChange={ this.handleChange }
+            id="addMovieTitleInput"
+            name="title"
           />
         </label>
         <label data-testid="subtitle-input-label" htmlFor="addMovieSubtitleInput">
@@ -39,6 +41,8 @@ class AddMovie extends React.Component {
             value={ subtitle }
             data-testid="subtitle-input"
             onChange={ this.handleChange }
+            id="addMovieSubtitleInput"
+            name="subtitle"
           />
         </label>
       </form>
