b25f08f06b11dae72a9974818fc7dc637b383684
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-06-01 14:56:12 -0300

    SearchBar added

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,21 @@
 // implement AddMovie component here
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class SearchBar extends React.Component {
+  render() {
+    const { searchText } = this.props;
+
+    return (
+    <div>
+      <input type="text" value={ searchText } data-testid="text-input"></input>
+    </div>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+};
+
+export default SearchBar;
