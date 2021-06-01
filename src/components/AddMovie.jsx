47d9064b83468ebee4770581886654c09838964a
Lucas Maestrelli <lucasmaestrelli95@gmail.com>
2021-06-01 14:43:49 -0300

    descontruindo labels

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,7 +1,9 @@
 // implement AddMovie component here
 import React from 'react';
 import PropTypes from 'prop-types';
+import LabelInputs from './LabelInputs';
 import Ratinginput from './RatingInput';
+import ButtonAddMovie from './ButtonAddMovie';
 
 const state0 = {
   subtitle: '',
@@ -15,41 +17,48 @@ class AddMovie extends React.Component {
   constructor() {
     super();
     this.state = state0;
+    this.changeHandler = this.changeHandler.bind(this);
+  }
+
+  changeHandler({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
   }
 
   render() {
     const { onClick } = this.props;
     return (
       <form method={ onClick } data-testid="add-movie-form">
-        <label htmlFor="TitleInput" data-testid="title-input-label">
-          Título
-          <input
-            id="TitleInput"
-            type="text"
-            data-testid="title-input"
-          />
-        </label>
-        <label htmlFor="SubtitleInput" data-testid="subtitle-input-label">
-          Subtítulo
-          <input
-            id="SubtitleInput"
-            type="text"
-            data-testid="subtitle-input"
-          />
-        </label>
-        <label htmlFor="ImagePathInput" data-testid="image-input-label">
-          Imagem
-          <input
-            id="ImagePathInput"
-            type="text"
-            data-testid="image-input"
-          />
-        </label>
+        <LabelInputs
+          labelInfo={ { labelId: 'title-input-label', labelText: 'Título' } }
+          rInfo={ {
+            rId: 'title-input', rValue: title, rName: 'title',
+          } }
+          funcOnChange={ this.changeHandler }
+        />
+
+        <LabelInputs
+          labelInfo={ { labelId: 'subtitle-input-label', labelText: 'Subtítulo' } }
+          rInfo={ {
+            rId: 'subtitle-input', rValue: subtitle, rName: 'subtitle',
+          } }
+          funcOnChange={ this.changeHandler }
+        />
+
+        <LabelInputs
+          labelInfo={ { labelId: 'image-input-label', labelText: 'Imagem' } }
+          rInfo={ {
+            rId: 'image-input', rValue: imagePath, rName: 'imagePath',
+          } }
+          funcOnChange={ this.changeHandler }
+        />
         <label htmlFor="StorylineInput" data-testid="storyline-input-label">
           Sinopse
           <textarea id="StorylineInput" data-testid="storyline-input" />
         </label>
-        <Ratinginput rValue={ rating } funcOnChange={ this.changeHandler }/>
+        <Ratinginput rValue={ rating } funcOnChange={ this.changeHandler } />
         <label htmlFor="GenreInput" data-testid="genre-input-label">
           Gênero
           <select id="GenreInput" data-testid="genre-input">
@@ -58,7 +67,7 @@ class AddMovie extends React.Component {
             <option value="thriller" data-testid="genre-option">Suspense</option>
           </select>
         </label>
-        <ButtonAddMovie addOnClick={ this.submit }/>
+        <ButtonAddMovie addOnClick={ this.submit } />
       </form>
     );
   }
