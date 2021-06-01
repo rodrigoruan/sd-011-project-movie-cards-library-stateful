c13b09daf1961cc06902079cb695483bbbb7947b
Rodolfo <rodolforrcont@gmail.com>
2021-05-31 21:59:19 -0300

    PropTypes

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import LabelInput from './LabelInput';
 import LabelSelect from './LabelSelect';
 import LabelTextArea from './LabelTextArea';
