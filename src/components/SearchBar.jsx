42d3ec49842ec9626bea7e16550eb796144d11e9
Vinicius <viniciusfod@gmail.com>
2021-06-01 15:08:48 -0300

    requisito 2

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,6 +3,6 @@ import React, { Component } from 'react';
 export default class SearchBar extends Component {
   render() {
     const { ...props } = this.props;
-    return <input { ...props } />;
+    return <form data-testid="search-bar-form" />;
   }
 }
