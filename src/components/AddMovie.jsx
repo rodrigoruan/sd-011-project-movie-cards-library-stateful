f5fdfbcc7d926cccc9db63d88943d01fe31bc31b
Luan Xavier <luantenorio3@gmail.com>
2021-06-01 18:56:22 -0300

    Requisito 14

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,5 +1,6 @@
 // implement AddMovie component here
 import React from 'react';
+import PropTypes from 'prop-types';
 import Title from './TitleComponent';
 import Subtitle from './SubtitleComponent';
 import Image from './ImageComponent';
@@ -21,6 +22,7 @@ class AddMovie extends React.Component {
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this.resetAllStates = this.resetAllStates.bind(this);
   }
 
   handleChange({ target }) {
@@ -31,7 +33,19 @@ class AddMovie extends React.Component {
     });
   }
 
+  resetAllStates() {
+    this.setState({
+      title: '',
+      subtitle: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: '',
+    });
+  }
+
   render() {
+    const { onClick } = this.props;
     const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
     return (
       <form data-testid="add-movie-form">
@@ -41,9 +55,23 @@ class AddMovie extends React.Component {
         <TextArea value={ storyline } handleChange={ this.handleChange } />
         <Rating value={ rating } handleChange={ this.handleChange } />
         <Select value={ genre } handleChange={ this.handleChange } />
+        <button
+          type="button"
+          data-testid="send-button"
+          onClick={ () => {
+            this.resetAllStates();
+            onClick();
+          } }
+        >
+          Adicionar filme
+        </button>
       </form>
     );
   }
 }
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
 
 export default AddMovie;
