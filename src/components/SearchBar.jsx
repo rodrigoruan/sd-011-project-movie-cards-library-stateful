caed2d9cbf5f17949851d0570b40072df0dd7676
benlopes <rubenssouss@hotmail.com>
2021-06-01 10:51:58 -0300

    Render 4 options select within the search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -10,11 +10,32 @@ export default class SearchBar extends Component {
             <input type='text' data-testid="text-input" value={this.props.searchText}
             onChange={this.props.onSearchTextChange} />
           </label>
+
           <label data-testid="checkbox-input-label"> "Mostrar somente favoritos"
             <input type='checkbox' data-testid="checkbox-input" checked={this.props.bookmarkedOnly}
             onChange={this.props.onBookmarkedChange} />
           </label>
 
+          <label data-testid="select-input-label"> "Filtrar por gênero"
+            <select data-testid="select-input" value={this.props.selectedGenre}
+            onChange={this.props.onSelectedGenreChange} >
+              <option data-testid="select-option" value=''>
+                Todos
+              </option>
+              
+              <option data-testid="select-option" value='action'>
+                Ação
+              </option>
+
+              <option data-testid="select-option" value='comedy'>
+                Comédia
+              </option>
+
+              <option data-testid="select-option" value='thriller'>
+                Suspense
+              </option>
+            </select>
+          </label>
         </form>
       </div>
     )
