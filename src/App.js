bf0405831fd06e9914a8979b5d8e1ba11b6c2ee2
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-05-31 23:47:02 -0300

    Realizacao do desafio 15

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import Header from './components/Header';
 import './App.css';
 import SearchBar from './components/SearchBar';
 import AddMovie from './components/AddMovie';
+import MovieLibrary from './components/MovieLibrary';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <AddMovie />
-      <SearchBar />
+      {/* <MovieLibrary /> */}
     </div>
   );
 }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,24 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+import AddMovie from './AddMovie';
+
+class MovieLibrary extends Component {
+  // constructor() {
+  //   super();
+
+  // }
+
+  render() {
+    return (
+      <div>
+        {/* <SearchBar />
+        <MovieList />
+        <AddMovie /> */}
+      </div>
+    );
+  }
+
+}
+
+export default MovieLibrary;
