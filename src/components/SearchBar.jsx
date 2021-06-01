76cf5d572b29497ba7b3aef97ed2caa71b5b112f
guinhoanjos <75916978+guinhoanjos@users.noreply.github.com>
2021-06-01 14:01:56 -0300

    props validation do search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
@@ -10,7 +11,7 @@ class SearchBar extends React.Component {
       selectedGenre,
       onSelectedGenreChange,
     } = this.props;
-      
+
     return (
       <form data-testid="search-bar-form">
         <label data-testid="text-input-label">
@@ -41,5 +42,15 @@ class SearchBar extends React.Component {
     );
   }
 }
+
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired
+};
 
 export default SearchBar;
