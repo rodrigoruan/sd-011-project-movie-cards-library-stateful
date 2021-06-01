91fa673729085d31a2d05cb7d8d4328d101d5955
VictorS <czpask@gmail.com>
2021-06-01 12:13:36 -0300

    App lint corrigido

--- src/App.js
+++ src/App.js
@@ -4,12 +4,11 @@ import './App.css';
 import MovieLibrary from './components/MovieLibrary';
 import movies from './data';
 
-
 function App() {
   return (
     <div className="App">
       <Header />
-      <MovieLibrary movies={movies} />
+      <MovieLibrary movies={ movies } />
     </div>
   );
 }
