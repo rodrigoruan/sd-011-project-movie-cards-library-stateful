d310e41435a036aabef946ad122d1cd170bbf83d
josanjohnata <josanjohnata@gmail.com>
2021-06-01 16:01:32 -0300

    Refatorando Title

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Title from './Title'
 
 class AddMovie extends React.Component {
   constructor() {
@@ -28,17 +29,7 @@ class AddMovie extends React.Component {
     const { title, subtitle, imagePath, storyline, rating } = this.state;
     return (
       <form data-testid="add-movie-form">
-        <label data-testid="title-input-label" htmlFor="addMovieTitleInput">
-          Título
-          <input
-            type="text"
-            value={ title }
-            data-testid="title-input"
-            onChange={ this.handleChange }
-            id="addMovieTitleInput"
-            name="title"
-          />
-        </label>
+        <Title value={this.state.title} handleChange={ this.handleChange } />
         <label data-testid="subtitle-input-label" htmlFor="addMovieSubtitleInput">
           Subtítulo
           <input
---
+++ src/components/Title.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class Title extends React.Component {
+  render() {
+    const { value, handleChange } = this.props
+
+    return (
+      <label data-testid="title-input-label" htmlFor="addMovieTitleInput">
+        Título
+        <input
+          type="text"
+          value={ value }
+          data-testid="title-input"
+          onChange={ handleChange }
+          id="addMovieTitleInput"
+          name="title"
+        />
+      </label>
+    )
+  }
+}
+
+export default Title
