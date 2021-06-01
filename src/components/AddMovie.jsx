53c1227f2afe3c2dfd8d9248acbba23144df688c
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-06-01 09:20:39 -0300

    exercicio 13 resolvido, com bastante erros de identacao

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -23,59 +23,45 @@ class AddMovie extends Component {
 
   render() {
     const { onClick } = this.props;
-    const { title, subtitle, imagePath, storyLine } = this.state;
+    const { title, subtitle, imagePath, storyLine, rating, genre } = this.state;
     return (
       <form data-testid="add-movie-form">
         <label data-testid="title-input-label">
           Título
           <input
-            name="title"
-            data-testid="title-input"
-            type="text"
-            value={ title }
-            onChange={ this.changeValues() }
+            name="title" data-testid="title-input" type="text" value={ title } onChange={ this.changeValues() }
           />
-        </label>
-        <br />
+        </label> <br />
         <label data-testid="subtitle-input-label">
           Subtítulo
           <input
-            name="subtitle"
-            data-testid="subtitle-input"
-            type="text"
-            value={ subtitle }
-            onChange={ this.changeValues() }
+            name="subtitle" data-testid="subtitle-input" type="text" value={ subtitle } onChange={ this.changeValues() }
           />
-        </label>
-        <br />
+        </label> <br />
         <label data-testid="image-input-label">
           Imagem
           <input
-            name="imagePath"
-            data-testid="image-input"
-            type="text"
-            value={ imagePath }
-            onChange={ this.changeValues() }
+            name="imagePath" data-testid="image-input" type="text" value={ imagePath } onChange={ this.changeValues() }
           />
-        </label>
-        <br />
+        </label> <br />
         <label data-testid="storyline-input-label">
           Sinopse
           <textarea
-            name="storyLine"
-            data-testid="storyline-input"
-            value={ storyLine }
-            onChange={ this.changeValues() }
+            name="storyLine" data-testid="storyline-input" value={ storyLine } onChange={ this.changeValues() }
           />
-        </label>
+        </label> <br />
         <label data-testid="rating-input-label">
           Avaliação
           <input
-            name="rating"
-            data-testid="rating-input"
-            value={ rating }
-            onChange={ this.changeValues() }
+            type="number" name="rating" data-testid="rating-input" value={ rating } onChange={ this.changeValues() }
           />
+        </label> <br />
+        <label data-testid="genre-input-label">
+        <select data-testid="genre-input" value={ genre } onChange={ this.changeValues() }>
+          <option data-testid="genre-option" value="action">Ação</option>
+          <option data-testid="genre-option" value="comedy">Comédia</option>
+          <option data-testid="genre-option" value="thriller">Suspense</option>
+        </select>
         </label>
       </form>
     );
