5ce3546d566f81b9786d689bd79c71438582ee44
guinhoanjos <75916978+guinhoanjos@users.noreply.github.com>
2021-06-01 15:15:18 -0300

    lint

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -24,7 +24,7 @@ class AddMovie extends React.Component {
   render() {
     return (
       <form data-testid="add-movie-form">
-        <label data-testid="title-input-label" for="title">
+        <label data-testid="title-input-label" htmlFor="title">
           Título
           <input
             type="text"
@@ -32,11 +32,12 @@ class AddMovie extends React.Component {
             onChange={ this.addMovieFunction }
             id="title"
             value={ this.state.title }
-					/>
+          />
         </label>
-        <label data-testid="subtitle-input-label" for="subtitle">
+        <label data-testid="subtitle-input-label" htmlFor="subtitle">
           Subtítulo
-          <input type="text"
+          <input
+            type="text"
             data-testid="subtitle-input"
             id="subtitle"
             onChange={ this.addMovieFunction }
@@ -44,7 +45,7 @@ class AddMovie extends React.Component {
           />
         </label>
       </form>
-    )
+    );
   }
 }
 
