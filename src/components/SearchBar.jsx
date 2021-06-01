73d574cdaa1931624b553a8a9298f8bf16a9fe68
Gustavo Mendes <gusm217@gmail.com>
2021-06-01 16:33:53 -0300

    Adiciona o 3 e 4

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -12,23 +12,31 @@ class SearchBar extends React.Component {
       selectedGenre,
       onSelectedGenreChange 
     } = this.props;
+
     return(
       <div>
         <form data-testid="search-bar-form">
-
+          <label data-testid="text-input-label">Inclui o texto 
+            <input data-testid="text-input" type="text" value={searchText} onChange={onSearchTextChange}>            
+            </input>
+          </label>
+          <label data-testid="checkbox-input-label">Mostrar somente favoritos
+            <input data-testid="checkbox-input" type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange}/>
+          </label>
+          <label></label>
         </form>
       </div>
     )
   }
 }
 
-SearchBar.PropTypes = {
-  searchText: PropTypes.string,
-  onSearchTextChange: PropTypes.func,
-  bookmarkedOnly: PropTypes.bool,
-  onBookmarkedChange: PropTypes.func,
-  selectedGenre: PropTypes.string,
-  onSelectedGenreChange: PropTypes.func,
-}
+// SearchBar.PropTypes = {
+//   searchText: PropTypes.string,
+//   onSearchTextChange: PropTypes.func,
+//   bookmarkedOnly: PropTypes.bool,
+//   onBookmarkedChange: PropTypes.func,
+//   selectedGenre: PropTypes.string,
+//   onSelectedGenreChange: PropTypes.func,
+// }
 
 export default SearchBar;
