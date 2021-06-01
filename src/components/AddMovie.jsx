de92bbad5ba2db2991c9a4747941eff9c594b021
Matheus Gaspar <msouzagaspar@gmail.com>
2021-06-01 17:10:07 -0300

    requisito 8: renderiza input do tipo texto dentro do AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 class AddMovie extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
       subtitle: '',
       title: '',
@@ -12,12 +13,26 @@ class AddMovie extends React.Component {
       rating: 0,
       genre: 'action',
     };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    this.setState({
+      title: event.target.value,
+    });
   }
 
   render() {
     const { onClick } = this.props;
+    const { title } = this.state;
     return (
-      <form data-testid="add-movie-form" />
+      <form data-testid="add-movie-form">
+        <label data-testid="title-input-label" htmlFor="title-input">
+          Título
+          <input type="text" value={ title } onChange={ this.handleInputChange } data-testid="title-input" name="title-input" />
+        </label>
+      </form>
     );
   }
 }
