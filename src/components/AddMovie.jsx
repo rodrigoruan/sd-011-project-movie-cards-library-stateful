a6eefd285eae33cb3ee9a040f14691e122feee8d
Dario Junior <dario.junior.rs@gmail.com>
2021-06-01 17:19:50 -0300

    Create : AddMovie component

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,16 @@
-// implement AddMovie component here
+import React from 'react';
+
+export default class AddMovie extends React.Component {
+  render() {
+    return (
+      <div>
+        <form action="">
+          <label htmlFor="title">
+            Titulo:
+            <input type="text" name="title" id="title"/>
+          </label>
+        </form>
+      </div>
+    )
+  }
+}
