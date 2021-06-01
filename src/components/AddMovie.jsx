0013519e1b2117a493c842260ab15cfcb4e4baa2
Dario Junior <dario.junior.rs@gmail.com>
2021-06-01 17:26:06 -0300

    Add state in AddMovie component

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
 
 export default class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action'
+    }
+  }
   render() {
     return (
       <div>
@@ -8,6 +20,26 @@ export default class AddMovie extends React.Component {
           <label htmlFor="title">
             Titulo:
             <input type="text" name="title" id="title"/>
+          </label>
+          <label htmlFor="subtitle">
+            Sub-titulo:
+            <input type="text" name="subtitle" id="subtitle"/>
+          </label>
+          <label htmlFor="imgSource">
+            Image Source:
+            <input type="text" name="imgSource" id="imgSource"/>
+          </label>
+          <label htmlFor="synopsis">
+            Synopsis:
+            <input type="text" name="synopsis" id="synopsis"/>
+          </label>
+          <label htmlFor="rate">
+            Rate:
+            <input type="number" name="rate" id="rate"/>
+          </label>
+          <label htmlFor="gender">
+            Gender:
+            <input type="text" name="gender" id="gender"/>
           </label>
         </form>
       </div>
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -7,6 +7,7 @@ import SearchBar from './SearchBar';
 class MovieLibrary extends Component {
   constructor(props) {
   super(props);
+  
     this.state = {
       searchText: '',
       bookmarkedOnly: false,
