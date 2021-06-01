3988757a9632202901dbc14186bcf11454e7d3bf
Savio Moraes <smmsavio@hotmail.com>
2021-06-01 00:38:14 -0300

    acrescenta props e propstypes

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,27 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
+    const { searchText, onSearchTextChange } = this.props;
+
     return (
       <div>
         <form data-testid="search-bar-form">
-          <fieldset>
-            <label htmlFor="name-input" data-testid="text-input-label">
-              inclui o texto
-              <input
-                type="text"
-                name=""
-                id=""
-                value="searchText"
-                onChange="onSearchTextChange"
-                data-testid="text-input"
-              />
-            </label>
-          </fieldset>
+          <label htmlFor="name-input" data-testid="text-input-label">
+            inclui o texto
+            <input
+              type="text"
+              name=""
+              id=""
+              value={ searchText }
+              onChange={ onSearchTextChange }
+              data-testid="text-input"
+            />
+          </label>
         </form>
       </div>
     );
   }
 }
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+};
 
 export default SearchBar;
