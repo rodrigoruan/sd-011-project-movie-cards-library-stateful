950ae32f862d0b3b64a2edbd714b0ad71da6f7c5
BeatrizR <biarl.ramalhorl@gmail.com>
2021-06-01 09:49:31 -0300

    req 1 ao 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,96 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleStates = this.handleStates.bind(this);
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  handleStates({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  render() {
+    const { func: onClick } = this.props;
+    return (
+      <form data-testid="add-movie-form">
+        <label htmlFor="cont" data-testid="title-input-label">
+          Título
+          <input
+            name="title"
+            type="text"
+            value={ this.state.title }
+            data-testid="title-input"
+            onChange={ this.handleStates }
+          />
+        </label>
+        <label htmlFor="cont">
+          Subtítulo
+          <input
+            type="text"
+            data-testid="subtitle-input-label"
+            value={ this.state.subtitle }
+            onChange={ this.handleStates }
+          />
+        </label>
+        <label htmlFor="cont" data-testid="image-imput-label">
+          Imagem
+          <input
+            type="text"
+            data-testid="image-input"
+            value={ this.state.imagePath }
+            onChange={ this.handleStates }
+          />
+        </label>
+        <label htmlFor="cont" data-testid="storyline-input-label">
+          Sinopse
+          <textarea
+            data-testid="storyline-input"
+            value={ this.state.storyline }
+            onChange={ this.handleStates }
+          />
+        </label>
+        <label htmlFor="cont" data-testid="rating-input-label">
+          Avaliação
+          <input
+            type="number"
+            data-testid="rating-input"
+            value={ this.state.rating }
+            onChange={ this.handleStates }
+          />
+        </label>
+        <label htmlFor="cont" data-testid="genre-input-label">
+          Gênero
+          <select
+            data-testid="genre-input"
+            value={ this.state.genre }
+            onChange={ this.handleStates }
+          >
+            <option value="action" data-testid="genre-option">Ação</option>
+            <option value="comedy" data-testid="genre-option">Comédia</option>
+            <option value="thriller" data-testid="genre-option">Suspense</option>
+          </select>
+        </label>
+
+        <button type="button" data-testid="send-button" onClick={ onClick }>
+          Adicionar filme
+        </button>
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,6 +1,5 @@
 // implement AddMovie component here
 import React, { Component } from 'react';
-
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
@@ -8,18 +7,45 @@ import AddMovie from './AddMovie';
 class MovieLibrary extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: props.movies,
+    };
+
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+
   }
 
   render() {
+    const { movies, searchText, bookmarkedOnly, selectedGenre } = this.state;
     return (
       <div>
         <h2> My awesome movie library </h2>
-        <SearchBar />
-        <MovieList movies={this.props.movies} />
-        <AddMovie />
+        <SearchBar
+          searchText={ searchText }
+          bookmarkedOnly={ bookmarkedOnly }
+          selectedGenre={ selectedGenre }
+
+        />
+        <MovieList movies={ movies } />
+        <AddMovie fun={ this.onClick } />
       </div>
     );
   }
 }
 
 export default MovieLibrary;
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf,
+};
+
+MovieLibrary.defaultProps = {
+  movies: {},
+};
