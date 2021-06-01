ca463af7a8b7bcb930f6303a45778c68b29a967b
benlopes <rubenssouss@hotmail.com>
2021-06-01 10:45:00 -0300

    Render checkbox input within the search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -7,8 +7,12 @@ export default class SearchBar extends Component {
       <div>
         <form data-testid="search-bar-form">
           <label data-testid="text-input-label"> "Inclui o texto:"
-            <input value={this.props.searchText} onChange={this.props.onSearchTextChange}
-            data-testid="text-input" />
+            <input type='text' data-testid="text-input" value={this.props.searchText}
+            onChange={this.props.onSearchTextChange} />
+          </label>
+          <label data-testid="checkbox-input-label"> "Mostrar somente favoritos"
+            <input type='checkbox' data-testid="checkbox-input" checked={this.props.bookmarkedOnly}
+            onChange={this.props.onBookmarkedChange} />
           </label>
 
         </form>
