ada52229512d4be977c9dee8d5a6f036b3c90b18
Gustavo Lemes <gustavolemes997@gmail.com>
2021-06-01 14:48:01 -0300

    Requisito 10

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -3,15 +3,22 @@ import React, { Component } from 'react';
 
 export default class AddMovie extends Component {
   render() {
-    const { title, subtitle } =this.props;
+    const { title, subtitle, imagePath } = this.props;
     return (
-      <form data-testid="add-movie-form" >
-        <label data-testid="title-input-label" htmlFor="">Título
-        <input data-testid="title-input" type="text" value={title} />
+      <form data-testid="add-movie-form">
+        <label data-testid="title-input-label" htmlFor="">
+          Título
+          <input data-testid="title-input" type="text" value={title} />
         </label>
 
-        <label data-testid="subtitle-input-label" htmlFor="">Subtítulo
-        <input data-testid="subtitle-input" type="text" value={subtitle} />
+        <label data-testid="subtitle-input-label" htmlFor="">
+          Subtítulo
+          <input data-testid="subtitle-input" type="text" value={subtitle} />
+        </label>
+
+        <label htmlFor="image-input" data-testid="image-input-label">
+          Imagem
+          <input type="text" data-testid="image-input" value={imagePath} />
         </label>
       </form>
     );
