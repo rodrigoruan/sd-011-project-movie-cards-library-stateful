27d22a4366ba16f21aa5b9291b6dcbcdd841125c
Gustavo Mendes <gusm217@gmail.com>
2021-06-01 17:03:10 -0300

    Adiciona o 6 e 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,35 @@
 // implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    }
+  }
+  render() {
+    const { onClick } = this.props;
+
+    return(
+      <div>
+        <form data-testid="add-movie-form"></form>
+      </div>
+    )
+  }
+}
+
+AddMovie.PropTypes = {
+  onClick: PropTypes.func,
+}
+
+
+export default AddMovie;
+
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -10,31 +10,32 @@ class SearchBar extends React.Component {
       bookmarkedOnly,
       onBookmarkedChange,
       selectedGenre,
-      onSelectedGenreChange 
+      onSelectedGenreChange,
     } = this.props;
 
-    return(
+    return (
       <div>
         <form data-testid="search-bar-form">
-          <label data-testid="text-input-label">Inclui o texto 
-            <input data-testid="text-input" type="text" value={searchText} onChange={onSearchTextChange}>            
-            </input>
+          <label data-testid="text-input-label">
+            Inclui o texto
+            <input data-testid="text-input" type="text" value={ searchText } onChange={ onSearchTextChange } />
           </label>
-          <label data-testid="checkbox-input-label">Mostrar somente favoritos
-            <input data-testid="checkbox-input" type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange}/>
+          <label data-testid="checkbox-input-label">
+            Mostrar somente favoritos
+            <input data-testid="checkbox-input" type="checkbox" checked={ bookmarkedOnly } onChange={ onBookmarkedChange } />
           </label>
           <label data-testid="select-input-label">
             Filtrar por gênero
-            <select data-testid="select-input" value={selectedGenre} name="" onChange={onSelectedGenreChange}>
+            <select data-testid="select-input" value={ selectedGenre } name="" onChange={ onSelectedGenreChange }>
               <option data-testid="select-option" value="">Todos</option>
               <option data-testid="select-option" value="action">Ação</option>
               <option data-testid="select-option" value="comedy">Comédia</option>
               <option data-testid="select-option" value="thriller">Suspense</option>
-            </select>  
+            </select>
           </label>
         </form>
       </div>
-    )
+    );
   }
 }
 
