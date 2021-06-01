1e57f42aa75bc7918cf00a75ef0448b90f6c46cd
Gustavo Lemes <gustavolemes997@gmail.com>
2021-06-01 14:31:45 -0300

    Requisito 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 export default class AddMovie extends Component {
   render() {
     return (
-      <div></div>
+      <form data-testid="add-movie-form" ></form>
     );
   }
 }
