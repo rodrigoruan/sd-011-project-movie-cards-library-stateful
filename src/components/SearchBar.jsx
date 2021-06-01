5b7cfdd1f6d6842a77849910fd3a0a074fd42f4a
Matheus Gaspar <msouzagaspar@gmail.com>
2021-06-01 16:29:52 -0300

    requisito 4: adiciona input do tipo checkbox dentro de SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,7 +3,13 @@ import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
-    const { searchText, onSearchTextChange } = this.props;
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+    } = this.props;
+
     return (
       <form data-testid="search-bar-form">
         <label data-testid="text-input-label" htmlFor="search">
@@ -16,6 +22,20 @@ class SearchBar extends React.Component {
             onChange={ onSearchTextChange }
           />
         </label>
+
+        <label
+          data-testid="checkbox-input-label"
+          htmlFor="checkbox-input"
+        >
+          <input
+            type="checkbox"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+            data-testid="checkbox-input"
+            name="checkbox-input"
+          />
+          Mostrar somente favoritos
+        </label>
       </form>
     );
   }
@@ -24,6 +44,8 @@ class SearchBar extends React.Component {
 SearchBar.propTypes = {
   searchText: PropTypes.string.isRequired,
   onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
 };
 
 export default SearchBar;
