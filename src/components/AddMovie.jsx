2968c35e2526eaf49680d36eb839e5232dbd948d
nandovbr <nandovbr15@hotmail.com>
2021-06-01 16:39:33 -0300

    requisitos iniciais

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,10 @@
+import React, { Component } from 'react';
+
 // implement AddMovie component here
+class addMovie extends Component {
+  constructor() {
+    super()
+  }
+  render()
+};
+export default addMovie;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,35 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'porp-type';
+
+class SearchBar extends Component {
+  render() {
+    const searchBarMovie = {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
+    return (
+      <div>
+        <form data-testid="search-bar-form">
+          <label data-testid="text-input-label">Inclui o texto:</label>
+          <input data-testid="text-input" value={ this.searchText } onChange={ this.onSearchTextChange }></input>
+        </form>
+      </div>
+    );
+  }
+}
+
+SearchBar.PropTypes = {
+  serachText: PropTypes.string,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
+};
+
+export default SearchBar;
