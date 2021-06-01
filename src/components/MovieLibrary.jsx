129500a4876a8e02950418a4ea80021b5af0eb0f
Julio Filizzola <juliofilizzola@hotmail.com>
2021-05-31 22:17:45 -0300

    [Julio Filizzola] - adit app.js e MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
 import AddMovie from './AddMovie';
+import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 
 export default class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: props.movies,
+    };
+  }
+
   render() {
+    const { movies, searchText, bookmarkedOnly, selectedGenre } = this.state;
     return (
       <div>
-        <SearchBar />
-        <AddMovie />
+        <h2>My awesome movie library</h2>
+        <SearchBar
+          searchText={ searchText }
+          bookmarkedOnly={ bookmarkedOnly }
+          selectedGenre={ selectedGenre }
+        />
+        <MovieList movies={ movies } />
+        <AddMovie onClick={ (teste) => console.log(teste) } />
       </div>
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.object,
+  ).isRequired,
+};
