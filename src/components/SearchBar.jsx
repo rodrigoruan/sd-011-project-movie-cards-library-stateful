9cd9e676bf8cf2317f6a82abc12166a61a7fbeee
fernando lasmar <f3rnandolasmar@gmail.com>
2021-05-31 23:32:26 -0300

    requisito 3

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,31 +1,36 @@
 // implement AddMovie component here
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends Component {
   render() {
+    const { searchText,
+      onSearchTextChange, bookmarkedOnly, onBookmarkedChange } = this.props;
     return (
       <div>
         <form data-testid="search-bar-form">
-          <label htmlFor="name-input" data-testid="text-input-label">
+          <label htmlFor="textInput" data-testid="text-input-label">
             Inclui o texto
             <input
-              value="searchText"
-              // onChange={ onSearchTextChange }
+              id="textInput"
+              value={ searchText }
+              onChange={ onSearchTextChange }
               data-testid="text-input"
             />
           </label>
-          <label htmlFor="name-input" data-testid="checkbox-input-label">
+          <label htmlFor="checkboxInput" data-testid="checkbox-input-label">
             Mostrar somente favoritos
             <input
+              id="checkboxInput"
               type="checkbox"
-              value="bookmarkedOnly"
-              // onChange={ onBookmarkedChange }
+              value={ bookmarkedOnly }
+              onChange={ onBookmarkedChange }
               data-testid="checkbox-input"
             />
           </label>
-          <label htmlFor="name-input" data-testid="select-input-label">
+          <label htmlFor="selectOption" data-testid="select-input-label">
             Filtrar po gênero
-            <select value="selectedGenre" data-testid="select-input">
+            <select id="selectOption" value="selectedGenre" data-testid="select-input">
               <option value="" data-testid="select-option">Todos</option>
               <option value="action" data-testid="select-option">Ação</option>
               <option value="comedy" data-testid="select-option">Comédia</option>
@@ -37,5 +42,14 @@ class SearchBar extends Component {
     );
   }
 }
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  // selectedGenre: PropTypes.string.isRequired,
+  // onSelectedGenreChange: PropTypes.func.isRequired,
+};
 
 export default SearchBar;
