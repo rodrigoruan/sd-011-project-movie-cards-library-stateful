7ddc7f2d5f7e427225399beb8de16365fb8c2fab
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-06-01 13:09:56 -0300

    AddMovie.jsx| Add Req 9 && 10

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -11,14 +11,60 @@ class AddMovie extends React.Component {
       rating: 0,
       genre: 'action',
     };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    const { name } = e.target;
+    const { value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  inputTitle() {
+    const { title } = this.state;
+    return (
+      <label
+        htmlFor="title"
+        data-testid="title-input-label"
+      >
+        Título
+        <input
+          type="text"
+          name="title"
+          value={ title }
+          onChange={ this.handleChange }
+        />
+      </label>
+    );
+  }
+
+  inputSubtitle() {
+    const { subtitle } = this.state;
+    return (
+      <label
+        htmlFor="subtitle"
+        data-testid="subtitle-input-label"
+      >
+        Subtítulo
+        <input
+          type="text"
+          name="subtitle"
+          value={ subtitle }
+          data-testid="subtitle-input"
+          onChange={ this.handleChange }
+        />
+      </label>
+    );
   }
 
   render() {
-    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
+    const { imagePath, storyline, rating, genre } = this.state;
     return (
       <form action="" data-testid="add-movie-form">
-        <div>{ title }</div>
-        <div>{ subtitle }</div>
+        {this.inputTitle()}
+        {this.inputSubtitle()}
         <div>{ imagePath }</div>
         <div>{ storyline }</div>
         <div>{ rating }</div>
