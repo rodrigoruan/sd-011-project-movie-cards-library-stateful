8ef171721bc6f3532f4cde3bac23e6a83abf8903
Tales Coelho <tales-coelho@live.com>
2021-06-01 08:45:20 -0300

    inserindo name nos selects

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -46,9 +46,9 @@ class AddMovie extends Component {
             value={ genre }
             data-testid="genre-input"
           >
-            <option value="action">Ação</option>
-            <option value="comedy">Comédia</option>
-            <option value="thriller">Suspense</option>
+            <option value="action" data-testid="genre-option">Ação</option>
+            <option value="comedy" data-testid="genre-option">Comédia</option>
+            <option value="thriller" data-testid="genre-option">Suspense</option>
           </select>
         </label>
         <button
