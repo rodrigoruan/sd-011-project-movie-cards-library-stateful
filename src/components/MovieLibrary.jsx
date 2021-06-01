94200145c9f210e5b197f33b6e18ef163e182c23
Diogo Augusto <diogotrescastro@gmail.com>
2021-06-01 19:27:05 -0300

    Resoluçao dos requisitos 15,16 e 17

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,33 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+
+export default class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+    const { movies } = props;
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+  }
+
+  render() {
+    const { movies } = this.state;
+    return (
+      <div>
+        <SearchBar />
+        <MovieList movies={ movies } />
+
+      </div>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
