f61bb4a2e3e6f943a9d04e594beff106a05a40a1
Savio Moraes <smmsavio@hotmail.com>
2021-05-31 23:56:44 -0300

    inicia a resoluçao do requisito 03

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,7 +4,21 @@ class SearchBar extends React.Component {
   render() {
     return (
       <div>
-        <form data-testid="search-bar-form" />
+        <form data-testid="search-bar-form">
+          <fieldset>
+            <legend>inclui o texto</legend>
+            <label htmlFor="name-input" data-testid="text-input-label">
+              <input
+                type="text"
+                name=""
+                id=""
+                value="searchText"
+                onChange="onSearchTextChange"
+                data-testid="text-input"
+              />
+            </label>
+          </fieldset>
+        </form>
       </div>
     );
   }
