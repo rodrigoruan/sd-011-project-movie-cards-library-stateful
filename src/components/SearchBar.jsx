d1c2ab68515e09a752c020ea4dc3894e05296c2d
Gustavo Mendes <gusm217@gmail.com>
2021-06-01 16:39:33 -0300

    Adiciona o 5 e finaliza o componente SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -23,7 +23,15 @@ class SearchBar extends React.Component {
           <label data-testid="checkbox-input-label">Mostrar somente favoritos
             <input data-testid="checkbox-input" type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange}/>
           </label>
-          <label></label>
+          <label data-testid="select-input-label">
+            Filtrar por gênero
+            <select data-testid="select-input" value={selectedGenre} name="" onChange={onSelectedGenreChange}>
+              <option data-testid="select-option" value="">Todos</option>
+              <option data-testid="select-option" value="action">Ação</option>
+              <option data-testid="select-option" value="comedy">Comédia</option>
+              <option data-testid="select-option" value="thriller">Suspense</option>
+            </select>  
+          </label>
         </form>
       </div>
     )
