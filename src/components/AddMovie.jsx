58044084171a95755e5b4170211abdf629739811
Tales Coelho <tales-coelho@live.com>
2021-06-01 09:37:09 -0300

    corrigindo erro de logica

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -25,14 +25,14 @@ class AddMovie extends Component {
     const { onClick } = this.props;
     event.preventDefault();
     onClick(this.state);
-    this.setState({
+    this.setState(() => ({
       subtitle: '',
       title: '',
       imagePath: '',
       storyline: '',
       rating: 0,
       genre: 'action',
-    });
+    }));
   }
 
   addState({ target }) {
