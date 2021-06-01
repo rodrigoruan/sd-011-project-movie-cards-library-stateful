1ed89ceb3e18fcc2a60b2254c3b5dd2a8d5adaf8
Bruno Duarte <brunoarduarte@gmail.com>
2021-06-01 20:29:00 -0300

    Requisito 15

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,14 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import AddMovie from './AddMovie';
+import SearchBar from './SearchBar';
+
+export default class MovieLibrary extends Component {
+  render() {
+    return (
+      <>
+        <SearchBar />
+        <AddMovie />
+      </>
+    );
+  }
+}
