7d0d3cccfd6b5d2b32ba19b77708776b25a310ec
Leandro Lopes <leandro_carlos@uol.com.br>
2021-06-01 16:30:06 -0300

    item 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,23 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+export class AddMovie extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    const { onClick } = this.props;
+    return <div />;
+  }
+}
+
+export default AddMovie;
