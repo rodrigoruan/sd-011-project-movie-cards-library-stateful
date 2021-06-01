eacc14fa4b8c4dbe5b4f3ceff6d24b1bc12cd3fb
OldRoge <rogeriomrjr@icloud.com>
2021-06-01 17:13:46 -0300

    Requirement 1 and 2, I think...

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/Header';
+import SearchBar from './components/SearchBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <SearchBar />
       <Header />
     </div>
   );
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,21 @@
-// implement AddMovie component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <form data-testid="search-bar-form">
+        <label data-testid="text-input-label" htmlFor="text-label">
+          Inclui o texto:
+          <input
+            value="searchText"
+            type="text"
+            onChange="onSearchTextChange"
+            data-testid="text-input"
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
