0c479d614c29b833a8469b90696821d591f1db11
Savio Moraes <smmsavio@hotmail.com>
2021-06-01 02:21:03 -0300

    inclui atributo no select e nos options

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -39,11 +39,12 @@ class SearchBar extends React.Component {
               id=""
               value={ selectedGenre }
               onChange={ onSelectedGenreChange }
+              data-testid="select-input"
             >
-              <option value="">Todos</option>
-              <option value="action">Ação</option>
-              <option value="comedy">Comédia</option>
-              <option value="thriller">Suspense</option>
+              <option value="" data-testid="select-option">Todos</option>
+              <option value="action" data-testid="select-option">Ação</option>
+              <option value="comedy" data-testid="select-option">Comédia</option>
+              <option value="thriller" data-testid="select-option">Suspense</option>
             </select>
           </label>
         </form>
