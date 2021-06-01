1058a4b5152a1ee93f3f56c7e92cc0ea55fa62b0
Dario Junior <dario.junior.rs@gmail.com>
2021-05-31 21:38:21 -0300

    Fix: Lint errors

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,22 +4,30 @@ import PropTypes from 'prop-types';
 export default class SearchBar extends React.Component {
   render() {
     const { searchText, onSearchTextChange } = this.props;
-    console.log(searchText)
+    // console.log(searchText)
     return (
       <form action="" data-testid="search-bar-form">
-        <label data-testid="text-input-label" > Inclui o texto
-          <input data-testid="text-input" type="text" value={searchText} onChange={onSearchTextChange}/>
+        <label htmlFor="search" data-testid="text-input-label">
+          Inclui o texto
+          <input
+            id="search"
+            data-testid="text-input"
+            type="text"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+          />
         </label>
+        <input type="checkbox" data-testid="checkbox-input" />
       </form>
-    )
+    );
   }
 }
 
 SearchBar.propTypes = {
-  searchText: PropTypes.string,
-  onSearchTextChange: PropTypes.func,
-  bookmarkedonly: PropTypes.bool,
-  onBookMarkedChange: PropTypes.func,
-  selectedGenre: PropTypes.string,
-  onSelectedGenreChange: PropTypes.func,
-}
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  // bookmarkedonly: PropTypes.bool,
+  // onBookMarkedChange: PropTypes.func,
+  // selectedGenre: PropTypes.string,
+  // onSelectedGenreChange: PropTypes.func,
+};
