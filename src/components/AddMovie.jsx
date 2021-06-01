96e0f43abf3c5a9462825ed0006fb89b068b6825
Gustavo Mendes <gusm217@gmail.com>
2021-06-01 19:02:25 -0300

    Adiciona o 10 e 11

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -41,6 +41,14 @@ class AddMovie extends React.Component {
             <input data-testid="subtitle-input" name="subtitle" type="text" value={this.state.subtitle} onChange={this.handleChange
             }/>
           </label>
+          <label data-testid="image-input-label">
+            Imagem
+            <input data-testid="image-input" name="imagePath" type="text" value={this.state.imagePath} onChange={this.handleChange}/>
+            </label>
+            <label data-testid="storyline-input-label">
+              Sinopse
+              <input data-testid="storyline-input" type="text-area" name="storyline" value={this.state.storyline} onChange={this.handleChange}/>
+              </label>
         </form>
       </div>
     )
