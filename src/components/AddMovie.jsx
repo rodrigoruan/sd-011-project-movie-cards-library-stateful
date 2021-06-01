e278fd4bba9db1d3fa10aed1b0819181593e6817
Raphael <raphaellfpg@gmail.com>
2021-06-01 20:05:47 -0300

    Ate o requisito 13

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,53 @@
-// implement AddMovie component heree
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  render() {
+    const { onClick } = this.props;
+    return (
+      <form data-testid="add-movie-form">
+        <label htmlFor data-testid="title-input-label">
+          Título
+          <input type="text" data-testid="title-input" />
+        </label>
+        <label htmlFor data-testid="subtitle-input-label">
+          Subtítulo
+          <input type="text" data-testid="subtitle-input" />
+        </label>
+        <label htmlFor data-testid="image-input-label">
+          Imagem
+          <input type="text" data-testid="image-input" />
+        </label>
+        <label htmlFor data-testid="storyline-input-label">
+          Sinopse
+          <input type="textarea" data-testid="storyline-input" />
+        </label>
+        <label htmlFor data-testid="rating-input-label">
+          Avaliação
+          <input type="number" data-testid="rating-input" defaultValue="0" />
+        </label>
+        <label htmlFor data-testid="genre-input-label">
+          Gênero
+          <select data-testid="genre-input">
+            <option value="action" data-testid="genre-option">Ação</option>
+            <option value="comedy" data-testid="genre-option">Comédia</option>
+            <option value="thriller" data-testid="genre-option">Suspense</option>
+          </select>
+        </label>
+        <button
+          type="submit"
+          data-testid="send-button"
+          onClick={ onClick }
+        >
+          Adicionar filme
+        </button>
+      </form>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  onClick: PropTypes.element.isRequired,
+};
+
+export default AddMovie;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,63 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends React.Component {
+  render() {
+    const { searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange } = this.props;
+    return (
+      <form data-testid="search-bar-form">
+
+        <label htmlFor="text-input-label" data-testid="text-input-label">
+          Inclui o texto:
+          <input
+            type="text"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+            data-testid="text-input"
+          />
+        </label>
+
+        <label htmlFor="checkbox-input-label" data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input
+            type="checkbox"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+            data-testid="checkbox-input"
+          />
+        </label>
+
+        <label htmlFor="select-input-label" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+            data-testid="select-input"
+          >
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
+        </label>
+
+      </form>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.string.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.bool.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.string.isRequired,
+};
+
+export default SearchBar;
