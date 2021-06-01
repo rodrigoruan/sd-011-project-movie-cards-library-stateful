b7a45814152a057562e65bb445823983958c4c48
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-06-01 15:21:03 -0300

    import fixes

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,6 +1,5 @@
-// implement AddMovie component here
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
