f4eed149bc7b46a4ae1f456e65f75dc383cc97ac
fernando lasmar <f3rnandolasmar@gmail.com>
2021-06-01 01:00:15 -0300

    requisito 11

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,20 +2,65 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      title: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { title, subtitle, imagePath, storyline } = this.state;
     return (
       <form data-testid="add-movie-form">
         <label htmlFor="titleInput" data-testid="title-input-label">
           Título:
-          <input type="texto" data-testid="title-input" />
+          <input
+            name="title"
+            type="text"
+            value={ title }
+            onChange={ this.handleChange }
+            data-testid="title-input"
+          />
         </label>
         <label htmlFor="subtitleInput" data-testid="subtitle-input-label">
           Subtítulo:
-          <input type="texto" data-testid="subtitle-input" />
+          <input
+            name="subtitle"
+            type="text"
+            value={ subtitle }
+            onChange={ this.handleChange }
+            data-testid="subtitle-input"
+          />
         </label>
         <label htmlFor="imageInput" data-testid="image-input-label">
           Imagem:
-          <input type="texto" data-testid="image-input" />
+          <input
+            name="imagePath"
+            type="text"
+            value={ imagePath }
+            onChange={ this.handleChange }
+            data-testid="image-input"
+          />
+        </label>
+        <label htmlFor="storylineInput" data-testid="storyline-input-label">
+          Sinopse:
+          <textarea
+            name="storyline"
+            value={ storyline }
+            onChange={ this.handleChange }
+            data-testid="storyline-input"
+          />
         </label>
       </form>
     );
