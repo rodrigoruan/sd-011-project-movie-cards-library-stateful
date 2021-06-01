95e478441fe6bced2a3f0704cb1f5ef9b39c3bc7
Vinicius <viniciusfod@gmail.com>
2021-06-01 16:04:27 -0300

    requisito 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -11,12 +11,31 @@ export default class AddMovie extends Component {
       rating: 0,
       genre: 'action',
     };
+    this.changeState = this.changeState.bind(this);
+  }
+
+  changeState({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
   }
 
   render() {
     const { onClick } = this.props;
     return (
-      <form data-testid="add-movie-form" />
+      <form data-testid="add-movie-form">
+        <label htmlFor="title">
+          Título
+          <input
+            onChange={ this.changeState }
+            name="title"
+            data-testid="title-input"
+            value={ this.title }
+            id="title"
+          />
+        </label>
+      </form>
     );
   }
 }
