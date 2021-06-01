8e6dc2c8575d4b92c274a5cb67cc8e0ef1c5bc4f
rubia braga <rubiaemilly29@gmail.com>
2021-06-01 16:18:26 -0300

    Requisito 1 -14

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,54 @@
 // implement AddMovie component here
+import React from 'react';
+import Title from './AddMovieElements/Title';
+import Subtitle from './AddMovieElements/Subtitle';
+import Storyline from './AddMovieElements/Storyline';
+import Rating from './AddMovieElements/NewRating';
+import ImagePath from './AddMovieElements/ImagePath';
+import Genre from './AddMovieElements/Genre';
+
+class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleSubmit() {
+
+  }
+
+  render() {
+    const { title, subtitle, imagePath, storyline, genre, rating } = this.state;
+    return (
+      <form data-testid="add-movie-form">
+        <Title value={ title } handleChange={ this.handleChange } />
+        <Subtitle value={ subtitle } handleChange={ this.handleChange } />
+        <ImagePath value={ imagePath } handleChange={ this.handleChange } />
+        <Storyline value={ storyline } handleChange={ this.handleChange } />
+        <Rating value={ rating } handleChange={ this.handleChange } />
+        <Genre value={ genre } handleChange={ this.handleChange } />
+        <button type="button" data-testid="send-button" onClick={ this.handleSubmit }>
+          Adicionar filme
+        </button>
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,25 +1,65 @@
 // implement AddMovie component here
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
-    const {{ search: searchText, 
+    const {
+      searchText,
       onSearchTextChange,
       bookmarkedOnly,
       onBookmarkedChange,
       selectedGenre,
       onSelectedGenreChange,
-    }
-    } = this.props
-    return(
+    } = this.props;
+    return (
       <form data-testid="search-bar-form">
-        <label data-testid="text-input-label"> Inclui o texto:
-          <input 
-          type='text'
-          onChange="onSearchTextChange"
-          ></input>
+        <label htmlFor="texto" data-testid="text-input-label">
+          Inclui o texto:
+          <input
+            id="texto"
+            type="text"
+            onChange={ onSearchTextChange }
+            value={ searchText }
+            data-testid="text-input"
+          />
+        </label>
+        <label htmlFor="check" data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input
+            id="check"
+            type="checkbox"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+            data-testid="checkbox-input"
+          />
+        </label>
+        <label htmlFor="selecionar" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            id="selecionar"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+            data-testid="select-input"
+          >
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
         </label>
       </form>
-    )
+    );
   }
 }
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
+};
+
+export default SearchBar;
