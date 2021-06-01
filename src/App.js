58a870adb738d25c8b65c3cd4b3efbb776ac8ba4
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-05-31 23:49:28 -0300

    Corrigindo erros do lint

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
-import SearchBar from './components/SearchBar';
-import AddMovie from './components/AddMovie';
-import MovieLibrary from './components/MovieLibrary';
+// import MovieLibrary from './components/MovieLibrary';
 
 function App() {
   return (
