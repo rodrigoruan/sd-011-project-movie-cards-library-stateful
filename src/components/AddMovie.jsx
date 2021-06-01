9ae96ec28a828d5d5ca7114e371f764df8287f06
benlopes <rubenssouss@hotmail.com>
2021-06-01 16:13:39 -0300

    Render imagePath input within AddMovie form

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -33,18 +33,17 @@ export default class AddMovie extends Component {
         <form data-testid="add-movie-form">
           <label data-testid="title-input-label"> "Título"
             <input type='text' data-testid="title-input" value={this.state.value}
-              onChange={this.handleInputs}
-            />
+              onChange={this.handleInputs} />
           </label>
           
           <label data-testid="subtitle-input-label"> "Subtítulo"
             <input type='text' data-testid="subtitle-input" value={this.state.value}
-              onChange={this.handleInputs}
-            />
+              onChange={this.handleInputs} />
           </label>
 
-          <label> caminho da imagem
-            <input />
+          <label data-testid="image-input-label"> "Imagem"
+            <input type='text' data-testid="image-input" value={this.state.value}
+              onChange={this.handleInputs} />
           </label>
 
           <label> sinopse
