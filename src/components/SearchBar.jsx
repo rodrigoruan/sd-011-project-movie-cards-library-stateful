8852b446732cf13bdb6d5ab98d52c02d686f6aa7
fernando lasmar <f3rnandolasmar@gmail.com>
2021-05-31 22:31:26 -0300

    resolvendo conflito lint 01

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,7 +6,32 @@ class SearchBar extends Component {
     return (
       <div>
         <form data-testid="search-bar-form">
-          <legend>Form</legend>
+          <label htmlFor="name-input" data-testid="text-input-label">
+            Inclui o texto
+            <input
+              value="searchText"
+              // onChange={ onSearchTextChange }
+              data-testid="text-input"
+            />
+          </label>
+          <label htmlFor="name-input" data-testid="checkbox-input-label">
+            Mostrar somente favoritos
+            <input
+              type="checkbox"
+              value="bookmarkedOnly"
+              // onChange={ onBookmarkedChange }
+              data-testid="checkbox-input"
+            />
+          </label>
+          <label htmlFor="name-input" data-testid="select-input-label">
+            Filtrar po gênero
+            <select value="selectedGenre" data-testid="select-input">
+              <option value="" data-testid="select-option">Todos</option>
+              <option value="action" data-testid="select-option">Ação</option>
+              <option value="comedy" data-testid="select-option">Comédia</option>
+              <option value="thriller" data-testid="select-option">Suspense</option>
+            </select>
+          </label>
         </form>
       </div>
     );
