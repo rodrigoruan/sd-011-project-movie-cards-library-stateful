c513891948d18193439101bb62158e4d87dc09fd
Vinicius <viniciusfod@gmail.com>
2021-06-01 15:24:35 -0300

    define as proptypes do componente SEARCHBAR

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class SearchBar extends Component {
   render() {
@@ -11,7 +12,6 @@ export default class SearchBar extends Component {
       onSelectedGenreChange,
     } = this.props;
 
-    const { ...props } = this.props;
     return (
       <form data-testid="search-bar-form">
         <label data-testid="text-input-label" htmlFor="Texto">
@@ -51,3 +51,12 @@ export default class SearchBar extends Component {
     );
   }
 }
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
+};
