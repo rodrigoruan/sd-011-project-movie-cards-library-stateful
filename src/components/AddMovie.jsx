cf17030ec864c4655f365acb092fb844f81ff9d4
israeltonholo <israeltonholo@hotmail.com>
2021-06-01 08:42:20 -0300

    adiciona requisito 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -4,14 +4,44 @@ import PropTypes from 'prop-types';
 export default class AddMovie extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
   }
 
   render() {
     const { onClick } = this.props;
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
     return (
-      <form data-testid="add-movie-form">
-        
-      </form>
-    )
+      <section>
+        <form data-testid="add-movie-form">
+          <label htmlFor="input-title" data-testid="title-input-label">
+            Título
+            <input
+              type="text"
+              name="title"
+              id="input-title"
+              value={ title }
+              onChange={ this.handleChange }
+              data-testid="title-input"
+            />
+          </label>
+        </form>
+
+      </section>
+    );
   }
 }
