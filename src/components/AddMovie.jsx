6514823095a37669dc42b1e756b04f2527fcf8fc
Bruno Duarte <brunoarduarte@gmail.com>
2021-06-01 11:54:44 -0300

    Refatorando e implementando Requisitos 10, 11 e 12

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,56 +1,14 @@
 import React, { Component } from 'react';
+import FisrtPart from './FisrtPart';
+import SecondPart from './SecondPart';
 // import PropTypes from 'prop-types';
 
 class AddMovie extends Component {
-  constructor() {
-    super();
-    this.state = {
-      subtitle: '',
-      title: '',
-      // imagePath: '',
-      // storyline: '',
-      // rating: 0,
-      // genre: 'action',
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange({ target }) {
-    const { name } = target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-
-    this.setState({
-      [name]: value,
-    });
-  }
-
   render() {
-    const { title, subtitle } = this.state;
     return (
       <form data-testid="add-movie-form">
-        <h1>bla</h1>
-        <label htmlFor="title" data-testid="title-input-label">
-          Título
-          <input
-            id="title"
-            value={ title }
-            name="title"
-            type="text"
-            data-testid="title-input"
-            onChange={ this.handleChange }
-          />
-        </label>
-        <label htmlFor="subtitle" data-testid="subtitle-input-label">
-          Subtítulo
-          <input
-            id="subtitle"
-            value={ subtitle }
-            name="subtitle"
-            type="text"
-            data-testid="subtitle-input"
-            onChange={ this.handleChange }
-          />
-        </label>
+        <FisrtPart />
+        <SecondPart />
       </form>
     );
   }
