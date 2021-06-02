a51ccc70ea1227d0ad802e81e0e4607b55976321
Julio Cesar <julio.cesarcj1402@gmail.com>
2021-06-02 18:51:35 -0300

    colocando movielibrary

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from './components/Header';
 import SearchBar from './components/SearchBar';
 import AddMovie from './components/AddMovie';
+import MovieLibrary from './components/MovieLibrary';
 import './App.css';
 
 class App extends Component {
@@ -48,6 +49,7 @@ class App extends Component {
           selectedGenre={ genre }
           onSelectedGenreChange={ this.searchGenre }
         />
+        <MovieLibrary />
         <AddMovie onChange={ this.createNewMovieItem } />
       </div>
     );
