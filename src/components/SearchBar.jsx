f87f62203c194961142ea751301978178a2bff90
Alexandre <naitwa.alexandre@gmail.com>
2021-06-02 07:55:48 -0300

    select controlled

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -9,18 +9,46 @@ export default class SearchBar extends Component {
       bookmarkedOnly,
       onBookmarkedChange,
       onSearchTextChange,
-      onSelectedGenreChange
+      onSelectedGenreChange,
     } = this.props;
     return (
       <form data-testid="search-bar-form">
         <label htmlFor="inputText" data-testid="text-input-label">
           Inclui o texto:
           <input
+            type="text"
             name="searchText"
             data-testid="text-input"
             value={ searchText }
             onChange={ onSearchTextChange }
           />
+        </label>
+        <label htmlFor="inputCheckBox" data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input
+            type="checkbox"
+            name="bookmarkedOnly"
+            data-testid="checkbox-input"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+          />
+        </label>
+        <label
+          htmlFor="select"
+          data-testid="select-input-label"
+        >
+          Filtrar por gênero
+          <select
+            name="selectedGenre"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+            data-testid="select-input"
+          >
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
         </label>
       </form>
     );
