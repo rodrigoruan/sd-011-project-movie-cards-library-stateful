658a922fd82c8123039adf0acf30a6a7a576b462
Pedro Castro <pa.coelho.castro@gmail.com>
2021-06-02 09:19:20 -0300

    Req 15

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class MovieLibrary extends Component {
   render() {
-    return (
-      <div />
-    );
+    const movies = this.props;
+    return <div />;
   }
 }
+
+MovieLibrary.defaultProps = {
+  movies: [],
+};
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object),
+};
 
 export default MovieLibrary;
