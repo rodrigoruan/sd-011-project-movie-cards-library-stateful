287c00ac53c663f847543923c80bc354444b5e61
Luciano Pimenta <l.a.p.pimenta.j@gmail.com>
2021-06-02 00:51:03 -0300

    Pesquisar funcionando

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -28,8 +28,21 @@ class MovieLibrary extends Component {
     const { movies } = this.props;
     const { searchText, bookmarkedOnly, selectedGenre } = this.state;
     // apenas pelo titulo
-    const filterMovies = movies.filter((movie) => movie.title.toLowerCase()
-      .includes(searchText.toLowerCase()));
+    let filterMovies = movies.filter((movie) => (
+      movie.title.toLowerCase().includes(searchText.toLowerCase())
+      || movie.subtitle.toLowerCase().includes(searchText.toLowerCase())
+      || movie.storyline.toLowerCase().includes(searchText.toLowerCase())
+    ));
+
+    if (bookmarkedOnly) {
+      filterMovies = filterMovies.filter((movie) => movie.bookmarked === true);
+    }
+
+    if (selectedGenre !== '') {
+      filterMovies = filterMovies.filter((movie) => (
+        movie.genre.includes(selectedGenre.toLowerCase())));
+    }
+
     return (
       <div>
         <h2> My awesome movie library </h2>
