0220b0ce9d8f84fccbf30bd75c51d592fe18e2b7
Luan Xavier <luantenorio3@gmail.com>
2021-06-02 11:07:00 -0300

    Resolver erro no requisito 4

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -17,6 +17,7 @@ class SearchBar extends React.Component {
         <label data-testid="text-input-label" htmlFor="text-input">
           Inclui o texto:
           <input
+            type="text"
             data-testid="text-input"
             value={ searchText }
             onChange={ onSearchTextChange }
@@ -25,6 +26,7 @@ class SearchBar extends React.Component {
         <label data-testid="checkbox-input-label" htmlFor="checkbox-input">
           Mostrar somente favoritos
           <input
+            type="checkbox"
             data-testid="checkbox-input"
             checked={ bookmarkedOnly }
             onChange={ onBookmarkedChange }
