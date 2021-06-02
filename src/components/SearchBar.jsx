5f6b7f7e6e70d835907eb077fc2df92d849448d3
marlon307 <marlon-ramosb@hotmail.com>
2021-06-02 11:05:40 -0300

    3 update

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -11,7 +11,7 @@ class SearchBar extends Component {
       selectedGenre, onSelectedGenreChange } = this.props;
     return (
       <form data-testid="search-bar-form">
-        <InputText srtTxt={ searchText } srcOnCh={ onSearchTextChange } />
+        <InputText searchText={ searchText } srcOnCh={ onSearchTextChange } />
         <InputCheckBox bkOnly={ bookmarkedOnly } srcbkOch={ onBookmarkedChange } />
         <SelectList slVl={ selectedGenre } evSl={ onSelectedGenreChange } />
       </form>
