503deae37bb6e6ff00b62c4794cf6287d86c00da
Julio Cesar <julio.cesarcj1402@gmail.com>
2021-06-02 18:55:22 -0300

    entendendo problema do requisito 15

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import SearchBar from './SearchBar';
-import MovieList from './MovieList';
 
 class MovieLibrary extends Component {
   render() {
     return (
       <div>
         <SearchBar />
-        <MovieList />
       </div>
     );
   }
