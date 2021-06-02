19e75f8a59bc3e47ba91317f669092d57e586f5b
OldRoge <rogeriomrjr@icloud.com>
2021-06-02 14:24:43 -0300

    Requirements 8, 9 e 10

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,37 +1,58 @@
 import React from 'react';
-// import propTypes from 'prop-types';
-// import InputFile from './InputFile';
 
 class AddMovie extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       title: '',
+      subtitle: '',
+      imagePath: '',
     };
 
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange({ target }) {
-    const { name } = target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const { name, value } = target;
     this.setState({
       [name]: value,
     });
   }
 
   render() {
-    const { title } = this.state;
+    const { title, subtitle, imagePath } = this.state;
     return (
       <form data-testid="add-movie-form">
         <label data-testid="title-input-label" htmlFor="title-label">
           Título
           <input
-            id="title"
-            name="title"
+            data-testid="title-input"
             type="text"
             value={ title }
             onChange={ this.handleChange }
+            name="title"
+          />
+        </label>
+
+        <label data-testid="subtitle-input-label" htmlFor="subtitle-lable">
+          Subtítulo
+          <input
+            data-testid="subtitle-input"
+            type="text"
+            value={ subtitle }
+            onChange={ this.handleChange }
+            name="subtitle"
+          />
+        </label>
+
+        <label data-testid="image-input-label" htmlFor="imagem-path-label">
+          Imagem
+          <input
+            data-testid="image-input"
+            type="text"
+            value={ imagePath }
+            onChange={ this.handleChange }
+            name="imagePath"
           />
         </label>
       </form>
