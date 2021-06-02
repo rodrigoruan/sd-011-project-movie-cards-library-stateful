e3a163fc3d23fc27d005354bcc761a4101f901c9
Diogo Augusto <diogotrescastro@gmail.com>
2021-06-02 20:18:27 -0300

    Resoluçao dos requisitos de 15 a 17

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -14,13 +14,53 @@ export default class MovieLibrary extends Component {
       selectedGenre: '',
       movies,
     };
+
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+    this.filter = this.filter.bind(this);
+  }
+
+  // Usado as dicas passadas pelo Frank Rocha no vídeo do slack https://trybecourse.slack.com/archives/C01PLFW7347/p1622580401484700?thread_ts=1622577412.474100&cid=C01PLFW7347
+  onSearchTextChange(event) {
+    const { value } = event.target;
+    this.setState({ searchText: value });
+  }
+
+  onBookmarkedChange(event) {
+    const { checked } = event.target;
+    this.setState({ bookmarkedOnly: checked });
+  }
+
+  onSelectedGenreChange(event) {
+    const { value } = event.target;
+    this.setState({ selectedGenre: value });
+  }
+
+  filter() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+    return movies
+      .filter((bookmarked) => bookmarked === bookmarkedOnly)
+      .filter((genre) => genre.includes(selectedGenre))
+      .filter((title, subtitle, storyline) => (
+        title.includes(searchText)
+        || subtitle.includes(searchText)
+        || storyline.includes(searchText)
+      ));
   }
 
   render() {
-    const { movies } = this.state;
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
     return (
       <div>
-        <SearchBar />
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ this.onSearchTextChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.onBookmarkedChange }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.onSelectedGenreChange }
+        />
         <MovieList movies={ movies } />
 
       </div>
