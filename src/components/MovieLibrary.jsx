ef672f1a68da252425a775af5e169d3484381762
Vitor HUgo <vitor.h.braz.leite@gmail.com>
2021-06-02 14:26:12 -0300

    vai curintiaaaa

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,94 @@
 // implement AddMovie component here
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+// import MovieCard from './MovieCard';
+import MovieList from './MovieList';
+
+class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+    const { movies } = this.props;
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+    this.onClick = this.onClick.bind(this);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+  }
+
+  onSearchTextChange(event) {
+    this.setState({ searchText: event.target.value });
+  }
+
+  onBookmarkedChange(event) {
+    this.setState({ bookmarkedOnly: event.target.checked });
+  }
+
+  onSelectedGenreChange(event) {
+    this.setState({ selectedGenre: event.target.value });
+  }
+
+  onClick(state, event) {
+    const { movies } = this.state;
+    this.setState({
+      movies: [...movies, state],
+    });
+    event.preventDefault();
+  }
+
+  // requisito 18
+  filterMovie() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+    let filteredMovies = movies;
+    if (searchText) {
+      filteredMovies = movies.filter((movie) => (
+        movie.title.includes(searchText)
+        || movie.subtitle.includes(searchText)
+        || movie.storyline.includes(searchText)
+      ));
+    }
+    if (bookmarkedOnly) {
+      filteredMovies = movies.filter((movie) => (
+        movie.bookmarked === true
+      ));
+    }
+    if (selectedGenre) {
+      filteredMovies = movies.filter((movie) => (
+        movie.genre === selectedGenre
+      ));
+    }
+    return filteredMovies;
+  }
+
+  render() {
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
+    const movies = this.filterMovie();
+    return (
+      <div>
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ this.onSearchTextChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.onBookmarkedChange }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.onSelectedGenreChange }
+        />
+        <MovieList movies={ movies } />
+        <AddMovie onClick={ this.onClick } />
+      </div>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object),
+}.isRequired;
+
+export default MovieLibrary;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,68 @@
 // implement AddMovie component here
+// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends React.Component {
+  render() {
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange,
+      selectedGenre, onSelectedGenreChange } = this.props;
+    return (
+      <div>
+        {searchText}
+        {onSearchTextChange}
+        {bookmarkedOnly}
+        {onBookmarkedChange}
+        {selectedGenre}
+        {onSelectedGenreChange}
+        <form data-testid="search-bar-form">
+          <label htmlFor="ex3" data-testid="text-input-label">
+            Inclui o texto:
+            <input
+              id="ex3"
+              type="text"
+              value={ searchText }
+              onChange={ onSearchTextChange }
+              data-testid="text-input"
+            />
+          </label>
+          <label htmlFor="ex4" data-testid="checkbox-input-label">
+            Mostrar somente favoritos
+            <input
+              id="ex4"
+              type="checkbox"
+              checked={ bookmarkedOnly }
+              onChange={ onBookmarkedChange }
+              data-testid="checkbox-input"
+            />
+          </label>
+          <label htmlFor="ex5" data-testid="select-input-label">
+            Filtrar por gênero
+            <select
+              id="ex5"
+              value={ selectedGenre }
+              onChange={ onSelectedGenreChange }
+              data-testid="select-input"
+            >
+              <option data-testid="select-option" value="">Todos</option>
+              <option data-testid="select-option" value="action">Ação</option>
+              <option data-testid="select-option" value="comedy">Comédia</option>
+              <option data-testid="select-option" value="thriller">Suspense</option>
+            </select>
+          </label>
+        </form>
+      </div>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string,
+  onSearchTextChange: PropTypes.func,
+  bookmarkedOnly: PropTypes.bool,
+  onBookmarkedChange: PropTypes.func,
+  selectedGenre: PropTypes.string,
+  onSelectedGenreChange: PropTypes.func,
+}.isRequired;
+
+export default SearchBar;
