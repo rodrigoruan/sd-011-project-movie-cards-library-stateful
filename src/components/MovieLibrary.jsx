c146d6628321f99f6023fe815d6f81014025903e
Pedro Castro <pa.coelho.castro@gmail.com>
2021-06-02 10:17:22 -0300

    Innitial Req 16 and full 17

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,19 +1,58 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import SearchBar from './SearchBar';
 
 class MovieLibrary extends Component {
-  render() {
+  constructor(props) {
+    super(props);
     const movies = this.props;
-    return <div />;
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: movies,
+    };
+    this.updateBookmarked = this.updateBookmarked.bind(this);
+    this.updateGenre = this.updateGenre.bind(this);
+    this.updateText = this.updateText.bind(this);
+  }
+
+  updateText(event) {
+    const { value } = event.target;
+    this.setState({
+      searchText: value,
+    });
+  }
+
+  updateBookmarked(event) {
+    const { value } = event.target;
+    this.setState({
+      bookmarkedOnly: value,
+    });
+  }
+
+  updateGenre(event) {
+    const { value } = event.target;
+    this.setState({
+      selectedGenre: value,
+    });
+  }
+
+  render() {
+    const {
+      bookmarkedOnly,
+      searchText,
+      selectedGenre } = this.state;
+    return (
+      <SearchBar
+        bookmarkedOnly={ bookmarkedOnly }
+        searchText={ searchText }
+        selectedGenre={ selectedGenre }
+        onSearchTextChange={ this.updateText }
+        onBookmarkedChange={ this.updateBookmarked }
+        onSelectedGenreChange={ this.updateGenre }
+      />
+    );
   }
 }
-
-MovieLibrary.defaultProps = {
-  movies: [],
-};
-
-MovieLibrary.propTypes = {
-  movies: PropTypes.arrayOf(PropTypes.object),
-};
 
 export default MovieLibrary;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,9 +2,31 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class SearchBar extends Component {
+  renderGenre() {
+    const { selectedGenre, onSelectedGenreChange } = this.props;
+    return (
+      <label
+        htmlFor="selectInput"
+        data-testid="select-input-label"
+      >
+        Filtrar por gênero
+        <select
+          value={ selectedGenre }
+          data-testid="select-input"
+          onChange={ onSelectedGenreChange }
+        >
+          <option value="" data-testid="select-option">Todos</option>
+          <option value="action" data-testid="select-option">Ação</option>
+          <option value="comedy" data-testid="select-option">Comédia</option>
+          <option value="thriller" data-testid="select-option">Suspense</option>
+        </select>
+      </label>
+    );
+  }
+
   render() {
     const { searchText, onSearchTextChange, bookmarkedOnly,
-      onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+      onBookmarkedChange } = this.props;
     return (
       <form data-testid="search-bar-form">
         <label
@@ -33,22 +55,7 @@ class SearchBar extends Component {
             onChange={ onBookmarkedChange }
           />
         </label>
-        <label
-          htmlFor="selectInput"
-          data-testid="select-input-label"
-        >
-          Filtrar por gênero
-          <select
-            value={ selectedGenre }
-            data-testid="select-input"
-            onChange={ onSelectedGenreChange }
-          >
-            <option value="" data-testid="select-option">Todos</option>
-            <option value="action" data-testid="select-option">Ação</option>
-            <option value="comedy" data-testid="select-option">Comédia</option>
-            <option value="thriller" data-testid="select-option">Suspense</option>
-          </select>
-        </label>
+        {this.renderGenre()}
       </form>
     );
   }
