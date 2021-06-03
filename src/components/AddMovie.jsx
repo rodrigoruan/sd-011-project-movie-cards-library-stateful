faa58466fdc06a4b0ccdf38745c55d8096142c85
Michel Pereira <michelbecauro@gmail.com>
2021-06-03 06:09:29 -0300

    feature/Requisitos 6 e 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,36 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+
+export default class AddMovie extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
+    return (
+      <div>
+        <form action="" data-testid="add-movie-form">
+          <span>
+            Só pra não deixar o form vazio vou puxar os states aqui tb:
+            { subtitle }
+            { title }
+            { imagePath }
+            { storyline }
+            { rating }
+            { genre }
+          </span>
+        </form>
+      </div>
+    );
+  }
+}
