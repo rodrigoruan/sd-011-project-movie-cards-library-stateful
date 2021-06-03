d579d2abaa2803886f5c196ff314c05277799029
Julio Cesar <julio.cesarcj1402@gmail.com>
2021-06-03 13:54:42 -0300

    requisito 17

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
 import SearchBar from './SearchBar';
-import MovieList from './MovieList';
-import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
   render() {
     return (
       <div>
         <SearchBar />
-        <MovieList />
-        <AddMovie />
       </div>
     );
   }
