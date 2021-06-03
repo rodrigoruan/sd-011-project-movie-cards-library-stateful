831d2888c0ee2026e015816b5e9974f37dc1a3d4
Rafael Schmitz <schimitaum@gmail.com>
2021-06-02 21:22:46 -0300

    resolve linter

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,35 +1,72 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+// import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
 
-class AddMovie extends Component {
-  constructor() {
-    super();
-    this.state = {
-      subtitle: '',
-      title: '',
-      imagePath: '',
-      storyline: '',
-      rating: 0,
-      genre: 'action',
-    };
-    this.handleFunction = this.handleFunction.bind(this);
-  }
+// class AddMovie extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       subtitle: '',
+//       title: '',
+//       imagePath: '',
+//       storyline: '',
+//       rating: 0,
+//       genre: 'action',
+//     };
+//     this.handleFunction = this.handleFunction.bind(this);
+//   }
 
-  handleFunction() {
-    this.setState({
-      
-    });
-  }
+//   handleForm() {
+//     this.setState({
+//       subtitle: '',
+//       title: '',
+//       imagePath: '',
+//       storyline: '',
+//       rating: 0,
+//       genre: 'action'
+//     });
+//   }
 
-  render() {
-    return (
-      <div />
-    );
-  }
-}
+//   render() {
+//     return (
+//       <form data-testid="add-movie-form">
+//         <label data-testid="title-input-label">
+//           Título
+//           <input
+//             type="text"
+//             value={ this.handleForms }
+//             onChange={ onSearchTextChange }
+//             data-testid="title-input"
+//           />
+//         </label>
+//         <label data-testid="checkbox-input-label">
+//           Mostrar somente favoritos
+//           <input
+//             type="checkbox"
+//             checked={ bookmarkedOnly }
+//             onChange={ onBookmarkedChange }
+//             data-testid="checkbox-input"
+//           />
+//         </label>
+//         <label data-testid="select-input-label">
+//           Filtrar por gênero
+//           <select
+//             value={ selectedGenre }
+//             onChange={ onSelectedGenreChange }
+//             data-testid="select-input"
+//           >
+//             <option value="" data-testid="select-option">Todos</option>
+//             <option value="action" data-testid="select-option">Ação</option>
+//             <option value="comedy" data-testid="select-option">Comédia</option>
+//             <option value="thriller" data-testid="select-option">Suspense</option>
+//           </select>
+//         </label>
+//       </form>
+//     );
+//   }
+// }
 
-AddMovie.propTypes = {
-  OnClick: PropTypes.func.isRequired,
-};
+// AddMovie.propTypes = {
+//   OnClick: PropTypes.func.isRequired,
+// };
 
-export default AddMovie;
+// export default AddMovie;
