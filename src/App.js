81e186e1dccfa8802d061817db58b0018a2e46ab
Leandro Lopes <leandro_carlos@uol.com.br>
2021-06-03 18:55:39 -0300

    Arummando barras de input

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
-import AddMovie from './components/AddMovie';
 import Data from './data';
 import MovieLibrary from './components/MovieLibrary';
 
@@ -9,7 +8,6 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <AddMovie />
       <MovieLibrary movies={ Data } />
     </div>
   );
