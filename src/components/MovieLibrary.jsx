d9d1309ec81d62ffdf75793efba5afb5609e9d55
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-06-03 00:48:50 -0300

    MovieLibrary structure

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,14 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieLibrary extends Component {
+  render() {
+    return <p>Test</p>;
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object),
+}.isRequired;
+
+export default MovieLibrary;
