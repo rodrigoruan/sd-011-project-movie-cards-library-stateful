7348c598c6f84bc2c957d7b292ed56aa4461723d
Michel Pereira <michelbecauro@gmail.com>
2021-06-03 01:16:20 -0300

    feature/Requisito 1

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,20 @@
 // implement AddMovie component here
+import React from 'react';
+
+export default class SearchBar extends React.Component {
+  render() {
+
+    const { searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange } = this.props;
+
+    return (
+      <div>
+        <input type="text" />
+      </div>
+    );
+  }
+}
