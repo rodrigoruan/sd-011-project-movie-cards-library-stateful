4c6befed3a30286fc10cd117b80477b4e31081ef
Nalu <aluziapantoja@gmail.com>
2021-06-04 11:05:41 -0300

    Req 1

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,18 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class SearchBar extends Component {
+  render() {
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+};
+
+export default SearchBar;
