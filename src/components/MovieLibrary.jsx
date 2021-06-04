78c31d18a78b120edda61b3d590a481b33f07fb2
Luan Xavier <luantenorio3@gmail.com>
2021-06-04 16:16:52 -0300

    Requisito 17

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 
@@ -14,6 +13,33 @@ class MovieLibrary extends Component {
       selectedGenre: '',
       movies: props.movies,
     };
+    this.handleSearchText = this.handleSearchText.bind(this);
+    this.handleBookmarkedOnly = this.handleBookmarkedOnly.bind(this);
+    this.handleSelectedGenre = this.handleSelectedGenre.bind(this);
+  }
+
+  handleSearchText({ target }) {
+    const { value } = target;
+
+    this.setState({
+      searchText: value,
+    });
+  }
+
+  handleBookmarkedOnly({ target }) {
+    const { checked } = target;
+
+    this.setState({
+      bookmarkedOnly: checked,
+    });
+  }
+
+  handleSelectedGenre({ target }) {
+    const { value } = target;
+
+    this.setState({
+      selectedGenre: value,
+    });
   }
 
   render() {
@@ -23,8 +49,11 @@ class MovieLibrary extends Component {
         <h2> My awesome movie library </h2>
         <SearchBar
           searchText={ searchText }
+          onSearchTextChange={ this.handleSearchText }
           bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.handleBookmarkedOnly }
           selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.handleSelectedGenre }
         />
         <MovieList movies={ movies } />
       </div>
