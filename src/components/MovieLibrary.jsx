4a1e85e73ef1942f86ff5f22ad305c9904ff6bea
Rafael Schmitz <schimitaum@gmail.com>
2021-06-04 00:39:10 -0300

    req. 15

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,24 +1,42 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import MovieList from './MovieList';
+// import PropTypes from 'prop-types';
 import SearchBar from './SearchBar';
-import AddMovie from './AddMovie';
+//import AddMovie from './AddMovie';
+// import MovieList from './MovieList';
 
 class MovieLibrary extends Component {
+  constructor() {
+    super();
+    // const { movies } = this.props;
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      // movies,
+    };
+  }
+
   render() {
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
     return (
       <div>
         <h2> My awesome movie library </h2>
-        <SearchBar />
-        <AddMovie />
-        <MovieList />
+        <SearchBar
+          searchText={ searchText }
+          bookmarkedOnly={ bookmarkedOnly }
+          selectedGenre={ selectedGenre }
+        />
+        {/*
+        <MovieList movies="" />
+        <AddMovie onClick="" />
+         */}
       </div>
     );
   }
 }
 
-MovieLibrary.propTypes = {
-  movies: PropTypes.exact({ }).isRequired,
-};
+// MovieLibrary.propTypes = {
+//   movies: PropTypes.exact().isRequired,
+// };
 
 export default MovieLibrary;
