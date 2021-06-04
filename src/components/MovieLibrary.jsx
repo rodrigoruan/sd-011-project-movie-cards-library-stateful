06dcdf3ae58c63d24345a600d998b5de89f109f1
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-06-04 12:02:59 -0300

    Add filter option

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -12,42 +12,93 @@ class MovieLibrary extends Component {
       searchText: '',
       bookmarkedOnly: false,
       selectedGenre: '',
-      // movies: props.movies,
+      movies: props.movies,
     };
+
+    this.handlerChange = this.handlerChange.bind(this);
+    this.filterMovies = this.filterMovies.bind(this);
+    this.addMovie = this.addMovie.bind(this);
   }
 
   handlerChange({ target }) {
-    const { name } = target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const { type } = target;
 
-    this.setState({
-      [name]: value,
-    });
+    switch (type) {
+    case 'text':
+      this.setState({
+        searchText: target.value,
+      });
+      break;
+
+    case 'checkbox':
+      this.setState({
+        bookmarkedOnly: target.checked,
+      });
+      break;
+
+    default:
+      this.setState({
+        selectedGenre: target.value,
+      });
+      break;
+    }
+  }
+
+  filterMovies() {
+    const { searchText, selectedGenre, bookmarkedOnly, movies } = this.state;
+
+    let filteredMovies = movies;
+
+    if (bookmarkedOnly === true) {
+      filteredMovies = filteredMovies.filter((movie) => movie.bookmarked === true);
+    }
+
+    if (selectedGenre !== '') {
+      filteredMovies = filteredMovies.filter((movie) => movie.genre === selectedGenre);
+    }
+
+    if (searchText !== '') {
+      filteredMovies = filteredMovies.filter((movie) => movie.title
+        .toLowerCase().includes(searchText)
+        || movie.subtitle.toLowerCase().includes(searchText)
+        || movie.storyline.toLowerCase().includes(searchText));
+    }
+
+    return filteredMovies;
+  }
+
+  addMovie(newMovie) {
+    this.setState((beforeState) => ({
+      movies: [...beforeState.movies, newMovie],
+    }));
   }
 
   render() {
-    const { movies } = this.props;
     const { searchText, bookmarkedOnly, selectedGenre } = this.state;
     return (
       <div>
         <h2> My awesome movie library </h2>
         <SearchBar
           searchText={ searchText }
-          onSearchTextChange
+          onSearchTextChange={ this.handlerChange }
           bookmarkedOnly={ bookmarkedOnly }
-          onBookmarkedChange
+          onBookmarkedChange={ this.handlerChange }
           selectedGenre={ selectedGenre }
-          onSelectedGenreChange
+          onSelectedGenreChange={ this.handlerChange }
         />
-        <MovieList movies={ movies } />
-        <AddMovie />
+        <MovieList movies={ this.filterMovies() } />
+        <AddMovie onClick={ (newMovie) => this.addMovie(newMovie) } />
       </div>
     );
   }
 }
 
 MovieLibrary.propTypes = {
-  movies: PropTypes.string.isRequired,
+  movies: PropTypes.arrayOf(PropTypes.object),
+};
+
+MovieLibrary.defaultProps = {
+  movies: [{}],
 };
 
 export default MovieLibrary;
