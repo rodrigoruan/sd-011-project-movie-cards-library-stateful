5cb2485e148c3b6600738e96337fed799c89643c
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-06-04 13:28:35 -0300

    Adiciona comentarios ao arquivo MovieLibrary.jsx

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -27,6 +27,10 @@ class MovieLibrary extends React.Component {
     });
   }
 
+  /** Essa função abaixo é responsável por fazer a filtragem na barra de pesquisa "Inclui texto"
+   * Ela também faz verificações na checkbox de favoritos e na parte de <select> por gênero.
+   * Não posso deixar de agradecer a todos meus amigos estudantes que me ajudaram a chegar nesta
+   * lógica e finalizar o requisito. Sem sombra de dúvidas, é um requisito muito bacana de ver funcionado */
   filter() {
     const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
     let filteredMovies = movies;
@@ -48,7 +52,9 @@ class MovieLibrary extends React.Component {
     return filteredMovies;
   }
 
-  // Lembrar de referenciar Thyago Pessoa
+  /** Meus agradecimentos a Thyago Pessoa por me fazer entender a lógica por trás desta função
+   * movieAdded abaixo. Como não se pode user o .push devido a alguns problemas relacionados ao React,
+   * o necessário é fazer com spread operator, que espalha os itens dentro de um array específico. */
   movieAdded(anyMovie) {
     const { movies } = this.state;
     this.setState({
@@ -57,10 +63,12 @@ class MovieLibrary extends React.Component {
   }
 
   render() {
-    // Esta desestruturação abaixo refere-se ao uso da prop no componente <MovieList>
+    /** Esta desestruturação abaixo refere-se ao uso da prop no componente <MovieList> */
     const { searchText, bookmarkedOnly, selectedGenre } = this.state;
     return (
-      // Lembrar de adicionar créditos a alberto candido: me ajudou a entender o que o requisito pedia
+      /* Às vezes, alguns requisitos passam despercebidos do meu entendimento, é aí onde eu busco ajuda
+       * para entender melhor o que alguns estão requerindo!
+       * Agradeçõ a ALberto Candido por me ajudar a entender o que o requisito pedia! */
       <div>
         <h1>Filminho e pipoquinha, ui ui ui</h1>
         <SearchBar
