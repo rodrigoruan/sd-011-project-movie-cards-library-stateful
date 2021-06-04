78f941bc8a6ded52e04b78c29f605884b63dac3e
guinhoanjos <75916978+guinhoanjos@users.noreply.github.com>
2021-06-04 11:58:38 -0300

    setando state do movie library

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -3,6 +3,15 @@ import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 
 export default class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: this.props.data,
+    }
+  }
   render() {
     return (
       <div>
