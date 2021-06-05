cf4a7794b9bb67e5eadfbda2718e46d243b77265
Anajulia Brito <juliehindius@gmail.com>
2021-06-05 15:56:44 -0300

    baby steps...

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -15,6 +15,7 @@ class SearchBar extends Component {
     return (
       <form data-testid="search-bar-form">
         <label htmlFor="text-input" data-testid="text-input-label">
+          <p>Adiciona o texto</p>
           <input
             type="search"
             name="searchText"
@@ -25,6 +26,7 @@ class SearchBar extends Component {
         </label>
 
         <label htmlFor="checkbox" data-testid="checkbox-input-label">
+          <p>Os favoritos</p>
           <input
             type="checkbox"
             name="checkbox"
@@ -35,6 +37,7 @@ class SearchBar extends Component {
         </label>
 
         <label htmlFor="select" data-testid="select-input-label">
+          <p>Filtrar por gênero</p>
           <select
             name="select"
             value={ selectedGenre }
