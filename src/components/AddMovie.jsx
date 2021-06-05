078f698b4a95c3b691a53c3272d1d070d3dcd8e9
Paulo Henrique de Paula Bomfim <paulopbomfim@gmail.com>
2021-06-04 22:34:54 -0300

    Requisito 6 concluído - Estados iniciais definidos

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,30 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class AddMovie extends Component {
+  constructor() {
+    super();
+    this.state = {
+      title: '',
+      subtitle: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  // prettier-ignore
+  render() {
+    const { onClick } = this.props;
+    return (
+      <div>
+        <h1>ola</h1>
+      </div>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
