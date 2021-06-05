1b67e81a43f7a686b3da2aaf54c12896e5c1b699
camiloLelis <lafirmafuerte@gmail.com>
2021-06-04 22:33:16 -0300

    requisito-1-tentativa-1

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,15 +1,33 @@
 // implement AddMovie component here
 import React from 'react';
+import PropTypes from 'prop-types';
 
-class SearchBar extends React.Component{
+class SearchBar extends React.Component {
   render() {
     const { searchText, onSearchTextChange, bookmarkedOnly} = this.props;
     const { onBookmarkedChange, selectedGenre, onSelectedGenreChange} = this.props;
     return(
-        <h1>t</h1>
+        <h1></h1>
     );
   }
-
 }
+
+SearchBar.prototype = {
+  searchText: PropTypes.string,
+  onSearchTextChange: PropTypes.func,
+  bookmarkedOnly: PropTypes.bool,
+  onBookmarkedChange: PropTypes.func,
+  selectedGenre: PropTypes.string,
+  onSelectedGenreChange: PropTypes.func,
+};
+
+SearchBar.defaultProps = {
+    searchText: '',
+    onSearchTextChange: () => {},
+    bookmarkedOnly: false,
+    onBookmarkedChange: () => {},
+    selectedGenre: '',
+    onSelectedGenreChange: () => {},
+  };
 
 export default SearchBar;
