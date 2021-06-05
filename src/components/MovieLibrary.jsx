e9930971efb1f0c3abb2b3d467300931165b5531
Michelle <mls.srbek@gmail.com>
2021-06-05 20:02:03 -0300

    movie library component rendering the search bar

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,38 @@
 // implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import SearchBar from './SearchBar';
+
+class MovieLibrary extends React.Component {
+    constructor(props) {
+        super(props);
+
+        const { movies } = this.props;
+        this.state = {
+            searchText: '',
+            bookmarkedOnly: false,
+            selectedGenre: '',
+            movies,
+        }
+    }
+
+    render() {
+        return(
+            <SearchBar />
+        );
+    }
+}
+
+MovieLibrary.propTypes = {
+    movies: PropTypes.arrayOf(
+        PropTypes.shape({
+        title: PropTypes.string,
+        subtitle: PropTypes.string,
+        storyline: PropTypes.string,
+        rating: PropTypes.number,
+        imagePath: PropTypes.string,
+        }),
+    ).isRequired,
+};
+
+export default MovieLibrary;
