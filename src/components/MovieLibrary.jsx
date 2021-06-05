1b9bff04d401c88acbbb098c8b5570dd6370568d
Rafael Schmitz <schimitaum@gmail.com>
2021-06-05 00:07:15 -0300

    yup

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import SearchBar from './SearchBar';
-import AddMovie from './AddMovie';
+// import AddMovie from './AddMovie';
 import MovieList from './MovieList';
 
 class MovieLibrary extends Component {
