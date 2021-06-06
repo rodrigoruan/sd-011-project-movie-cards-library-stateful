5751f014895812784aba2d8dd83bb95bfd2b78cb
camiloLelis <lafirmafuerte@gmail.com>
2021-06-06 16:02:59 -0300

    requisito-6-tentativa-1

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,34 @@
 // implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state= {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyLine: '',
+      rating: '0',
+      genre: 'action',
+    }
+  } 
+  render() {
+    const { onClick } = this.props; 
+    return (
+      <form action="">
+      </form>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+    onClick: PropTypes.func,
+};
+
+AddMovie.defaultProps = {
+    onClick: () => {},
+}
+export default AddMovie;
