04d8132c0e1daa63ce7478d6d109e7386f4f1b3b
Savio Moraes <smmsavio@hotmail.com>
2021-06-06 19:38:26 -0300

    adiciona requisito 15, 16 e 17, o 15 ja estava passando e o 16 dependia do 17 e 18...

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -25,7 +25,7 @@ MovieCard.propTypes = {
     title: PropTypes.string,
     subtitle: PropTypes.string,
     storyline: PropTypes.string,
-    rating: PropTypes.number,
+    rating: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
     imagePath: PropTypes.string,
   }).isRequired,
 };
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,31 +1,61 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
-import movies from '../data';
 
 class MovieLibrary extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   const { movies } = this.props;
-  //   this.handleAddMovies = this.handleAddMovies.bind(this);
-  //   this.state = {
-  //     movies,
-  //   };
+  constructor(props) {
+    super(props);
+    const { movies } = this.props;
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    // this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    // this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+  }
+
+  onSearchTextChange({ target }) {
+    this.setState({
+      searchText: target.value,
+    });
+  }
+
+  // onBookmarkedChange() {
+  //   this.setState({
+
+  //   });
   // }
 
-  // handleAddMovies(movie) {
-  //   this.setState(({ movies }) => ({
-  //     movies: [...movies, movie],
-  //   }));
+  // onSelectedGenreChange() {
+  //   this.setState({
+
+  //   });
+  // }
+
+  // filtersMovies() {
+  //   movies = { movies };
   // }
 
   render() {
+    const { onSearchTextChange, onBookmarkedChange, onSelectedGenreChange } = this;
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
     return (
       <div>
-        <h2> My awesome movie library </h2>
-        <SearchBar />
+        <h2> Minha incrível biblioteca de filmes </h2>
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ onSearchTextChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ onBookmarkedChange }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ onSelectedGenreChange }
+        />
         <MovieList movies={ movies } />
         <AddMovie onclick={ () => {} } />
       </div>
@@ -33,12 +63,12 @@ class MovieLibrary extends Component {
   }
 }
 
-// MovieLibrary.propTypes = {
-//   movies: PropTypes.arrayOf(PropTypes.object),
-// };
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object),
+};
 
-// MovieLibrary.defaultProps = {
-//   movies: '',
-// };
+MovieLibrary.defaultProps = {
+  movies: '',
+};
 
 export default MovieLibrary;
