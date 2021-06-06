d7b71a368057a0400474f95e45d7add207571460
teofilobrandao <teofiloafssbrandao@gmail.com>
2021-06-06 11:20:53 -0300

    Projeto Movie Cards Library Stateful - Envio Item 1

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -7,30 +7,54 @@ class SearchBar extends React.Component {
 
     this.state = {
       teste: '',
+      teste2: '',
     };
   }
 
   handleChange = (event) => {
     this.setState({
       teste: event.target.value,
+      teste2: event.target.value,
     });
   }
 
   render() {
-    const { teste } = this.state;
+    const { teste, teste2 } = this.state;
+    // const { movie: {
+    //   searchText,
+    //   onSearchTextChange,
+    //   bookmarkedOnly,
+    //   onBookmarkedChange,
+    //   selectedGenre,
+    //   onSelectedGenreChange,
+
+    // },
+    // } = this.state;
 
     return (
       <div>
 
-        <form>
+        <form data-testid="search-bar-form">
           <label htmlFor="teste">
             Inclui o texto:
             <input
+              data-testid="text-input-label"
               type="text"
               value={ teste }
               onChange={ this.handleChange }
               name="teste"
               id="teste"
+            />
+          </label>
+
+          <label htmlFor="teste">
+            Input Teste 2:
+            <input
+              type="text"
+              value={ teste2 }
+              onChange={ this.handleChange }
+              name="teste2"
+              id="teste2"
             />
           </label>
         </form>
@@ -39,5 +63,28 @@ class SearchBar extends React.Component {
     );
   }
 }
+
+// SearchBar.propTypes = {
+//   movie: PropTypes.shape({
+//     searchText: PropTypes.string,
+//     onSearchTextChange: PropTypes.func,
+//     bookmarkedOnly: PropTypes.bool,
+//     onBookmarkedChange: PropTypes.func,
+//     selectedGenre: PropTypes.string,
+//     onSelectedGenreChange: PropTypes.func,
+
+//   }),
+// };
+
+// SearchBar.defaultProps = {
+//   movie: {
+//     searchText: '',
+//     onSearchTextChange: '',
+//     bookmarkedOnly: '',
+//     onBookmarkedChange: '',
+//     selectedGenre: '',
+//     onSelectedGenreChange: '',
+//   },
+// };
 
 export default SearchBar;
