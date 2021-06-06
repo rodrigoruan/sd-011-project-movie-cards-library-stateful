66605cbe01f788b307183a69f7f0ec8ea555698e
Cris Lustosa <cmlustosa@gmail.com>
2021-06-06 16:21:35 -0300

    Adiciona requisito 9

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -25,7 +25,7 @@ class AddMovie extends Component {
   }
 
   render() {
-    const { title } = this.state;
+    const { title, subtitle } = this.state;
     return (
       <form data-testid="add-movie-form">
         <label htmlFor="title" data-testid="title-input-label">
@@ -36,6 +36,17 @@ class AddMovie extends Component {
             name="title"
             type="text"
             data-testid="title-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="subtitle" data-testid="subtitle-input-label">
+          Subtítulo
+          <input
+            id="subtitle"
+            value={ subtitle }
+            name="subtitle"
+            type="text"
+            data-testid="subtitle-input"
             onChange={ this.handleChange }
           />
         </label>
