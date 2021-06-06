96c51e0c64b7eb7c90e40ecb3e1adaaf4ab180a6
Michelle <mls.srbek@gmail.com>
2021-06-06 20:33:38 -0300

    add movie component

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,32 @@
 // implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import { render } from 'enzyme';
+
+class AddMovie extends React.Component {
+    constructor(props){
+        super(props);
+        const { onClick } = this.props;
+
+        this.state = {
+            subtitle: '',
+            title: '',
+            imagePath: '',
+            storyline: '',
+            rating: 0,
+            genre: 'action',
+        }
+    }
+
+    render() {
+        return(
+
+        );
+    }
+}
+
+AddMovie.propTypes = {
+
+};
+
+export default AddMovie;
