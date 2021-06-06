54a4eede370660a1a4c196a9e5537dfb5562bbb8
teofilobrandao <teofiloafssbrandao@gmail.com>
2021-06-06 12:57:06 -0300

    Projeto Movie Cards Library Stateful - Envio Item 1

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -7,20 +7,20 @@ class SearchBar extends React.Component {
     super();
 
     this.state = {
-      teste: '',
+      searchText: '',
       teste2: '',
     };
   }
 
   handleChange = (event) => {
     this.setState({
-      teste: event.target.value,
+      searchText: event.target.value,
       teste2: event.target.value,
     });
   }
 
   render() {
-    const { teste, teste2 } = this.state;
+    const { searchText, onSearchTextChange, teste2 } = this.state;
     // const { movie: {
     //   searchText,
     //   onSearchTextChange,
@@ -39,24 +39,24 @@ class SearchBar extends React.Component {
           <label htmlFor="onSearchTextChange" data-testid="text-input-label">
             Inclui o texto:
             <input
-              data-testid="text-input-label"
+              data-testid="text-input"
               type="text"
-              value={ teste }
-              onChange={ this.handleChange }
+              value={ searchText }
+              onChange={ onSearchTextChange }
               name="onSearchTextChange"
               id="onSearchTextChange"
             />
           </label>
 
-          <label htmlFor="onSearchTextChange" data-testid="checkbox-input-label">
+          <label htmlFor="bookmarkedOnly" data-testid="checkbox-input-label">
             Mostrar somente favoritos
             <input
               data-testid="checkbox-input"
               type="checkbox"
               value={ teste2 }
               onChange={ this.handleChange }
-              name="onSearchTextChange"
-              id="onSearchTextChange"
+              name="bookmarkedOnly"
+              id="bookmarkedOnly"
             />
           </label>
         </form>
