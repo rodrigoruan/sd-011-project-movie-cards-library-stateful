ee26ef6183fd78c589c32d3aaef6b49b80ec502b
Lucas Santos <lucassantos_dev@outlook.com>
2021-06-05 21:20:46 -0300

    start

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,9 @@
-// implement AddMovie component here
+import { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    return (algo);
+  }
+}
+
+export default SearchBar;
