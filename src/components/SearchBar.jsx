76922767383c8a31534e7bd4bd0efcd0cfea0794
Nick-713 <pereiranicolas713@gmail.com>
2021-06-06 00:52:42 -0300

    requisito 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -14,10 +14,8 @@ export default class SearchBar extends Component {
 
     return (
       <form data-testid="search-bar-form">
-
         <label htmlFor="text-input" data-testid="text-input-label">
           Inclui o texto:
-
           <input
             type="text"
             value={ searchText }
@@ -25,19 +23,30 @@ export default class SearchBar extends Component {
             data-testid="text-input"
           />
         </label>
-
         <label htmlFor="checkbox" data-testid="checkbox-input-label">
           Mostrar somente favoritos:
-
           <input
             type="checkbox"
             checked={ bookmarkedOnly }
             onChange={ onBookmarkedChange }
             data-testid="checkbox-input"
-
           />
         </label>
+        <label htmlFor="selected-input" data-testid="select-input-label">
+          Filtrar por gênero:
+          <select
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+            data-testid="select-input"
+          >
 
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+
+          </select>
+        </label>
       </form>
     );
   }
