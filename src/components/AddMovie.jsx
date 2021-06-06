4fa19cc0f37a6df348f3613dcd5a913edbc6d1de
Nick-713 <pereiranicolas713@gmail.com>
2021-06-06 01:39:21 -0300

    requisito 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,29 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class AddMovie extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+
+    };
+  }
+
+  render() {
+    const { onClick } = this.props;
+    return (
+      <div />
+    );
+  }
+}
+
+AddMovie.prototype = {
+  onClick: PropTypes.func.isRequired,
+};
