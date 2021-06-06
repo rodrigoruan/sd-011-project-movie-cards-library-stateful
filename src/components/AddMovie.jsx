205e03e0aa010e6f68556fd922bda20cf9d117dd
Antonio Malato <malatoneto@gmail.com>
2021-06-06 11:41:06 -0300

    Requisito 9

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -14,12 +14,30 @@ class AddMovie extends React.Component {
     };
   }
 
+  handlerCheck({ event }) {
+    console.log('Olá');
+  }
+
   render() {
+    const { subtitle, title, imagePath, storyLine, rating, genre } = this.props;
+
     return (
       <form data-testid="add-movie-form">
         <label htmlFor="title-input-label" data-testid="title-input-label">
           Título
-          <input type="text" data-testid="title-input" />
+          <input type="text" data-testid="title-input" onChange={ title } />
+        </label>
+        <label htmlFor="subtitle-input-label" data-testid="subtitle-input-label">
+          Subtítulo
+          <input type="text" data-testid="subtitle-input" onChange={ subtitle } />
+        </label>
+        <label htmlFor="image-input-label" data-testid="image-input-label">
+          Imagem
+          <input type="text" data-testid="image-input" onChange={ imagePath } />
+        </label>
+        <label htmlFor="storyline-input-label" data-testid="storyline-input-label">
+          Sinopse
+          <input type="textarea" data-testid="storyline-input" onChange={ imagePath } />
         </label>
       </form>
     );
