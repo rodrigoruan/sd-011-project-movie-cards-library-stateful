f0f1153d672fbaeeb965ac0ad0cfbe53acadbc16
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-06-06 19:28:12 -0300

    Ajust requirement 14.

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -25,19 +25,18 @@ class AddMovie extends React.Component {
   }
 
   onclick() {
-    this.setState = InitialState;
+    this.setState(InitialState);
   }
 
   hadleState({ target }) {
-    const { name, value } = target;
+    const { name } = target;
+    const value = target.type === 'number' ? Number(target.value) : target.value;
     this.setState(() => ({ [name]: value }));
   }
 
   render() {
     const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
-    // Recebe uma callback no onClik que é enviada por searchBar.
-    // Esse componente vai criar campos de input para um novo filme. Esses valores ficarão no state do Addmovie., ou seja, todos os inputs devem alterar o state do AddMovie
-    // Quando clicacr no botão o mesmo deve adicionar uma nova tag de filme na lista de filmes que fica no state no searchbar para que searche bar mande para a biblioteca renderizar.
+
     return (
       <form data-testid="add-movie-form">
         <InputTitlemovie
