10601bfe4b91ef57270fa6926700a67d5decab1d
Nick-713 <pereiranicolas713@gmail.com>
2021-06-05 23:23:15 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Header from './components/Header';
+import './App.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
+  }
+}
+
+/* import React from 'react';
 import Header from './components/Header';
 import './App.css';
 
@@ -11,3 +25,4 @@ function App() {
 }
 
 export default App;
+ */
