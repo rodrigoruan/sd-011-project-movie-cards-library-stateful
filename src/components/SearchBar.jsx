31846000da526153f1a9907ae5d04f88be6c02ea
Nick-713 <pereiranicolas713@gmail.com>
2021-06-05 23:55:01 -0300

    texto da label (requisito 3) e requisito 4

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -16,11 +16,25 @@ export default class SearchBar extends Component {
       <form data-testid="search-bar-form">
 
         <label htmlFor="text-input" data-testid="text-input-label">
+          Inclui o texto:
+
           <input
             type="text"
             value={ searchText }
             onChange={ onSearchTextChange }
             data-testid="text-input"
+          />
+        </label>
+
+        <label htmlFor="checkbox" data-testid="checkbox-input-label">
+          Mostrar somente favoritos:
+
+          <input
+            type="checkbox"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+            data-testid="checkbox-input"
+
           />
         </label>
 
