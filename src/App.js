eaff632e6e89790ca3e00e51d100c50c752cc606
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-06-06 20:08:58 -0300

    Requisitos 15, 16 e 17 - cria o componente MovieLibrary e inicia logica de busca

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-import MovieList from './components/MovieList';
+import MovieLibrary from './components/MovieLibrary';
+/* import MovieList from './components/MovieList';
+import AddMovie from './components/AddMovie';
+import SearchBar from './components/SearchBar'; */
 import Data from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieLibrary movies={ Data } />
+      {/*       <SearchBar />
       <MovieList movies={ Data } />
+      <AddMovie /> */}
     </div>
   );
 }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,5 +1,5 @@
-/* import React, { Component } from 'react';
-
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
@@ -7,19 +7,91 @@ import AddMovie from './AddMovie';
 class MovieLibrary extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: props.movies, // filmes filtrados
+    };
+
+    this.onSearchTextChange = this.onSearchTextChange.bind(this); // callbacks que alteram state
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+  }
+
+  onSearchTextChange(event) {
+    const { target } = event;
+    this.setState({
+      searchText: target.value,
+    });
+
+    const { searchText, movies } = this.state;
+    let researchedMovies = movies;
+    if (searchText !== '') {
+      researchedMovies = researchedMovies
+        .filter((movie) => (movie.title.includes(searchText))
+      || (movie.subtitle.includes(searchText))
+      || (movie.storyline.includes(searchText)));
+    }
+    return researchedMovies;
+  }
+
+  onBookmarkedChange(event) {
+    const { target } = event;
+    this.setState({
+      bookmarkedOnly: target.checked,
+    });
+
+    const { bookmarkedOnly, movies } = this.state; // para acessar os valores e conseguir filtrar
+    if (bookmarkedOnly === target.checked) {
+      this.setState({
+        movies: movies.filter((movie) => movie.bookmarked === true),
+      });
+    }
+  }
+
+  onSelectedGenreChange(event) {
+    const { target } = event;
+    this.setState({
+      selectedGenre: target.value,
+    });
+
+    const { selectedGenre, movies } = this.state;
+    if (selectedGenre !== '') {
+      this.setState({
+        movies: movies.filter((movie) => movie.genre === selectedGenre),
+      });
+    }
   }
 
   render() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+
     return (
       <div>
         <h2> My awesome movie library </h2>
-        <SearchBar />
-        <MovieList movies={ this.props.movies } />
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ this.onSearchTextChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.onBookmarkedChange }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.onSelectedGenreChange }
+        />
+        <MovieList movies={ movies } />
         <AddMovie />
       </div>
     );
   }
 }
 
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object),
+};
+
+MovieLibrary.defaultProps = {
+  movies: [{}],
+};
+
 export default MovieLibrary;
- */
