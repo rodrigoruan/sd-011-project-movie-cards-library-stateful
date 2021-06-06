e2b0000d31f6f4406c9c8cfad26e699c0f283caa
camiloLelis <lafirmafuerte@gmail.com>
2021-06-06 19:27:33 -0300

    requisito-11-tentativa-importações erros

---
+++ src/components/textArea.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class TextArea extends React.Component {
+  render() {
+    const { storyLine, change } = this.props; 
+    return(
+        <label htmlFor="textArea" data-testid="storyline-input-label">
+          Sinopse
+          <textarea
+            name="storyLine"
+            id="textArea"
+            value={ storyLine }
+            data-testid="storyline-input"
+            onChange={change}
+            cols="30"
+            rows="10"
+          />
+        </label>    
+    )
+  };
+}
+
+TextArea.propTypes = {
+    storyLine: PropTypes.string,
+}
+
+TextArea.defaultProps = {
+    storyLine: '',
+}
+
+export default TextArea;
