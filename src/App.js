2c6848f46079c78ea232a3b68c56c4489b7d0a79
Julio Cesar <julio.cesarcj1402@gmail.com>
2021-06-07 13:34:42 -0300

    adcionando movielibrary

--- src/App.js
+++ src/App.js
@@ -49,8 +49,7 @@ class App extends Component {
           selectedGenre={ genre }
           onSelectedGenreChange={ this.searchGenre }
         />
-        <MovieLibrary />
-        <AddMovie onChange={ this.createNewMovieItem } />
+        <MovieLibrary movies={ [] } />
       </div>
     );
   }
