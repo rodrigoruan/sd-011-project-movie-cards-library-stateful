5a770169c78d9f7ad8c44947928de1b77f8d5e30
Cris Lustosa <cmlustosa@gmail.com>
2021-06-07 12:01:15 -0300

    Adicionando componente InputStoryline ao stage

---
+++ src/components/InputStoryline.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+import PropTypes from 'prop-types';
+
+class InputStoryline extends Component {
+  render() {
+    const { handleChange, storyline } = this.props;
+    return (
+      <label htmlFor="storyline" data-testid="storyline-input-label">
+        Sinopse
+        <textarea
+          name="storyline"
+          id="storyline"
+          data-testid="storyline-input"
+          value={ storyline }
+          onChange={ handleChange }
+        />
+      </label>
+    );
+  }
+}
+InputStoryline.propTypes = {
+  storyline: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired,
+};
+
+export default InputStoryline;
