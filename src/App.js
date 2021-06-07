078c34b7097c5c776ae9c586f61de1ade92749b8
Julio Cesar <julio.cesarcj1402@gmail.com>
2021-06-07 13:44:39 -0300

    removendo addmovie

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
 import SearchBar from './components/SearchBar';
-import AddMovie from './components/AddMovie';
 import MovieLibrary from './components/MovieLibrary';
 import './App.css';
 
