332f706d025905fea90aa87c34fa9e7d69260c6e
camiloLelis <lafirmafuerte@gmail.com>
2021-06-07 17:55:34 -0300

    requisito-15-tentat-3

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -3,14 +3,12 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 
 class MovieLibrary extends React.Component {
-
   render() {
     // const { movies } = this.props;
     return (
     <div>
-          eeeeeeeeee
-      </div>
-
+        eeeeeeeeee
+    </div>
     );
   }
 }
