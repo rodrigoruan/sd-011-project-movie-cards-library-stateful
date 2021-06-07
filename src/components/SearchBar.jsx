5a56c396b12de38434a30f566fedc1a7a045dc34
Alan Gonçalves <vekio.soft@gmail.com>
2021-06-07 11:52:51 -0300

    conclusão requisito 2

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -23,7 +23,35 @@ class SearchBar extends React.Component {
         onBookmarkedChange={ onBookmarkedChange }
         selectedGenre={ selectedGenre }
         onSelectedGenreChange={ onSelectedGenreChange }
-      />
+      >
+        {/* 2 - Renderize um formulário dentro de <SearchBar /> */}
+        <form data-testid="search-bar-form">
+          {/* 3 - Renderize um input tipo texto dentro do formulário em <SearchBar /> */}
+          <label data-testid="text-input-label" htmlFor="">Inclui o texto:</label>
+          <input type="text" value={ searchText } onChange={ onSearchTextChange } />
+          {/* 4 - Renderize um input do tipo checkbox no formulário em <SearchBar /> */}
+          <label data-testid="checkbox-input-label" htmlFor="">
+            Mostrar somente favoritos
+          </label>
+          <input
+            type="text"
+            data-testid="checkbox-input"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+          />
+          {/* 5 - Renderize um select dentro do formulário em <SearchBar /> */}
+          <label data-testid="select-input-label" htmlFor="">Filtrar por gênero</label>
+          <select
+            data-testid="select-input"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+          />
+          <option data-testid="select-option" value="">Todos</option>
+          <option data-testid="select-option" value="action">Ação</option>
+          <option data-testid="select-option" value="comedy">Comédia</option>
+          <option data-testid="select-option" value="thriller">Suspense</option>
+        </form>
+      </div>
     );
   }
 }
