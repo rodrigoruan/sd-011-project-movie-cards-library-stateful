1bbac599da72578daa5047f1ae9d7bfdfe3eeefc
guinhoanjos <iago.anjos@hotmail.com>
2021-06-07 19:49:23 -0300

    lint

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 import MovieList from './MovieList';
@@ -6,17 +7,17 @@ import MovieList from './MovieList';
 export default class MovieLibrary extends React.Component {
   constructor(props) {
     super(props);
-   const { data } = this.props;
+    const { data } = this.props;
     this.state = {
       searchText: '',
       bookmarkedOnly: false,
       selectedGenre: '',
       movies: data,
     };
-  this.onSearchTextChange = this.onSearchTextChange.bind(this);
-  this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
-  this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
-  this.filtredMovies = this.filtredMovies.bind(this);
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+    this.filtredMovies = this.filtredMovies.bind(this);
   }
 
   onSearchTextChange(event) {
@@ -45,10 +46,9 @@ export default class MovieLibrary extends React.Component {
     const { searchText, movies, selectedGenre, bookmarkedOnly } = this.state;
     const array = movies.filter((value) => (value.title.includes(searchText))
      || (value.subtitle.includes(searchText))
-     || (value.storyline.includes(searchText))
-     )
-    const array2 = array.filter((value2) => (value2.genre.includes(selectedGenre)))
-    const array3 = array2.filter((value3) => (value3.bookmarked === bookmarkedOnly))
+     || (value.storyline.includes(searchText)));
+    const array2 = array.filter((value2) => (value2.genre.includes(selectedGenre)));
+    const array3 = array2.filter((value3) => (value3.bookmarked === bookmarkedOnly));
     return array3;
   }
 
@@ -69,4 +69,8 @@ export default class MovieLibrary extends React.Component {
       </div>
     );
   }
+}
+
+MovieLibrary.propTypes = {
+  data: PropTypes.array.isRequired,
 }
