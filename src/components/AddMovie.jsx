4fd2e43db497430b44a7bce6bafab2f8b3d8f7fa
Raphael <raphaellfpg@gmail.com>
2021-06-08 12:27:47 -0300

    linter 3

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import MovieTitle from './MovieTitle';
-import MovieSubtitle from './MovieSubtitle';
-import MovieImage from './MovieImage';
-import MovieStoryline from './MovieStoryline';
-import MovieRating from './MovieRating';
-import MovieGenre from './MovieGenre';
+import MovieTitle from './MovieTitle.jsx';
+import MovieSubtitle from './MovieSubtitle.jsx';
+import MovieImage from './MovieImage.jsx';
+import MovieStoryline from './MovieStoryline.jsx';
+import MovieRating from './MovieRating.jsx';
+import MovieGenre from './MovieGenre.jsx';
 
 class AddMovie extends React.Component {
   constructor(props) {
