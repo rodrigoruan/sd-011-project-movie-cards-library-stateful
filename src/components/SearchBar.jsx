7ed57e5f1d329134a2fdfc7771ca2572694076fc
Guilherme Sucupira Oliveira <guilherme.so23@gmail.com>
2021-06-07 23:54:04 -0300

    Exercise 5 concluded

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -11,21 +11,39 @@ class SearchBar extends Component {
       onSelectedGenreChange } = this.props;
     return (
       <form data-testid="search-bar-form">
-        <label data-testid="text-input-label">
+        <label htmlFor="textId" data-testid="text-input-label">
           Inclui o texto:
           <input
+            type="text"
+            id="textId"
             data-testid="text-input"
             value={ searchText }
             onChange={ onSearchTextChange }
           />
         </label>
-        <label data-testid="checkbox-input-label">
+        <label htmlFor="checkboxId" data-testid="checkbox-input-label">
           Mostrar somente favoritos
           <input
+            type="checkbox"
+            id="checkboxId"
             data-testid="checkbox-input"
             checked={ bookmarkedOnly }
             onChange={ onBookmarkedChange }
           />
+        </label>
+        <label htmlFor="selectId" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            id="selectId"
+            data-testid="select-input"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+          >
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
         </label>
       </form>
     );
