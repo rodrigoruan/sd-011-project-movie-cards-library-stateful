f204385face39fa9c5b3398fd1f52bab7bc6681e
Diego Figueirêdo <8kball@gmail.com>
2021-06-07 22:27:32 -0300

    Commit bomba

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,88 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: '',
+      subtitle: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    }
+
+    this.handleOnChangeTitle = this.handleOnChangeTitle.bind(this);
+    this.handleOnChangeSubtitle = this.handleOnChangeSubtitle.bind(this);
+    this.handleOnChangeImagePath = this.handleOnChangeImagePath.bind(this);
+    this.handleOnChangeStoryline = this.handleOnChangeStoryline.bind(this);
+    this.handleOnChangeRating = this.handleOnChangeRating.bind(this);
+    this.handleOnChangeGenre = this.handleOnChangeGenre.bind(this);
+  }
+
+  handleOnChangeTitle({ target }) {
+    this.setState({ title: target.value })
+  }
+
+  handleOnChangeSubtitle({ target }) {
+    this.setState({ subtitle: target.value })
+  }
+
+  handleOnChangeImagePath({ target }) {
+    this.setState({ imagePath: target.value })
+  }
+
+  handleOnChangeStoryline({ target }) {
+    this.setState({ storyline: target.innerText })
+  }
+
+  handleOnChangeRating({ target }) {
+    this.setState({ rating: target.value })
+  }
+
+  handleOnChangeGenre({ target }) {
+    this.setState({ genre: target.value })
+  }
+
+  // handleState(e, property) {
+  //   this.setState({ [property]: target.value })
+  // }
+
+  render() { 
+    const {
+      title,
+      subtitle,
+      imagePath,
+      storyline,
+      rating,
+      genre,
+    } = this.state;
+
+    return (
+      <form data-testid="add-movie-form" action="">
+        <label data-testid="title-input-label" htmlFor="">Título</label>
+        <input data-testid="title-input" type="text" value={ title } onChange={ this.handleOnChangeTitle }/>
+        <label data-testid="subtitle-input-label"htmlFor="">Subtítulo</label>
+        <input data-testid="subtitle-input" type="text" value={ subtitle } onChange={ this.handleOnChangeSubtitle } />
+        <label data-testid="image-input-label" htmlFor="">Imagem</label>
+        <input data-testid="image-input" type="text" src={ imagePath } onChange={ this.handleOnChangeTitle }/>
+        <label data-testid="storyline-input-label" htmlFor="">Sinopse</label>
+        <textarea data-testid="storyline-input" cols="30" rows="10" onChange={ this.handleOnChangeStoryline }>
+          { storyline }
+        </textarea>
+        <label data-testid="rating-input-label" htmlFor="">Avaliação</label>
+        <input data-testid="rating-input" type="number" value={ rating } onChange={ this.handleOnChangeRating }/>
+        <label data-testid="genre-input-label" htmlFor="">Gênero</label>
+        <select data-testid="genre-input" value={ genre } onChange={ this.handleOnChangeGenre }>
+          <option data-testid="genre-option" value="action">Ação</option>
+          <option data-testid="genre-option" value="comedy">Comédia</option>
+          <option data-testid="genre-option" value="thriller">Suspense</option>
+        </select>
+        <button data-testid="send-button">Adicionar filme</button>
+      </form>
+    );
+  }
+}
+ 
+export default AddMovie;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,50 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends React.Component {
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
+    return (
+      <div
+        searchText={ searchText }
+        onSearchTextChange={ onSearchTextChange }
+        bookmarkedOnly={ bookmarkedOnly }
+        onBookmarkedChange={ onBookmarkedChange }
+        selectedGenre={ selectedGenre }
+        onSelectedGenreChange={ onSelectedGenreChange }
+      >
+        <form data-testid="search-bar-form">
+          <label data-testid="text-input-label" htmlFor="">Inclui o texto:</label>
+          <input data-testid="text-input" type="text" value={ searchText } onChange={ onSearchTextChange }/>
+          <label data-testid="checkbox-input-label" htmlFor="">Mostrar somente favoritos</label>
+          <input data-testid="checkbox-input" type="text" data-testid="checkbox-input" checked={ bookmarkedOnly } onChange={ onBookmarkedChange } />
+          <label data-testid="select-input-label" htmlFor="">Filtrar por gênero</label>
+          <select data-testid="select-input" name="" id="" value={ selectedGenre } onChange={ onSelectedGenreChange }>
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
+        </form>
+      </div>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
+};
+
+export default SearchBar;
