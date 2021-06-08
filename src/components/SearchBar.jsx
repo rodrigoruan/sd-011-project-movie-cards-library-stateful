13d1e9a60a051836954f193bc22dba9fbac73369
Matheus Costa <mathws.costag@gmail.com>
2021-06-08 03:45:44 -0300

    Cria um input do tipo checkbox

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,46 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends React.Component {
+  render() {
+    const
+      { searchText,
+        onSearchTextChange,
+        bookmarkedOnly,
+        onBookmarkedChange,
+      } = this.props;
+    return (
+      <form data-testid="search-bar-form">
+        <label htmlFor="inpInclude">
+          Inclui o texto:
+          <input
+            type="text"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+            data-testid="text-input"
+            id="inpInclude"
+          />
+        </label>
+        <label htmlFor="favCheck">
+          Mostrar somente favoritos
+          <input
+            type="checkbox"
+            id="favCheck"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+            data-testid="checkbox-input"
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.func.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+};
+
+export default SearchBar;
