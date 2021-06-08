d8315de21b98f87861112283fa0c27fc01414837
Guilherme Sucupira Oliveira <guilherme.so23@gmail.com>
2021-06-08 03:05:37 -0300

    Exercise 16 concluded

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
@@ -7,7 +6,13 @@ import AddMovie from './AddMovie';
 class MovieLibrary extends Component {
   constructor(props) {
     super(props);
-  }
+    const { movies } = this.props;
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
 
   render() {
     return (
