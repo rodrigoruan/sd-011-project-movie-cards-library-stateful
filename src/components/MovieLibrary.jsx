1121206fe7c3ec26764adcc073e04e3f722cbdd0
Júlio Mota <juliomotacampos@gmail.com>
2021-06-08 14:35:23 -0300

    Primeiro commit. Vamos com tudo :rocket:

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,13 @@
+import React, { Component } from 'react';
+
 // implement AddMovie component here
+
+class MovieLibrary extends Component {
+  render() {
+    return (
+      <div> Movie Library Component </div>
+    );
+  }
+}
+
+export default MovieLibrary;
