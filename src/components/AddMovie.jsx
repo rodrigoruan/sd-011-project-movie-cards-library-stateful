1a7cc21223aaa39210dc3bbc52229504e26d1cb2
Guilherme Sucupira Oliveira <guilherme.so23@gmail.com>
2021-06-08 00:12:03 -0300

    Exercise 6 concluded

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,28 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+    onClick = this.props;
+  render() {
+    const { 
+      title,
+      subtitle,
+      imagePath,
+      storyline,
+      rating,
+      genre } = this.state;
+
+    return ();
+  }
+}
+
+export default AddMovie;
