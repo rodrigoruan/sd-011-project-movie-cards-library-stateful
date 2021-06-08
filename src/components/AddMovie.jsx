372f8e90ec9fa983ea514bf88736e75beb8f6ff9
Raphael <raphaellfpg@gmail.com>
2021-06-08 12:24:39 -0300

    Conferindo 2

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import MovieTitle from './MovieTitle';
 import MovieSubtitle from './MovieSubtitle';
 import MovieImage from './MovieImage';
