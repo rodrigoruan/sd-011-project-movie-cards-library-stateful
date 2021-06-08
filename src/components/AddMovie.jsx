e3db52c8de2a41735147048a25d61cc80ec4f6de
IgorMendez <igor.emp@hotmail.com>
2021-06-08 15:11:26 -0300

    Teste

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,37 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+
+  render() {
+    const { onClick } = this.props;
+    return (
+      console.log(this.state)
+      <form>
+      </form>
+    )
+  }
+}
+
+AddMovie.propTypes = {
+  subtitle: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  imagePath: PropTypes.string.isRequired,
+  storyline: PropTypes.string.isRequired,
+  rating: PropTypes.string.isRequired,
+  genre: PropTypes.string.isRequired,
+};
+
+export default AddMovie;
