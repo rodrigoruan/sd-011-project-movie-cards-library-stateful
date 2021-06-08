a2c00a78699edbe495f3748622ff82189aedded8
AlexSmith7192 <alexsmith7192@gmail.com>
2021-06-07 23:06:33 -0300

    15 - Cria um componente chamado <MovieLibrary />

---
+++ src/components/MovieLibrary.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+// const stateInitial = {
+//   calma: calm,
+// };
+
+class MovieLibrary extends React.Component {
+  // constructor() {
+  //   super();
+
+  //   this.state = stateInitial;
+  // }
+
+  render() {
+    return (
+      <h1>eita</h1>
+    );
+  }
+}
+
+// MovieLibrary.propTypes = {
+//   onClick: PropTypes.func.isRequired,
+// };
+
+export default MovieLibrary;
