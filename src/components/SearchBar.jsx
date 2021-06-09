069183d8f81db0f7923461743c4d11b763d9d4b6
Bernardo Coelho <bernardocezario21@gmail.com>
2021-06-09 17:24:43 -0300

    Complete Exercise 2 & 3

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,7 +3,24 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
   render() {
-    return (<p>SearchBar</p>);
+    const { searchText, onSearchTextChange } = this.props;
+    return (
+      <div>
+        <p>SearchBar</p>
+        <form data-testid="search-bar-form">
+          <label htmlFor="text-input" data-testid="text-input-label">
+            Inclui o texto:
+            <input 
+              id="text-input" 
+              type="text" 
+              data-testid="text-input" 
+              value={ searchText }
+              onChange={ onSearchTextChange }
+            />
+          </label>
+        </form>
+      </div>
+    );
   }
 }
 
