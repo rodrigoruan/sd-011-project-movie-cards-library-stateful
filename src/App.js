1f80fb0eaf4a3bd663e2a722844bb14010bf9602
Anajulia Brito <juliehindius@gmail.com>
2021-06-10 17:45:08 -0300

    quase nada, mas blz

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import SearchBar from './components/SearchBar';
-import AddMovie from './components/AddMovie';
+import MovieLibrary from './components/MovieLibrary';
 import './App.css';
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <Header />
       <SearchBar />
-      <AddMovie />
+      <MovieLibrary movies={ movies } />
     </div>
   );
 }
