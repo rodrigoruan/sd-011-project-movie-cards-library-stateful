cad9b460326c429a3e09c1f48cf95507eaab28c3
Reginara Gomes <refagomes@gmail.com>
2021-06-10 18:04:29 -0300

    Adiciona requisito 13

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -68,6 +68,18 @@ class AddMovie extends Component {
             onChange={}
           />
         </label>
+        <label htmlFor="genre" data-testid="genre-input-label">
+          Gênero
+          <select
+            data-testid="genre-input"
+            value={ genre }
+            onChange={}
+          >
+            <option value="action" data-testid="genre-option">Ação</option>
+            <option value="comedy" data-testid="genre-option">Comédia</option>
+            <option value="thriller" data-testid="genre-option">Suspense</option>
+          </select>
+        </label>
       </form>
     )
   }
