9dc55b088c2bf9eb0dca756e95df7cbef83e232e
heyynat <natalifplima@gmail.com>
2021-06-10 10:25:35 -0400

    Rquisito 14 - componente AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,79 +1,102 @@
 import React from 'react';
 
+const initialState = {
+    subtitle: '',
+    title: '',
+    imagePath: '',
+    storyline: '',
+    rating: 0,
+    genre: 'action',
+}
+
 class AddMovie extends React.Component {
     constructor(props) {
         super(props)
-
-        this.state = {
-            subtitle: '',
-            title: '',
-            imagePath: '',
-            storyline: '',
-            rating: 0,
-            genre: 'action',
-        };
+        
+        this.state = initialState;
+        
+        this.handleChange = this.handleChange.bind(this)
+        this.handleClick = this.handleClick.bind(this)
     };
     
-    render() {
+    handleChange({ target }) {
+        const { name, value } = target;
+        this.setState({
+            [name]: value,
+        });
+    }
+    
+    handleClick(){
         const { onClick } = this.props;
-
+        onClick(this.state)
+        this.setState(initialState);
+    }
+    
+    render() {
+        
         return (
             <form data-testid="add-movie-form">
-                <label data-testid="title-input-label">
-                Título
-                <input
-                data-testid="title-input"
-                name="title"
-                />
-                </label>
-                <label data-testid="subtitle-input-label">
-                Subtítulo
-                <input
-                data-testid="subtitle-input"
-                name="subtitle"
-                />
-                </label>
-                <label data-testid="image-input-label">
-                Imagem
-                <input
-                data-testid="image-input"
-                name="imagePath"
-                />
-                </label>
-                <label data-testid="storyline-input-label">
-                Sinopse
-                <textarea
-                data-testid="storyline-input"
-                name="storyline"
-                />
-                </label>
-                <label data-testid="rating-input-label">
-                Avaliação
-                <input
-                data-testid="rating-input"
-                name="rating"
-                type="number"
-                value={this.state.rating}
-                />
-                </label>
-                <label data-testid="genre-input-label">
-                Gênero
-                <select
-                data-testid="genre-input"
-                name="genre"
-                >
-                <option value="action" data-testid="genre-option">Ação</option>
-                <option value="comedy" data-testid="genre-option">Comédia</option>
-                <option value="thriller" data-testid="genre-option">Suspense</option>
-                </select>
-                </label>
-                <button data-testid="send-button"
-                onClick={onClick}>
-                Adicionar filme
-                </button>
+            <label data-testid="title-input-label">
+            Título
+            <input
+            data-testid="title-input"
+            name="title"
+            value={this.state.title}
+            onChange={this.handleChange}/>
+            </label>
+            <label data-testid="subtitle-input-label">
+            Subtítulo
+            <input
+            data-testid="subtitle-input"
+            name="subtitle"
+            value={this.state.subtitle}
+            onChange={this.handleChange}/>
+            </label>
+            <label data-testid="image-input-label">
+            Imagem
+            <input
+            data-testid="image-input"
+            name="imagePath"
+            value={this.state.imagePath}
+            onChange={this.handleChange}/>
+            </label>
+            <label data-testid="storyline-input-label">
+            Sinopse
+            <textarea
+            data-testid="storyline-input"
+            name="storyline"
+            value={this.state.storyline}
+            onChange={this.handleChange}/>
+            </label>
+            <label data-testid="rating-input-label">
+            Avaliação
+            <input
+            data-testid="rating-input"
+            name="rating"
+            type="number"
+            value={this.state.rating}
+            onChange={this.handleChange}/>
+            </label>
+            <label data-testid="genre-input-label">
+            Gênero
+            <select
+            data-testid="genre-input"
+            name="genre"
+            value={this.state.genre}
+            onChange={this.handleChange}>
+            <option value="action" data-testid="genre-option">Ação</option>
+            <option value="comedy" data-testid="genre-option">Comédia</option>
+            <option value="thriller" data-testid="genre-option">Suspense</option>
+            </select>
+            </label>
+            <button
+            data-testid="send-button"
+            onClick={this.handleClick}>
+            Adicionar filme
+            </button>
             </form>
-        )
+            )
+        }
     }
-}
-
-export default AddMovie;
+    
+    export default AddMovie;
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,31 @@
-// implement AddMovie component here
+import React from 'react';
+import AddMovie from './AddMovie';
+
+class MovieLibrary extends React.Component {
+    constructor(props) {
+      super(props)
+      const { movies } = this.props;
+      this.state = {
+        searchText: '',
+        bookmarkedOnly: false,
+        selectedGenre: '',
+        movies,
+      }
+      this.onClick = this.onClick.bind(this);
+    };
+
+    onClick(e) {
+      e.preventDefault();
+      this.setState((previousState) => ({
+        movies: [...previousState, e]
+      }));
+    }
+
+  render() {
+    return (
+      <AddMovie onClick={this.onClick}/>
+    );
+  }
+}
+
+export default MovieLibrary;
