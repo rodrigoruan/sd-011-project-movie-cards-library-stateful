5bbb99197a6a26b739fc7f6761a80596da9d2ee9
Reginara Gomes <refagomes@gmail.com>
2021-06-10 17:49:40 -0300

    Adiciona requisito 9

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -29,6 +29,16 @@ class AddMovie extends Component {
             onChange={}
           />
         </label>
+        <label htmlFor="subtitle" data-testid="subtitle-input-label">
+          Subtítulo
+          <input
+            name="subtitle"
+            type="text"
+            data-testid="subtitle-input"
+            value={ subtitle }
+            onChange={}
+          />
+        </label>
       </form>
     )
   }
