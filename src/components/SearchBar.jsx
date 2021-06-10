4068dc3ee1ac1dab1992dbbdf6d20b9871e92962
Renato Bispo <renatoslvbispo@gmail.com>
2021-06-10 17:11:24 -0300

    Finish requirements 1, 2 and 3

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,2 +1,29 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
+
+export default function SearchBar(props) {
+  const {
+    searchText,
+    onSearchTextChange,
+    bookmarkedOnly,
+    onBookmarkedChange,
+    selectedGenre,
+    onSelectedGenreChange,
+  } = props;
+
+  return (
+    <form data-testid="search-bar-form">
+      <label htmlFor="includes" data-testid="text-input-label">
+        Inclui o texto:
+        <input
+          type="text"
+          name="includes"
+          id="includes"
+          data-testid="text-input"
+          value={ searchText }
+          onChange={ onSearchTextChange }
+        />
+      </label>
+    </form>
+  );
+}
