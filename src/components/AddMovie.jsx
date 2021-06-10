a6b1ab6ea917f18212ba8692597aaff0afab68ae
Bernardo Coelho <bernardocezario21@gmail.com>
2021-06-10 00:06:18 -0300

    Complete Exercise 6 & 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,15 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+  render() {
+    return (
+      <div>
+        <p>Add Movie</p>
+        <form data-testid="add-movie-form" />
+      </div>
+    );
+  }
+}
+
+export default AddMovie;
