daf10ecc5a0873943914c9dd59795df8d3ab5bd7
fredmartins <fredweb22@gmail.com>
2021-06-10 23:57:52 -0300

    renderizando textArea

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,23 +1,51 @@
 import React, { Component } from 'react';
 
 class AddMovie extends Component {
+  renderTitleInput() {
+    return (
+      <label htmlFor="title-input" data-testid="title-input-label">
+        Título
+        <input id="title-input" type="text" data-testid="title-input" />
+      </label>
+    );
+  }
+
+  renderSubTitleInput() {
+    return (
+      <label htmlFor="subtitle-input" data-testid="subtitle-input-label">
+        Subtítulo
+        <input id="subtitle-input" type="text" data-testid="subtitle-input" />
+      </label>
+    );
+  }
+
+  renderImagemInput() {
+    return (
+      <label htmlFor="image-input" data-testid="image-input-label">
+        Imagem
+        <input id="image-input" type="text" data-testid="image-input" />
+      </label>
+    );
+  }
+
+  renderTextAreaInput() {
+    return (
+      <label htmlFor="storyline-input" data-testid="storyline-input-label">
+        Sinopse
+        <input type="text" id="storyline-input" data-testid="storyline-input" />
+      </label>
+    );
+  }
+
   render() {
     return (
       <>
         <p>Add Movie</p>
         <form data-testid="add-movie-form">
-          <label htmlFor="title-input" data-testid="title-input-label">
-            Título
-            <input id="title-input" type="text" data-testid="title-input" />
-          </label>
-          <label htmlFor="subtitle-input" data-testid="subtitle-input-label">
-            Subtítulo
-            <input id="subtitle-input" type="text" data-testid="subtitle-input" />
-          </label>
-          <label htmlFor="image-input" data-testid="image-input-label">
-            Imagem
-            <input id="image-input" type="text" data-testid="image-input" />
-          </label>
+          {this.renderTitleInput()}
+          {this.renderSubTitleInput()}
+          {this.renderimagemInput()}
+          {this.renderTextAreaInput()}
         </form>
       </>
     );
