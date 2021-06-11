c553de50563c5a0262bdc5ae7bc7bb7a8e840678
jeferson casagrande <jefersoncasagrandedesouza@gmail.com>
2021-06-10 21:08:19 -0300

    atualizando

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,10 +1,40 @@
 // implement AddMovie component here
 import React, { Component } from 'react';
 
+// const InitialState = {
+//   title: '',
+//   subtitle: '',
+//   storyline: '',
+//   rating: 0,
+//   imagePath: '',
+//   genre: 'action',
+// };
+
 class AddMovie extends Component {
+
   render() {
     return (
-      <form data-testid="add-movie-form"> </form>
+      <div>
+        <p>Add Movie</p>
+        <form data-testid="add-movie-form">
+          <label htmlFor="title-input" data-testid="title-input-label">
+            Título
+            <input id="title-input" data-testid="title-input" />
+          </label>
+          <label htmlFor="subtitle-input" data-testid="subtitle-input-label">
+            Subtítulo
+            <input id="subtitle-input" data-testid="subtitle-input" />
+          </label>
+          <label htmlFor="image-input" data-testid="image-input-label">
+            Imagem
+            <input id="image-input" data-testid="image-input" />
+          </label>
+          <label htmlFor="storyline-input" data-testid="storyline-input-label">
+            Sinopse
+            <input id="storyline-input" data-testid="storyline-input" />
+          </label>
+        </form>
+      </div>
     );
   }
 }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,16 @@
 // implement AddMovie component here !
+import React, { Component } from 'react';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+
+export default class MovieLibrary extends Component {
+  render() {
+    return (
+      <div>
+        <h2>My awesome movie library</h2>
+        <SearchBar />
+        <AddMovie />
+      </div>
+    );
+  }
+}
