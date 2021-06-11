c9b12a6f8d92067a6f56cf0241ca13f18939e026
fredmartins <fredweb22@gmail.com>
2021-06-10 22:00:55 -0300

    renderizando form em Add Movie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 class AddMovie extends Component {
   render() {
     return (
-      <p>Add Movie</p>
+      <>
+        <p>Add Movie</p>
+        <form data-testid="add-movie-form" />
+      </>
     );
   }
 }
