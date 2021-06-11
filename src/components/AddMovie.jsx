560977adecb27787d7205ea23d19ee8defb3b51b
fredmartins <fredweb22@gmail.com>
2021-06-10 22:57:30 -0300

    renderizando inpt e label dentro do form em AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -5,7 +5,12 @@ class AddMovie extends Component {
     return (
       <>
         <p>Add Movie</p>
-        <form data-testid="add-movie-form" />
+        <form data-testid="add-movie-form">
+          <label htmlFor="title-input" data-testid="title-input-label">
+            Título
+            <input id="title-input" type="text" data-testid="title-input" />
+          </label>
+        </form>
       </>
     );
   }
