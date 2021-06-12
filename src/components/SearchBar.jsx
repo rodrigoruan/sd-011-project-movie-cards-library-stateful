8798d873d5ac63827dba57bba66c36e26912cded
Lucas Santos <lucassantos_dev@outlook.com>
2021-06-12 20:55:16 -0300

    mais devagar q isso, apenas morto

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,35 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 
 class SearchBar extends Component {
+/*
+ searchText, uma string
+bookmarkedOnly, um boolean
+onBookmarkedChange, uma callback
+selectedGenre, uma string
+onSelectedGenreChange, uma callback
+ */
+  // onSearchTextChange, uma callback: acho q no material tem um exempl q se encaixa aqui..
+  onSearchTextChange() {
+    return 1 + 1;
+  }
+
   render() {
-    return (algo);
+    return (
+      <form data-testid="search-bar-form">
+        Inclui o texto
+        <label data-testid="text-input-label" htmlFor="text-input">
+          <input
+            type="text"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+            data-testid="text-input"
+          />
+        </label>
+        <label>
+          <input type="checkbox" name="" id="" />
+        </label>
+      </form>
+    );
   }
 }
 
