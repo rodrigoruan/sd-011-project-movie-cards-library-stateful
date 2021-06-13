039484ec8befe90399075c9f5f2b26fb2e6d15fa
Luís Seixas <luiseixas2@gmail.com>
2021-06-13 13:32:56 -0300

    refatorando o codigo

---
+++ src/components/StorylineInput.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class StorylineInput extends React.Component {
+  render() {
+    const { funcValue, stateValue } = this.props;
+    return (
+      <label htmlFor="input" data-testid="storyline-input-label">
+        Sinopse
+        <textarea
+          onChange={ funcValue }
+          data-testid="storyline-input"
+          value={ stateValue }
+        />
+      </label>
+    );
+  }
+}
+
+StorylineInput.propTypes = {
+  funcValue: PropTypes.func.isRequired,
+  stateValue: PropTypes.string.isRequired,
+};
+
+export default StorylineInput;
