dd9b7eac859fc0eff4699e7f2fa06b30e471bb22
pmaiamateus <pmaiamateus@gmail.com>
2021-06-14 00:48:43 -0300

    adiciona passo 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -11,13 +11,31 @@ class AddMovie extends Component{
       storyline: '',
       rating: 0,
       genre: 'action'
-    }
+    };
+    this.changer = this.changer.bind(this)
   }
+
+  changer({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render(){
     const { title, subtitle, imagePath, storyline, rating, genre} = this.state
     return (
       <form data-testid="add-movie-form">
-
+        <label htmlFor="title-input" data-testid="title-input-label">
+          Título
+          <input
+            type="text"
+            data-testid="title-input"
+            name="title"
+            onChange={ this.changer }
+            value={ title }
+          />
+        </label>
       </form>
     )
   }
