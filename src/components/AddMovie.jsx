902249c71fa0c349f94f4377f0a5b8ff131971c1
pmaiamateus <pmaiamateus@gmail.com>
2021-06-14 00:36:08 -0300

    adiciona passos 6 e 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,26 @@
-// implement AddMovie component here
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class AddMovie extends Component{
+  constructor(){
+    super();
+    this.state = {
+      title: '',
+      subtitle: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action'
+    }
+  }
+  render(){
+    const { title, subtitle, imagePath, storyline, rating, genre} = this.state
+    return (
+      <form data-testid="add-movie-form">
+
+      </form>
+    )
+  }
+}
+
+export default AddMovie
