0812e22b620f2edc1ced13e60dc86c38efa3dae0
Genivaldo <cgenivaldo46@gmail.com>
2021-06-14 08:08:30 -0300

    pra começar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,14 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+  render() {
+    return (
+      <form>
+        <label htmlFor="myLabel">
+          <input name="name" type="text" placeholder="escreva algo" />
+        </label>
+      </form>
+    );
+  }
+}
