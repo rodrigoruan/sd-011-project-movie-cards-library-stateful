84b8a6866d815b700a3d2430f2d760c8a3fafb27
Michelle <mls.srbek@gmail.com>
2021-06-15 20:50:39 -0300

    added associated text to label

--- src/components/InputGenre.jsx
+++ src/components/InputGenre.jsx
@@ -6,6 +6,7 @@ class InputGenre extends Component {
         const { value, handleChange } = this.props;
         return(
             <label htmlFor="genre" data-testid="genre-input-label">
+                Gênero
                 <select data-testid="genre-input" name="genre"value={ value } onChange={ handleChange }>
                     <option data-testid="genre-option" value="action">Ação</option>
                     <option data-testid="genre-option" value="comedy">Comédia</option>
