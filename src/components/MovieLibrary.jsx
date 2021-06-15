359aceab3cc3e09c9a366e722e51a44c2e8eeafb
Bernardo Coelho <bernardocezario21@gmail.com>
2021-06-15 02:26:44 -0300

    Complete Exercises 15, 16 & 17

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,27 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+import AddMovie from './AddMovie';
+
+class MovieLibrary extends Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        <p>Movie Library</p>
+        <SearchBar />
+        <MovieList movies={ movies } />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.string.isRequired,
+};
+
+export default MovieLibrary;
