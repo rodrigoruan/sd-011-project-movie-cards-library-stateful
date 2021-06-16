0737ddc608256c9e7649daa072cb4ddbf2a364d9
Ana Jacomassi <anajacomassip@gmail.com>
2021-06-16 13:10:28 -0300

    renderiza titulo com estado no componente AddMovie criado

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
 
 export default class AddMovie extends Component {
+  constructor() {
+    super();
+    this.state = {
+      title: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { value } = target;
+
+    this.setState({
+      title: value,
+    });
+  }
+
   render() {
+    const { title } = this.state;
     return (
-      <>
-        addmovie
-      </>
+      <form data-testid="add-movie-form">
+        <label htmlFor="title-input-label" data-testid="title-input-label">
+          Título
+          <input
+            type="text"
+            value={ title }
+            data-testid="title-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+
+      </form>
     );
   }
 }
