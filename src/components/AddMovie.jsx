5caa9eb4ee7dfef2254d9cfb0b40309d90f4f3ac
Ana Jacomassi <anajacomassip@gmail.com>
2021-06-16 23:32:29 -0300

    renderiza input de subtitle

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -5,6 +5,7 @@ export default class AddMovie extends Component {
     super();
     this.state = {
       title: '',
+      subtitle: '',
     };
     this.handleChange = this.handleChange.bind(this);
   }
@@ -13,12 +14,12 @@ export default class AddMovie extends Component {
     const { value } = target;
 
     this.setState({
-      title: value,
+      [title]: value,
     });
   }
 
   render() {
-    const { title } = this.state;
+    const { title, subtitle } = this.state;
     return (
       <form data-testid="add-movie-form">
         <label htmlFor="title-input-label" data-testid="title-input-label">
@@ -26,7 +27,18 @@ export default class AddMovie extends Component {
           <input
             type="text"
             value={ title }
+            name="title"
             data-testid="title-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="subtitle-input-label" data-testid="subtitle-input-label">
+          Subtítulo
+          <input
+            type="text"
+            value={ subtitle }
+            name="subtitle"
+            data-testid="subtitle-input"
             onChange={ this.handleChange }
           />
         </label>
