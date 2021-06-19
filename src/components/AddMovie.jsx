20ddf98956d721c25cbfcc3c573d0eb716168bf1
Lais Simenikim <l.simenikim@gmail.com>
2021-06-19 20:13:48 -0300

    add requisito 6, 7 e 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -6,20 +6,38 @@ class AddMovie extends React.Component {
     super();
 
     this.state = {
-      subtitle: '',
       title: '',
-      imagePath: '',
-      storyline: '',
-      rating: 0,
-      genre: 'action',
+    //   subtitle: '',
+    //   imagePath: '',
+    //   storyline: '',
+    //   rating: 0,
+    //   genre: 'action',
     };
+
+    this.onTitleChange = this.onTitleChange.bind(this);
+  }
+
+  onTitleChange(event) {
+    const newTitle = event.target.value;
+    this.setState({ title: newTitle });
   }
 
   render() {
+    const { title } = this.state;
     const { onClick } = this.props;
 
     return (
-      <form onClick={ () => {} } />
+      <form data-testid="add-movie-form">
+        <label htmlFor="AddMovie__title" data-testid="title-input-label">
+          Título
+          <input
+            id="AddMovie__title"
+            data-testid="title-input"
+            value={ title }
+            onChange={ this.onTitleChange }
+          />
+        </label>
+      </form>
     );
   }
 }
