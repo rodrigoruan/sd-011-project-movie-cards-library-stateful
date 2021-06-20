d9fe2f7ccd361436a2b01ff0a110032bcd7248b0
Lais Simenikim <l.simenikim@gmail.com>
2021-06-20 18:27:07 -0300

    add requisito 12

---
+++ src/components/AddMovieRating.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovieRating extends React.Component {
+  render() {
+    const { value, onChange } = this.props;
+
+    return (
+      <label
+        htmlFor="AddMovie__rating"
+        data-testid="rating-input-label"
+      >
+        Avaliação
+        <input
+          type="number"
+          id="AddMovie__rating"
+          data-testid="rating-input"
+          value={ value }
+          onChange={ onChange }
+        />
+      </label>
+    );
+  }
+}
+
+AddMovieRating.propTypes = {
+  value: PropTypes.number.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default AddMovieRating;
