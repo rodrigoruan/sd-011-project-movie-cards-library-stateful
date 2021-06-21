5a1495e57c02b1afbdab91f8b3f101d18bded001
fredmartins <fredweb22@gmail.com>
2021-06-21 15:40:02 -0300

    renderizando componentes em movie library

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,28 +1,43 @@
 import React, { Component } from 'react';
-// import MovieList from './MovieList';
+import PropTypes from 'prop-types';
+import MovieList from './MovieList';
 import SearchBar from './SearchBar';
-// import AddMovie from './AddMovie';
+import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     searchText: '',
-  //     bookmakedOnly: false,
-  //     selectedGenre: '',
+  constructor(props) {
+    super(props);
+    const { movies } = this.props;
 
-  //   };
-  // }
+    this.state = {
+      movies,
+    };
+
+    this.handleAddMovie = this.handleAddMovie.bind(this);
+  }
+
+  handleAddMovie(newMovie) {
+    const { movies } = this.state;
+    this.setState({
+      movies: [...movies, newMovie],
+    });
+  }
 
   render() {
+    const { movies } = this.state;
     return (
       <>
         <p>Mivie Library</p>
         <SearchBar />
+        <MovieList movies={ movies } />
+        <AddMovie onCLick={ this.handleAddMovie } />
       </>
-
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object),
+}.isRequired;
 
 export default MovieLibrary;
