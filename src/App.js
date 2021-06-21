0626dd9441b5aa3b7eef5ff3244a8c3e6e0301f9
fredmartins <fredweb22@gmail.com>
2021-06-21 16:03:34 -0300

    corrigindo renderizacao de componentes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import Header from './components/Header';
 import './App.css';
+
+import Header from './components/Header';
+import MovieLibrary from './components/MovieLibrary';
+
+import movies from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieLibrary movie={ movies } />
     </div>
   );
 }
