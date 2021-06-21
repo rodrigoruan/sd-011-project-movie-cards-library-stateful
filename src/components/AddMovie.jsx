4adfb53cd395b38a7f517d90b231866aa3bd3445
LuanAlexandre <luanrb19@gmail.com>
2021-06-21 17:13:42 -0400

    Dia 12.3 - projeto-movie-cards-library-stateful: requisito 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,5 +1,6 @@
 // implement AddMovie component here
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class AddMovie extends Component {
   constructor(props) {
@@ -13,12 +14,45 @@ export default class AddMovie extends Component {
       rating: 0,
       genre: 'action',
     };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState(() => ({
+      [name]: value,
+    }));
   }
 
   render() {
     const { onClick } = this.props;
+    const { title } = this.state;
     return (
-      <form data-testid="add-movie-form"></form>
-    )
+      <form data-testid="add-movie-form">
+        <label htmlFor="add-movie-title-input" data-testid="title-input-label">
+          Título
+          <input
+            data-testid="title-input"
+            name="title"
+            id="add-movie-title-input"
+            value={ title }
+            onChange={ this.handleChange }
+          />
+        </label>
+      </form>
+    );
   }
 }
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
+  imagepath: PropTypes.string.isRequired,
+  storyline: PropTypes.string.isRequired,
+  ratign: PropTypes.number.isRequired,
+  genre: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired,
+};
