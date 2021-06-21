0f341fb3bd8935541f99c908bdc68929e3d9a37b
fredmartins <fredweb22@gmail.com>
2021-06-21 08:10:10 -0300

    ultimas alterações

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class AddMovie extends Component {
   renderTitleInput() {
     return (
       <label htmlFor="title-input" data-testid="title-input-label">
         Título
-        <input type="text" id="title-input" data-testid="title-input" />
+        <input
+          type="text"
+          id="title-input"
+          data-testid="title-input"
+        />
       </label>
     );
   }
@@ -14,7 +19,11 @@ class AddMovie extends Component {
     return (
       <label htmlFor="subtitle-input" data-testid="subtitle-input-label">
         Subtítulo
-        <input id="subtitle-input" type="text" data-testid="subtitle-input" />
+        <input
+          id="subtitle-input"
+          type="text"
+          data-testid="subtitle-input"
+        />
       </label>
     );
   }
@@ -23,18 +32,68 @@ class AddMovie extends Component {
     return (
       <label htmlFor="image-input" data-testid="image-input-label">
         Imagem
-        <input type="text" id="image-input" data-testid="image-input" />
+        <input
+          type="text"
+          id="image-input"
+          data-testid="image-input"
+        />
       </label>
     );
   }
 
-  renderTextAreaInput() {
+  renderStoryLine() {
     return (
       <label htmlFor="storyline-input" data-testid="storyline-input-label">
         Sinopse
-        <textarea id="storyline-input" data-testid="storyline-input" />
+        <input
+          type="text"
+          id="storyline-input"
+          data-testid="storyline-input"
+        />
       </label>
     );
+  }
+
+  renderRatingInput() {
+    return (
+      <label htmlFor="rating-input" data-testid="rating-input-label">
+        Avaliação
+        <input
+          type="number"
+          id="rating-input"
+          data-testid="rating-input"
+          defaultValue={ 0 }
+        />
+      </label>
+    );
+  }
+
+  renderGenderInput() {
+    return (
+      <label htmlFor="genre-input" data-testid="genre-input-label">
+        Gênero
+        <select
+          id="genre-input"
+          data-testid="genre-input"
+        >
+          <option value="action" data-testid="select-option">Ação</option>
+          <option value="comedy" data-testid="select-option">Comédia</option>
+          <option value="thriller" data-testid="select-option">Suspense</option>
+        </select>
+      </label>
+    );
+  }
+
+  renderButton() {
+    const { onClick } = this.props;
+    return (
+      <button
+        type="button"
+        onClick={ onClick }
+        data-testid="send-button"
+      >
+        Adicionar filme
+      </button>);
   }
 
   render() {
@@ -45,11 +104,18 @@ class AddMovie extends Component {
           {this.renderTitleInput()}
           {this.renderSubTitleInput()}
           {this.renderImagemInput()}
-          {this.renderTextAreaInput()}
+          {this.renderStoryLine()}
+          {this.renderRatingInput()}
+          {this.renderGenderInput()}
+          {this.renderButton()}
         </form>
       </>
     );
   }
 }
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func,
+}.isRequired;
 
 export default AddMovie;
