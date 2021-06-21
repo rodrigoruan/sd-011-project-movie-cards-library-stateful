a755c5745d320ce2e6b43aa4f8016c3ccf2f3b73
LuanAlexandre <luanrb19@gmail.com>
2021-06-21 16:00:52 -0400

    Dia 12.3 - projeto-movie-cards-library-stateful: requisito 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,18 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+
+export default class AddMovie extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>MyComponent</div>
+    )
+  }
+}
