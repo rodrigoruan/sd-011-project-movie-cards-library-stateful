38889501c23353ef98bb60ffcff1e27fc392b661
Eduardo-A-P-Teixeira <eduardo.apteixeira00@gmail.com>
2021-06-20 22:33:36 -0300

    Requisitos 1 ao 6 com lint

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,63 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends Component {
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
+    return (
+      <div>
+        <form data-testid="search-bar-form" action="" method="get">
+          <label data-testid="text-input-label" htmlFor="text-input">
+            Inclui o texto
+            <input
+              type="text"
+              data-testid="text-input"
+              onChange={ onSearchTextChange }
+              value={ searchText }
+            />
+          </label>
+          <label data-testid="checkbox-input-label" htmlFor="checkbox-input">
+            Mostrar somente favoritos
+            <input
+              type="checkbox"
+              onChange={ onBookmarkedChange }
+              checked={ bookmarkedOnly }
+              data-testid="checkbox-input"
+            />
+          </label>
+          <label data-testid="select-input-label" htmlFor="select-input">
+            Filtrar por gênero
+            <select
+              value={ selectedGenre }
+              onChange={ onSelectedGenreChange }
+              data-testid="select-input"
+            >
+              <option data-testid="select-option" value="">Todos</option>
+              <option data-testid="select-option" value="action">Ação</option>
+              <option data-testid="select-option" value="comedy">Comédia</option>
+              <option data-testid="select-option" value="thriller">Suspense</option>
+            </select>
+          </label>
+        </form>
+      </div>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
+};
+
+export default SearchBar;
