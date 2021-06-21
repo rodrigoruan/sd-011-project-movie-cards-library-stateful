c21734e84d23c8802d26ac1d55d80bf1a8478e40
LuanAlexandre <luanrb19@gmail.com>
2021-06-21 15:54:18 -0400

    Dia 12.3 - projeto-movie-cards-library-stateful: requisito 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -10,9 +10,6 @@ export default class SearchBar extends Component {
     return (
       <form
         data-testid="search-bar-form"
-        onBookmarkedChange={ onBookmarkedChange }
-        selectedGenre={ selectedGenre }
-        onSelectedGenreChange={ onSelectedGenreChange }
       >
         <label htmlFor="input-text" data-testid="text-input-label">
           Inclui o texto:
@@ -33,6 +30,20 @@ export default class SearchBar extends Component {
             checked={ bookmarkedOnly }
             onChange={ onBookmarkedChange }
           />
+        </label>
+        <label htmlFor="select-input" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            data-testid="select-input"
+            id="select-input"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+          >
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
         </label>
       </form>
     );
