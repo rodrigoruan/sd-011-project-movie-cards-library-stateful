1a6650e620e36633768f33b430b7d3b6f7e3efd7
Kevin Willian <kevinwillian084@gmail.com>
2021-06-21 16:02:12 -0300

    Adiciona requisitos 6, 7 e 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,54 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    const { onClick } = this.props;
+    return (
+      <form data-testid="add-movie-form">
+        <label htmlFor="title">
+          Titulo
+          <input id="title" />
+        </label>
+        <label htmlFor="title">
+          Subtítulo
+          <input id="title" />
+        </label>
+        <label htmlFor="title">
+          Imagem
+          <input id="title" />
+        </label>
+        <label htmlFor="title">
+          Sinopse
+          <input id="title" />
+        </label>
+        <label htmlFor="title">
+          Avaliação
+          <input id="title" />
+        </label>
+        <label htmlFor="title">
+          Gênero
+          <input id="title" />
+        </label>
+      </form>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+
+}.isRequired;
+
+export default AddMovie;
