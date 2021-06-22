6a2bd46a7e8b231e3509977055bec8ebfe062b06
Rafael Bamberg <raafaelbamberg@gmail.com>
2021-06-21 23:13:18 -0300

    requisito 1 e 2 fix lint

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,31 @@
-import React from 'react';
+// implement AddMovie component here
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-class SearchBar extends React.component {
-    render() {
-      const { searchText, onSearchTextChange,
-        bookmarkedOnly, onBookmarkedChange, 
-        selectedGenre, onSelectedGenreChange } = this.props
-      return (
-        <form data-testid="search-bar-form">
-        </form>
-      )
-    }
+export default class SearchBar extends Component {
+  render() {
+    const { searchText, onSearchTextChange,
+      bookmarkedOnly, onBookmarkedChange,
+      selectedGenre, onSelectedGenreChange } = this.props;
+    return (
+      <form
+        data-testid="search-bar-form"
+        searchText={ searchText }
+        onSearchTextChange={ onSearchTextChange }
+        bookmarkedOnly={ bookmarkedOnly }
+        onBookmarkedChange={ onBookmarkedChange }
+        selectedGenre={ selectedGenre }
+        onSelectedGenreChange={ onSelectedGenreChange }
+      />
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
+};
