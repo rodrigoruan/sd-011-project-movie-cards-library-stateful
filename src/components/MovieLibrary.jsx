37804dbf0d0cbc892f4aa84b2bebde53b5b4ec89
LuanAlexandre <luanrb19@gmail.com>
2021-06-21 22:24:54 -0400

    Dia 12.3 - projeto-movie-cards-library-stateful: requisito 15

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,19 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+
+export default class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>MyComponent</div>
+    );
+  }
+}
