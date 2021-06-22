b25868e01315409801a22ee0ae6a5195f2949287
Wendell Costa <wendellcosta9141@gmail.com>
2021-06-22 18:24:01 -0300

    [EsLint]

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,35 +2,46 @@ import React from 'react';
 import PropTypes from 'prop-types';
 /* Requisito 6 */
 class AddMovie extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            subtitle: '',
-            title: '',
-            imagePath: '',
-            storyline: '',
-            rating: 0,
-            genre: 'action',
-        };
-        this.stateSet = this.stateSet.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+    this.stateSet = this.stateSet.bind(this);
+  }
 
-    stateSet() {
-        const { onClick } = this.props;
-        onClick();
-    }
-
-    render() {
-        return (
-            <form data-testid="add-movie-form">
-                
-            </form>
-        );
-    }
+  stateSet() {
+    const { onClick } = this.props;
+    onClick();
+  }
+  /* Requisito 7,8 */
+  render() {
+    const {
+      title,
+    } = this.state;
+    return (
+      <form data-testid="add-movie-form">
+        <label data-testid="title-input-label" htmlFor="text">
+          Título
+                    <input
+            value={title}
+            data-testid="title-input"
+            onChange={this.stateSet}
+            type="text"
+          />
+        </label>
+      </form>
+    );
+  }
 }
 
 AddMovie.propTypes = {
-    onClick: PropTypes.func.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default AddMovie;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,67 +1,68 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
 /* Requisito 1 */
 class SearchBar extends React.Component {
-    render() {
-        const {
-            searchText,
-            onSearchTextChange,
-            bookmarkedOnly,
-            onBookmarkedChange,
-            selectedGenre,
-            onSelectedGenreChange,
-        } = this.props;
-        return (
-            /* Requisito 2 */
-            <form data-testid="search-bar-form">
-                {/* Requisito 3 */}
-                <label data-testid="text-input-label" htmlFor="text">
-                    Inclui o texto
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
+    return (
+      /* Requisito 2 */
+      <form data-testid="search-bar-form">
+        {/* Requisito 3 */}
+        <label data-testid="text-input-label" htmlFor="text">
+          Inclui o texto
                     <input
-                        value={searchText}
-                        onChange={onSearchTextChange}
-                        data-testid="text-input"
-                        type="text"
-                    />
-                </label>
-                {/* Requisito 4 */}
-                <label data-testid="checkbox-input-label" htmlFor="text">
-                    Mostrar somente favoritos
+            value={searchText}
+            onChange={onSearchTextChange}
+            data-testid="text-input"
+            type="text"
+          />
+        </label>
+        {/* Requisito 4 */}
+        <label data-testid="checkbox-input-label" htmlFor="text">
+          Mostrar somente favoritos
                     <input
-                        checked={bookmarkedOnly}
-                        onChange={onBookmarkedChange}
-                        data-testid="checkbox-input"
-                        type="checkbox"
-                    />
-                </label>
-                {/* Requisito 5 */}
-                <label data-testid="select-input-label" htmlFor="text">
-                    Filtrar por gênero
+            checked={bookmarkedOnly}
+            onChange={onBookmarkedChange}
+            data-testid="checkbox-input"
+            type="checkbox"
+          />
+        </label>
+        {/* Requisito 5 */}
+        <label data-testid="select-input-label" htmlFor="text">
+          Filtrar por gênero
                     <select
-                        value={selectedGenre}
-                        onChange={onSelectedGenreChange}
-                        data-testid="select-input"
-                    > 
+            value={selectedGenre}
+            onChange={onSelectedGenreChange}
+            data-testid="select-input"
+          >
 
-                    <option value="" data-testid="select-option">Todos</option>
-                    <option value="action" data-testid="select-option">Ação</option>
-                    <option value="comedy" data-testid="select-option">Comédia</option>
-                    <option value="thriller" data-testid="select-option">Suspense</option>
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
 
-                    </select>
-                </label>
-            </form>
-        );
-    }
+          </select>
+        </label>
+      </form>
+    );
+  }
 }
 
 SearchBar.propTypes = {
-    searchText: PropTypes.string.isRequired,
-    onSearchTextChange: PropTypes.func.isRequired,
-    bookmarkedOnly: PropTypes.bool.isRequired,
-    onBookmarkedChange: PropTypes.func.isRequired,
-    selectedGenre: PropTypes.string.isRequired,
-    onSelectedGenreChange: PropTypes.func.isRequired,
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
 };
 
 export default SearchBar;
