44b731c7cb8c4d936475d54de1125e003e202a80
Wendell Costa <wendellcosta9141@gmail.com>
2021-06-22 17:41:04 -0300

    [Requisito 6 - Concluido]

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,36 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+/* Requisito 6 */
+class AddMovie extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            subtitle: '',
+            title: '',
+            imagePath: '',
+            storyline: '',
+            rating: 0,
+            genre: 'action',
+        };
+        this.stateSet = this.stateSet.bind(this);
+    }
+
+    stateSet() {
+        const { onClick } = this.props;
+        onClick();
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+AddMovie.propTypes = {
+    onClick: PropTypes.func.isRequired,
+};
+
+export default AddMovie;
