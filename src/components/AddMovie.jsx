ab3f9c774f37854326172684cc6b03ae1110ca0f
Wendell Costa <wendellcosta9141@gmail.com>
2021-06-22 19:43:00 -0300

    [Requisitos de 8 a 14 - Iniciados / Requisito 15 - Concluido]

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -23,18 +23,71 @@ class AddMovie extends React.Component {
   render() {
     const {
       title,
+      subtitle,
+      imagePath,
+      storyline,
+      rating,
+      genre,
     } = this.state;
     return (
       <form data-testid="add-movie-form">
         <label data-testid="title-input-label" htmlFor="text">
           Título
-                    <input
+          <input
             value={title}
             data-testid="title-input"
             onChange={this.stateSet}
             type="text"
           />
         </label>
+        <label data-testid="subtitle-input-label" htmlFor="text">
+          Subtítulo
+          <input
+            value={subtitle}
+            data-testid="subtitle-input"
+            onChange={this.stateSet}
+            type="text"
+          />
+        </label>
+        <label data-testid="image-input-label" htmlFor="text">
+          Imagem
+          <input
+            value={imagePath}
+            data-testid="image-input"
+            onChange={this.stateSet}
+            type="text"
+          />
+        </label>
+        <label data-testid="storyline-input-label" htmlFor="text">
+          Sinopse
+          <textarea
+            value={storyline}
+            data-testid="storyline-input"
+            onChange={this.stateSet}
+          />
+        </label>
+        <label data-testid="rating-input-label" htmlFor="text">
+          Avaliação
+          <input
+            value={rating}
+            data-testid="rating-input"
+            onChange={this.stateSet}
+            type="number"
+          />
+        </label>
+        <label data-testid="genre-input-label" htmlFor="text">
+          Gênero
+          <select
+            value={genre}
+            data-testid="genre-input"
+            onChange={this.stateSet}
+          >
+            <option value="action" data-testid="genre-option">Ação</option>
+            <option value="comedy" data-testid="genre-option">Comédia</option>
+            <option value="thriller" data-testid="genre-option">Suspense</option>
+          </select>
+        </label>
+        <button type="button" data-testid="send-button">Adicionar filme</button>
       </form>
     );
   }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,18 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieLibrary extends React.Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+MovieLibrary.propTypes = {
+    movies: PropTypes.array.isRequired,
+  };
+  
+export default MovieLibrary;
