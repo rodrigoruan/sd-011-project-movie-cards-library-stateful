721160fe4a522a60c70dda10c5e774ed95f0af5c
Wendell Costa <wendellcosta9141@gmail.com>
2021-06-22 15:49:52 -0300

    [Requisito 2 - Concluido]

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,9 +4,9 @@ import PropTypes from 'prop-types';
 class SearchBar extends React.Component {
     render() {
         return (
-            <div>
+            <form data-testid="search-bar-form">
 
-            </div>
+            </form>
         );
     }
 }
