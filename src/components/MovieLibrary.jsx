83bfa1d73034ae30dedbb3cce65c8b2e86a5bc51
Taís Carvalho <taispcarvalho@gmail.com>
2021-06-22 21:22:56 -0300

    15, 16, 19 lint

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,62 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+import AddMovie from './AddMovie';
+
+class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const { movies } = props;
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick(myState) {
+    const { title, subtitle, storyline, imagePath, rating, genre } = myState;
+    const newMovie = {
+      title,
+      subtitle,
+      storyline,
+      imagePath,
+      rating,
+      genre,
+    };
+    this.setState((oldState) => ({
+      movies: [...oldState.movies, newMovie],
+    }));
+  }
+
+  render() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+
+    return (
+      <div>
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ this.onChangeHandle }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.onChangeHandle }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.onChangeHandle }
+        />
+        <MovieList movies={ movies } />
+        <AddMovie onClick={ this.onClick } />
+      </div>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object),
+}.isRequired;
+
+export default MovieLibrary;
