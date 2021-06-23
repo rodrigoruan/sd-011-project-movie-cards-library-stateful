2d7c26a351b50f36e9954edba5e3e02b9db6a9a1
Wendell Costa <wendellcosta9141@gmail.com>
2021-06-22 21:03:40 -0300

    [Teste EsLint]

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -31,16 +31,17 @@ class AddMovie extends React.Component {
     } = this.state;
     return (
       <form data-testid="add-movie-form">
-        <label data-testid="title-input-label" htmlFor="text">
+        <label data-testid="title-input-label" htmlFor="title">
           Título
           <input
             value={title}
             data-testid="title-input"
             onChange={this.stateSet}
             type="text"
+            name="title"
           />
         </label>
-        <label data-testid="subtitle-input-label" htmlFor="text">
+        <label data-testid="subtitle-input-label" htmlFor="subtitle-input">
           Subtítulo
           <input
             value={subtitle}
@@ -49,7 +50,7 @@ class AddMovie extends React.Component {
             type="text"
           />
         </label>
-        <label data-testid="image-input-label" htmlFor="text">
+        <label data-testid="image-input-label" htmlFor="image-input">
           Imagem
           <input
             value={imagePath}
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -2,6 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class MovieLibrary extends React.Component {
+    constructor(props) {
+        super(props);
+        const { movies } = this.props;
+        this.state = {
+          searchText: '',
+          bookmarkedOnly: false,
+          selectedGenre: '',
+          movies,
+        };
+        
+    }
     render() {
         return (
             <div>
