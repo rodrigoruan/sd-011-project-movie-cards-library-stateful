ecafb782771e3d56b459f56390f982da61b8a589
Rafael Bamberg <raafaelbamberg@gmail.com>
2021-06-23 02:14:46 -0300

    requisito 10 funcao grande

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,9 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-export default class AddMovie extends Component {
-  constructor(props) {
-    super(props)
-
+class AddMovie extends Component {
+  constructor() {
+    super();
     this.state = {
       subtitle: '',
       title: '',
@@ -12,15 +12,71 @@ export default class AddMovie extends Component {
       rating: 0,
       genre: 'action',
     };
+
+    this.handleChange = this.handleChange.bind(this);
   }
+
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState(() => ({
+      [name]: value,
+    }));
   }
 
   render() {
     const { onClick } = this.props;
+    const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
     return (
-      <div>
-        
-      </div>
-    )
+      <form data-testid="add-movie-form">
+        <label htmlFor="title">
+          Titulo
+          <input
+            name="title"
+            id="add-movie-title-input"
+            data-testid="title-input"
+            value={ title }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="subtitle" data-testid="subtitle-input-label">
+          Subtítulo
+          <input
+            name="subtitle"
+            id="add-movie-title-input"
+            data-testid="subtitle-input"
+            value={ subtitle }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="title">
+          Imagem
+          <input id="title" />
+        </label>
+        <label htmlFor="title">
+          Sinopse
+          <input id="title" />
+        </label>
+        <label htmlFor="title">
+          Avaliação
+          <input id="title" />
+        </label>
+        <label htmlFor="title">
+          Gênero
+          <input id="title" />
+        </label>
+      </form>
+    );
   }
 }
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
+  imagepath: PropTypes.string.isRequired,
+  storyline: PropTypes.string.isRequired,
+  ratign: PropTypes.number.isRequired,
+  genre: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired,
+};
