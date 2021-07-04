813d9d0e9a00f29c0c543a0a0076cbb09cdd5bfc
thosijulio <thosijulio@gmail.com>
2021-07-04 19:30:04 -0300

    resolucao exercicio 15

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
-import SearchBar from './components/SearchBar';
-import AddMovie from './components/AddMovie';
+import MovieLibrary from './components/MovieLibrary';
 
 function App() {
-  function teste(state) {
-    console.log(state);
-  }
   return (
     <div className="App">
       <Header />
-      <SearchBar
-        searchText=""
-        onSearchTextChange={ () => {} }
-        bookmarkedOnly={ false }
-        onBookmarkedChange={ () => {} }
-        selectedGenre=""
-        onSelectedGenreChange={ () => {} }
-      />
-      <AddMovie onClick={ teste } />
+      <MovieLibrary movie={ [1, 2] } />
     </div>
   );
 }
